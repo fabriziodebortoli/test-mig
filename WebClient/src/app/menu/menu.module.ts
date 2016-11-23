31d2a0cd6043389a97c197ab51536c8a3e0c4a23
Luca Bruni <luca.bruni@microarea.it>
2016-11-23 17:25:00 +0100

    aggiunto ulteriore componente menu

--- WebClient/src/app/menu/menu.module.ts
+++ WebClient/src/app/menu/menu.module.ts
@@ -5,6 +5,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { ApplicationSelectorComponent } from './components/menu/application-selector/application-selector.component';
 import { GroupSelectorComponent } from './components/menu/group-selector/group-selector.component';
 import { MenuSelectorComponent } from './components/menu/menu-selector/menu-selector.component';
+import { TileContainerComponent } from './components/menu/tile-container/tile-container.component';
 
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
@@ -17,8 +18,8 @@ import { CommonModule } from '@angular/common';
     FormsModule,
   ],
 
-  declarations: [LoginComponent, MenuComponent, ApplicationSelectorComponent, GroupSelectorComponent, MenuSelectorComponent],
-  exports: [LoginComponent, MenuComponent, ApplicationSelectorComponent, GroupSelectorComponent, MenuSelectorComponent],
+  declarations: [LoginComponent, MenuComponent, ApplicationSelectorComponent, GroupSelectorComponent, MenuSelectorComponent, TileContainerComponent],
+  exports: [LoginComponent, MenuComponent, ApplicationSelectorComponent, GroupSelectorComponent, MenuSelectorComponent, TileContainerComponent],
   providers:[MenuService]
 })
 export class MenuModule { }
