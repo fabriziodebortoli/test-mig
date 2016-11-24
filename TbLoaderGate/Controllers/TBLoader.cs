7c7ab77aa81363e9a2c26606c5fbabd7ea752e15
marco.perasso <marco.perasso@gmail.com>
2016-11-24 15:25:03 +0100

    web server, progetto contenitore

---
+++ TbLoaderGate/Controllers/TBLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassLibrary
+{
+    public class TBLoaderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ObjectResult("TBLoader Gate");
+        }
+
+    }
+}
+
