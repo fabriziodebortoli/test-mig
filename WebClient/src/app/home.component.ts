9b3ef011e03e92df928f6e07ddafbf6eb40ac656
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2016-11-25 15:35:44 +0100

    Home Component works

--- WebClient/src/app/home.component.ts
+++ WebClient/src/app/home.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tb-home',
-  template: `
-  `,
+  template: `<p>Home Component works!</p>`,
   styles: []
 })
 export class HomeComponent implements OnInit {
