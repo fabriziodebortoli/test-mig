a91b615a31931e998ba1cf274ed9c603c7311e91
Marco Perasso <marco.perasso@gmail.com>
2016-11-29 12:25:08 +0100

    eliminato file locale

--- TbLoaderGate/Application/TbLoaderInstance.cs
+++ TbLoaderGate/Application/TbLoaderInstance.cs
@@ -5,7 +5,7 @@ namespace TBLoaderGate
 {
     public class TBLoaderInstance
     {
-        public int port = 10000;
+        public int port = 11000;
         public string server = "localhost";
 
         public string BaseUrl { get { return string.Concat("http://", server, ":", port); } }
@@ -13,7 +13,7 @@ namespace TBLoaderGate
 
         internal void Execute()
         {
-            TBLoaderService svc = new TBLoaderService("localhost", 11000);
+            TBLoaderService svc = new TBLoaderService("localhost", port);
             port = svc.ExecuteRemoteProcess();
         }
 
