5fa07b53fade508a4c207f127f5ceecbd4d0ef76
marco.perasso <marco.perasso@gmail.com>
2016-11-30 18:08:27 +0100

    accoppiamento web socket

--- TbLoaderGate/Application/SocketDispatcher.cs
+++ TbLoaderGate/Application/SocketDispatcher.cs
@@ -11,42 +11,78 @@ namespace TBLoaderGate
 {
     class WebSocketCouple
     {
-        WebSocket first;
-        WebSocket second;
+        private WebSocket first;
+        private WebSocket second;
+
+        internal bool Contains(WebSocket webSocket)
+        {
+            return first == webSocket || second == webSocket;
+        }
+
+        internal void Add(WebSocket webSocket)
+        {
+            if (first == null)
+                first = webSocket;
+            else if (second == null)
+                second = webSocket;
+            else
+                throw new Exception("Web sockets already assigned");
+        }
+
+        internal WebSocket GetOther(WebSocket webSocket)
+        {
+             return (first == webSocket) ? second : first;
+        }
     }
     public class SocketDispatcher
     {
+        const string setWebSocketName = "SetWebSocketName:";
         Dictionary<string, WebSocketCouple> socketMap = new Dictionary<string, WebSocketCouple>();
         internal async Task HandleAsync(ISession session, WebSocketManager webSockets)
         {
             var webSocket = await webSockets.AcceptWebSocketAsync();
-            
+            string coupleName = "";
+
             while (webSocket.State == WebSocketState.Open)
             {
                 var token = CancellationToken.None;
                 var buffer = new ArraySegment<Byte>(new Byte[4096]);
                 var received = await webSocket.ReceiveAsync(buffer, token);
 
-
                 switch (received.MessageType)
                 {
                     case WebSocketMessageType.Text:
-                        var request = Encoding.UTF8.GetString(buffer.Array,
-                                                buffer.Offset,
-                                                buffer.Count);
-
-                        if (request.StartsWith("SetWebSocketName:"))
                         {
+                            var message = Encoding.UTF8.GetString(buffer.Array,
+                                                    buffer.Offset,
+                                                    buffer.Count);
+                            //se il messaggio imposta il nome del socket, metto da parte l'istanza per accoppiarla con la controparte
+                            if (message.StartsWith(setWebSocketName))
+                            {
+                                coupleName = message.Substring(setWebSocketName.Length);
+                                WebSocketCouple couple = null;
+                                if (!socketMap.TryGetValue(coupleName, out couple))
+                                {
+                                    couple = new WebSocketCouple();
+                                    socketMap[coupleName] = couple;
+                                }
+                                if (!couple.Contains(webSocket))
+                                    couple.Add(webSocket);
+                            }
+                            else
+                            {
+                                //altrimenti cerco il socket della controparte (by name) e gli mando una copia di quanto ricevuto
+                                WebSocketCouple couple = null;
+                                if (socketMap.TryGetValue(coupleName, out couple))
+                                {
+                                    WebSocket otherSocket = couple.GetOther(webSocket);
+                                    await otherSocket.SendAsync(buffer, WebSocketMessageType.Text, true, token);
+                                }
 
+                            }
+                            break;
                         }
-                        else
-                        {
-                            WebSocket otherSocket = null;
-                            //await otherSocket.SendAsync(buffer, WebSocketMessageType.Text, true, token);
-                        }
-                        break;
                 }
             }
         }
     }
-}
