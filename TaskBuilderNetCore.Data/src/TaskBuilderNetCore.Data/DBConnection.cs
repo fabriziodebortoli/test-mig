2532690962632195a94cf4b6075b4083361b0ab0
HolaChica <anastasia.sarmant@hotmail.com>
2016-12-01 12:50:12 +0100

    Aggiunto prototipo di dbClient

---
+++ TaskBuilderNetCore.Data/src/TaskBuilderNetCore.Data/DBConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+
+namespace TaskBuilderNetCore.Data
+{
+    public class DBConnection 
+    {    
+        private string connectionString { get; set; }
+        private Provider.DBType dbType { get; set; }
+
+        private DbConnection connection;
+
+        // constructor 
+        public DBConnection (Provider.DBType dbType, string connectionString)
+        {
+            this.dbType = dbType;
+            this.connectionString = connectionString;
+
+            switch(dbType)
+            {
+                case Provider.DBType.POSTGRE:
+                    {
+                        connection = new NpgsqlConnection(new NpgsqlConnectionStringBuilder(connectionString));
+                        break;
+                    }
+                case Provider.DBType.SQLSERVER:
+                    {                
+                        connection = new SqlConnection(connectionString);
+                        break;
+                    }
+              
+            }
+        }
+
+
+
+    }
+}
---
+++ TaskBuilderNetCore.Data/src/TaskBuilderNetCore.Data/Provider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace TaskBuilderNetCore.Data
+{
+    public class Provider
+    {
+        public enum DBType { SQLSERVER, POSTGRE };
+    }
+
+
+    public class DBException : DbException
+    {
+        public DBException() : base(){ }
+        public DBException(string message) : base(message) { }
+        public DBException(string message, Exception innerException) : base( message, innerException) { }
+
+    }
+}
