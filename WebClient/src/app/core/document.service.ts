393de42f867c3ea2c7e51d8bdf9631d683c6db50
SilvanoParodi-Microarea <silvano.parodi@microarea.it>
2016-12-06 17:33:47 +0100

    DataBinding grezzo

--- WebClient/src/app/core/document.service.ts
+++ WebClient/src/app/core/document.service.ts
@@ -2,10 +2,14 @@ import { WebSocketService } from './websocket.service';
 import { Injectable } from '@angular/core';
 @Injectable()
 export class DocumentService {
+    data: any;
+
     constructor(private webSocketService: WebSocketService) {
 
+        var me = this;
         this.webSocketService.on('DataReady', data => {
             console.debug(data);
+            me.data = data
         });
     }
 }
