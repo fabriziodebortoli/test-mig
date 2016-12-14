dacdb4825f03ebaf966dbf4af6535b291da1cb17
Luca Bruni <luca.bruni@microarea.it>
2016-12-14 10:24:22 +0100

    Sistemato change tab in seguito a cambiamenti sul tabs material

--- WebClient/FormClient/src/app/menu/components/menu/menu-selector/menu-selector.component.ts
+++ WebClient/FormClient/src/app/menu/components/menu/menu-selector/menu-selector.component.ts
@@ -1,29 +1,43 @@
 import { UtilsService } from 'tb-core';
 import { MenuService } from './../../../services/menu.service';
 import { SettingsService } from './../../../services/settings.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'tb-menu-selector',
   templateUrl: './menu-selector.component.html',
   styleUrls: ['./menu-selector.component.css']
 })
 export class MenuSelectorComponent implements OnInit {
-private selectedIndex: number;
+
   constructor(private menuService: MenuService, private utilsService: UtilsService, private settingsService: SettingsService) {
   }
+
+
+  private group: any ;
+    get Group(): any {
+        return this.group;
+    }
+    
+    @Input()
+    set Group(group: any) {
+        this.group = group;
+        this.initTab();
+    }
+
 
   ngOnInit() {
     this.initTab();
   }
 
   initTab() {
-    if (this.menuService.selectedGroup == undefined)
+    if (this.menuService.selectedGroup == undefined) {
       return;
+    }
 
-    var tempMenuArray = this.utilsService.toArray(this.menuService.selectedGroup.Menu);
+    let tempMenuArray = this.utilsService.toArray(this.menuService.selectedGroup.Menu);
 
-    var found = false;
-    for (var i = 0; i < tempMenuArray.length; i++) {
+    let found = false;
+    for (let i = 0; i < tempMenuArray.length; i++) {
       if (tempMenuArray[i].name.toLowerCase() == this.settingsService.lastMenuName.toLowerCase()) {
         this.menuService.setSelectedMenu(tempMenuArray[i]);
         return;
@@ -36,7 +50,23 @@ private selectedIndex: number;
 
   }
 
-  changeTab (tab) {
-    this.menuService.setSelectedMenu(tab);
+   changeTab(menu) {
+
+    if (menu == undefined)
+      return;
+
+
+      this.menuService.setSelectedMenu(menu);
+  }
+
+  changeTabByIndex(index) {
+
+    if (index < 0)
+      return;
+
+    let tempMenuArray = this.utilsService.toArray(this.menuService.selectedGroup.Menu);
+    let tab = tempMenuArray[index];
+    if (tab != undefined)
+      this.menuService.setSelectedMenu(tab);
   }
 }
