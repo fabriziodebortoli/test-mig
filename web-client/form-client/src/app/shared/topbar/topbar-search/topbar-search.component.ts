8f5c0d5c0ed8647fba882a22baf92c49682c0570
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2016-12-27 16:45:07 +0100

    input search ma deprecata

--- web-client/form-client/src/app/shared/topbar/topbar-search/topbar-search.component.ts
+++ web-client/form-client/src/app/shared/topbar/topbar-search/topbar-search.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarSearchComponent implements OnInit {
 
+  private searchText: string = "Search"; //TODO localizzazione
+
   constructor() { }
 
   ngOnInit() {
