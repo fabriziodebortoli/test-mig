b1c65908cb0a0b6b7a39225e7e07fdcbbb3dafa6
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2016-12-30 15:40:11 +0100

    toolbar button separator

--- web-client/form-client/src/app/shared/toolbar/index.ts
+++ web-client/form-client/src/app/shared/toolbar/index.ts
@@ -1,3 +1,4 @@
 export * from './toolbar-top/toolbar-top.component';
+export * from './toolbar-top/toolbar-separator.component';
 export * from './toolbar-top/history/history.component';
 export * from './toolbar-button.component';
