fe1638124b9ba5d03206948afc04b86ca040141a
SilvanoParodi-Microarea <silvano.parodi@microarea.it>
2017-01-03 11:20:40 +0100

    Logging Model

--- web-client/form-client/src/app/core/document.service.ts
+++ web-client/form-client/src/app/core/document.service.ts
@@ -1,16 +1,20 @@
 import { WebSocketService } from './websocket.service';
 import { Injectable } from '@angular/core';
+import { Logger } from 'libclient';
+
 @Injectable()
 export class DocumentService {
     data: any;
     mainCmpId: string;
-    constructor(private webSocketService: WebSocketService) {
+    constructor( private webSocketService: WebSocketService,
+                 private logger: Logger) {
         this.webSocketService.dataReady.subscribe(data => {
             let models: Array<any> = data.models;
             let cmpId = this.mainCmpId;
             models.forEach(model => {
                 if (model.id === cmpId) {
                     this.data = model;
+                    logger.debug("Model received from server: " + JSON.stringify(this.data));
                 }
             });
 
