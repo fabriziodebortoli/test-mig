450e0584d70b843c368dc0c786536824c4415fee
Luca Bruni <luca.bruni@microarea.it>
2017-01-31 16:31:32 +0100

    aggiunti stub di metodi di login manager rest

--- server/login-manager/Controllers/LoginManagerController.cs
+++ server/login-manager/Controllers/LoginManagerController.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Microarea.LoginManager.Controllers
 {
 	[Route("login-manager")]
 	public class LoginManagerController : Controller
-    {
-		[Route("index")]
-		public IActionResult Index()
+	{
+		[Route("login-compact")]
+		public IActionResult LoginCompact()
 		{
-			return new ObjectResult("TBLoader Gate default page");
+			string user = HttpContext.Request.Form["user"];
+			string password = HttpContext.Request.Form["password"];
+			string company = HttpContext.Request.Form["company"];
+			string askingProcess = HttpContext.Request.Form["askingProcess"];
+			string overwriteLogin = HttpContext.Request.Form["overwriteLogin"];
+
+			//string json = "{\"Companies\": { \"Company\": [{ \"name\": \"Development\" },{\"name\": \"Development2\" }] }}";
+			string json = "{}";
+			return new ContentResult { Content = json, ContentType = "application/json" };
 		}
 
-       
-    }
+		[Route("logoff")]
+		public IActionResult Logoff()
+		{
+			string user = HttpContext.Request.Form["token"];
+			string json = "{}";
+			return new ContentResult { Content = json, ContentType = "application/json" };
+		}
+
+		[Route("getCompaniesForUser")]
+		public IActionResult getCompanyForUser()
+		{
+			
+
+			string user = HttpContext.Request.Form["user"];
+			string json = "{}";
+			return new ContentResult { Content = json, ContentType = "application/json" };
+
+		}
+
+
+	}
 }
