b14e8e7fad8c68c98650cd49c235c7182a6bd49f
Luca Bruni <luca.bruni@microarea.it>
2017-02-03 15:44:33 +0100

    aggiunto stub di controller per menu-gate

---
+++ server/menu-gate/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+	[Route("menu-gate")]
+    public class MenuController : Controller
+    {
+        
+    }
+}
