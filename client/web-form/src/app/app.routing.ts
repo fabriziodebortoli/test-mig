5435225af8cb2c01fadcc0201f032153883fdea5
darkzena <darkzenatux@gmail.com>
2017-02-08 11:43:35 +0100

    Data service component test

--- client/web-form/src/app/app.routing.ts
+++ client/web-form/src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './menu/components/login/login.component';
 import { PageNotFoundComponent } from 'tb-shared';
 import { HomeComponent } from './home/home.component';
 import { ModuleWithProviders } from '@angular/core';
+import { DataServiceComponent } from './applications/test/data-service/data-service.component';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CoreGuard } from './core/core.guard';
@@ -15,6 +16,7 @@ export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [CoreGuard] },
     { path: 'Framework/TbGes/IDD_Unsupported', component: UnsupportedFactoryComponent, outlet: 'dynamic' },
     { path: 'menu', loadChildren: 'app/menu/menu.module#MenuModule' },
+    { path: 'ds', component: DataServiceComponent },
 
     { path: '**', component: PageNotFoundComponent },
 
