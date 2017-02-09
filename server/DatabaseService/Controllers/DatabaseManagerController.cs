4593aa87699f95c298de4b57e3c51dffb0fc9b2c
mdelbene <michela.delbene@microarea.it>
2017-02-09 14:22:18 +0100

    Aggiunto progetto DatabaseService

---
+++ server/DatabaseService/Controllers/DatabaseManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microarea.DatabaseService.Controllers
+{
+	[Route("database-manager")]
+	//============================================================================
+	public class DatabaseManagerController : Controller
+	{
+		[Route("create-database")]
+		//---------------------------------------------------------------------
+		public IActionResult CreateDatabase()
+		{
+			/*string server = "USR-DELBENEMIC";
+			string database = HttpContext.Request.Form["database"];
+			string user = "sa";
+			string password = "14";*/
+
+			return new ContentResult { Content = "", ContentType = "application/json" };
+		}
+	}
+}
