5f6ebfc1514de53c49cd9883304eff57a3b79cb4
RiccardoRaccuglia <riccardo.raccuglia@microarea.it>
2017-02-09 08:58:06 +0100

    corretto un namespace

--- server/menu-gate/Controllers/MenuController.cs
+++ server/menu-gate/Controllers/MenuController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApplication1.Controllers
+namespace Microarea.Menu.Controllers
 {
 	[Route("menu-gate")]
     public class MenuController : Controller
