6b382b5e152591499d857dea05dde01c5287b6df
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-02-13 12:15:45 +0100

    toolbar-top-button moved

--- client/web-form/src/app/shared/toolbar/index.ts
+++ client/web-form/src/app/shared/toolbar/index.ts
@@ -1,5 +1,5 @@
 export * from './toolbar-top/toolbar-top.component';
 export * from './toolbar-top/toolbar-separator.component';
-export * from './toolbar-top/toolbar-top-button.component';
+export * from './toolbar-top/toolbar-top-button/toolbar-top-button.component';
 export * from './toolbar-bottom/toolbar-bottom.component';
 export * from './toolbar-bottom/toolbar-bottom-button.component';
--- client/web-form/src/app/shared/toolbar/toolbar-top/toolbar-top-button/toolbar-top-button.component.ts
+++ client/web-form/src/app/shared/toolbar/toolbar-top/toolbar-top-button/toolbar-top-button.component.ts
@@ -1,22 +1,11 @@
-import { TbComponent } from './../..';
-import { HttpService, DocumentService, WebSocketService } from 'tb-core';
 import { Component, OnInit, Input } from '@angular/core';
-
+import { TbComponent } from './../../..';
+import { HttpService, DocumentService, WebSocketService } from 'tb-core';
 
 @Component({
   selector: 'tb-toolbar-top-button',
-  template: `<div (click)='onCommand()' title='{{caption}}'><img src="{{getIconUrl()}}"/></div>`,
-  styles: [`
-    div{
-      cursor:pointer;
-      margin: 0 4px;
-    }
-    md-icon{
-      line-height: 30px;
-      display: block;
-      font-size: 22px;
-    }
-  `]
+  templateUrl: './toolbar-top-button.component.html',
+  styleUrls: ['./toolbar-top-button.component.scss']
 })
 export class ToolbarTopButtonComponent extends TbComponent implements OnInit {
 
