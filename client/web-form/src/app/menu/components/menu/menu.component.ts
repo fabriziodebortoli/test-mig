dc731589915df3fbc9ea6643475301a8f1ef73ff
marco.perasso <marco.perasso@gmail.com>
2017-02-15 12:27:20 +0100

    refactoring

--- client/web-form/src/app/menu/components/menu/menu.component.ts
+++ client/web-form/src/app/menu/components/menu/menu.component.ts
@@ -1,3 +1,8 @@
+import { EventManagerService } from './../../services/event-manager.service';
+import { SettingsService } from './../../services/settings.service';
+import { LocalizationService } from './../../services/localization.service';
+import { MenuService } from './../../services/menu.service';
+import { HttpMenuService } from './../../services/http-menu.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MenuComponent implements OnInit {
-
+  getMenuElementsSubscription: any;
   constructor(
-    
+    private httpMenuService: HttpMenuService,
+    private menuService: MenuService,
+    private localizationService: LocalizationService,
+    private settingsService: SettingsService,
+    private eventManagerService: EventManagerService
   ) {
 
+    this.eventManagerService.preferenceLoaded.subscribe(result => {
+      this.menuService.initApplicationAndGroup(this.menuService.applicationMenu.Application);  //qui bisogna differenziare le app da caricare, potrebbero essere app o environment
+    });
   }
   ngOnInit() {
+    this.getMenuElementsSubscription = this.httpMenuService.getMenuElements().subscribe(result => {
 
+      this.menuService.onAfterGetMenuElements(result.Root);
+
+      this.localizationService.loadLocalizedElements(true);
+      this.settingsService.getSettings();
+    });
+  }
+
+  ngOnDestroy() {
+    this.getMenuElementsSubscription.unsubscribe();
   }
 }
