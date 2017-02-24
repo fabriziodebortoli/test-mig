122678004880a07b6aaf39b7d6a7f72d21e831c8
RiccardoRaccuglia <riccardo.raccuglia@microarea.it>
2017-02-24 16:25:47 +0100

    esempio

--- server/data-service/Controllers/DSController.cs
+++ server/data-service/Controllers/DSController.cs
@@ -7,12 +7,21 @@ using Microarea.DataService.Models;
 
 namespace DataService.Controllers
 {
-    [Route("ds")]
+    [Route("data-service")]
     public class DSController : Controller
     {
-        [Route("data-service")]
+        [Route("GetData/{namespace}/{queryname}")]
         public IActionResult GetData()
         {
+            string sAuthT = HttpContext.Request.Cookies["AuthToken"];
+            if (string.IsNullOrEmpty(sAuthT))
+                return new ContentResult { StatusCode = 504, Content = "non sei autenticato!", ContentType = "application/text" };
+
+            //HttpContext.Context.Request.Query
+
+            //TODO login come RS
+            //  var response = await client.PostAsync("account-manager/getLoginInformation/", content);
+
             Datasource ds = new Datasource(null);
             if (!ds.Load("erp.items.ds_ItemsSimple", "Code"))
                 return new ContentResult { Content = "It fails to load", ContentType = "application/text" };
