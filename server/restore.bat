f2fde2106caafd50a600f40de4af9e3b2af4c5d1
RiccardoRaccuglia <riccardo.raccuglia@microarea.it>
2017-03-01 12:45:19 +0100

    tolto cadavere

--- server/restore.bat
+++ server/restore.bat
@@ -4,7 +4,6 @@ dotnet restore ./library/taskbuilder-netcore-data/project.json --no-cache
 dotnet restore ./library/taskbuilder-netcore-common/project.json --no-cache
 dotnet restore ./account-manager/project.json --no-cache
 dotnet restore ./provisioning-server/project.json --no-cache
-dotnet restore ./login-manager/project.json --no-cache
 dotnet restore ./menu-gate/project.json --no-cache
 dotnet restore ./data-service/project.json --no-cache
 dotnet restore ./database-service/project.json --no-cache
