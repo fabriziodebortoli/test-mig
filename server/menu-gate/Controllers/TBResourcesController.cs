1c53b628b3797135d1e6084f724ac66ad8a35fa7
laragrillo <lara.grillo@microarea.it>
2017-03-03 11:49:57 +0100

    visualizzo le folder

--- server/menu-gate/Controllers/TBResourcesController.cs
+++ server/menu-gate/Controllers/TBResourcesController.cs
@@ -29,6 +29,7 @@ namespace Microarea.Menu.Controllers
             return apps; 
         } 
 
+      
         [Route("explorer-open/get-applications")]
         //--------------------------------------------------------------------------
         public IActionResult GetApplicationsJson()
@@ -67,14 +68,19 @@ namespace Microarea.Menu.Controllers
             return new ContentResult { Content = sb.ToString(), ContentType = "application/json" };
         }
 
-        [Route("get-folders")]
+
+        [Route("explorer-open/get-folders/{applicationPath}")]
         //--------------------------------------------------------------------------
-        public IActionResult GetFolders(string applicationName)
+        public IActionResult GetFolders(string applicationPath)
         {
             //string json = "{\"Companies\": { \"Company\": [{ \"name\": \"Development\" },{\"name\": \"Development2\" }] }}";
-           
-           /// string[] folders = task.Result;
-
+            if (string.IsNullOrEmpty(applicationPath))
+                return null;
+            
+            string app = HttpContext.Request.Query["applicationPath"];
+            DirectoryInfo currentAppDir = new DirectoryInfo(app);
+            IList folders = currentAppDir.GetDirectories();
+            
             StringBuilder sb = new StringBuilder();
             StringWriter sw = new StringWriter(sb);
             JsonWriter jsonWriter = new JsonTextWriter(sw);
@@ -88,13 +94,15 @@ namespace Microarea.Menu.Controllers
 
             jsonWriter.WriteStartArray();
 
-            //foreach (string item in folders)
-            //{
-            //    jsonWriter.WriteStartObject();
-            //    jsonWriter.WritePropertyName("name");
-            //    jsonWriter.WriteValue(item);
-            //    jsonWriter.WriteEndObject();
-            //}
+            foreach (DirectoryInfo item in folders)
+            {
+                jsonWriter.WriteStartObject();
+                jsonWriter.WritePropertyName("name");
+                jsonWriter.WriteValue(item.Name);
+                jsonWriter.WritePropertyName("path");
+                jsonWriter.WriteValue(item.FullName);
+                jsonWriter.WriteEndObject();
+            }
             jsonWriter.WriteEndArray();
 
             jsonWriter.WriteEndObject();
@@ -104,128 +112,6 @@ namespace Microarea.Menu.Controllers
             return new ContentResult { Content = sb.ToString(), ContentType = "application/json" };
         }
 
-        ////--------------------------------------------------------------------------
-        //public TBResourcesExplorer(ObjectType aObjectType, NameSpace aNameSpace)
-        //{
-        //    if (aNameSpace.NameSpaceType.Type == NameSpaceObjectType.NotValid)
-        //        return;
-
-        //    if (aNameSpace.IsValid())              //costruzione del NameSpace 
-        //    {
-        //        nameSpace = aNameSpace;
-        //    }
-        //       // string typeNs = aNameSpace.NameSpaceType.Type.ToString() + ".";
-
-        //        //typeNs = typeNs + 
-        //        //nameSpace.CreateNameSpace();
-        //        //        if (aNameSpace.GetType() == CTBNamespace::NOT_VALID)
-        //        //            m_NameSpace.SetType(CTBNamespace::REPORT);
-        //        //        else
-        //        //            m_NameSpace.SetType(aNameSpace.GetType());
-
-        //        //        if (aNameSpace.GetApplicationName().IsEmpty())
-        //        //        {
-        //        //            AddOnApplication* pApp = AfxGetBaseApp()->GetMasterAddOnApp();
-        //        //            m_NameSpace.SetApplicationName(pApp->m_strAddOnAppName);
-        //        //        }
-        //        //        else
-        //        //            m_NameSpace.SetApplicationName(aNameSpace.GetApplicationName());
-
-        //        //        if (aNameSpace.GetObjectName(CTBNamespace::MODULE).IsEmpty())
-        //        //        {
-        //        //            AddOnApplication* pAddOnApp = AfxGetAddOnApp(m_NameSpace.GetApplicationName());
-        //        //            if (pAddOnApp)
-        //        //                pMods = pAddOnApp->m_pAddOnModules;
-
-        //        //            if (pMods && pMods->GetSize())
-        //        //            {
-        //        //                for (int i = 0; i <= pMods->GetUpperBound(); i++)
-        //        //                {
-        //        //                    //tra i moduli cerca il primo attivo e lo inizializza come primo namespace
-        //        //                    if (!AfxIsActivated(pMods->GetAt(i)->GetApplicationName(), pMods->GetAt(i)->GetModuleName()))
-        //        //                        continue;
-
-        //        //                    m_NameSpace.SetObjectName(CTBNamespace::MODULE, pMods->GetAt(i)->GetModuleName());
-        //        //                    break;
-        //        //                }
-        //        //            }
-        //        //        }
-        //        //        else
-        //        //            m_NameSpace.SetObjectName(CTBNamespace::MODULE, aNameSpace.GetObjectName(CTBNamespace::MODULE));
-
-        //        //        CTBExplorerCachePtr cache = GetExplorerCache();
-
-        //        //        //memorizza l'ultimo modulo selezionato
-        //        //        if (!cache->m_LastUsedNameSpace.IsEmpty())
-        //        //            m_NameSpace.SetObjectName(CTBNamespace::MODULE, cache->m_LastUsedNameSpace.GetModuleName());
-
-        //        //    }
-        //        //    else
-        //        //        m_NameSpace = aNameSpace;
-        //        //}
-
-
-
-        //        //    {
-        //        //        CString strApp;
-        //        //        AddOnModsArray* pMods = NULL;
-
-
-        //        //        m_ExplorerType = aType;
-        //        //        m_bCanLink = FALSE;
-
-        //        //        //Cancella le informazioni relative all'ultimo modulo selezionato
-        //        //        CTBExplorer::ClearStoredInfo();
-
-        //        //        if (!aNameSpace.IsValid())              //costruzione del NameSpace 
-        //        //        {
-        //        //            if (aNameSpace.GetType() == CTBNamespace::NOT_VALID)
-        //        //                m_NameSpace.SetType(CTBNamespace::REPORT);
-        //        //            else
-        //        //                m_NameSpace.SetType(aNameSpace.GetType());
-
-        //        //            if (aNameSpace.GetApplicationName().IsEmpty())
-        //        //            {
-        //        //                AddOnApplication* pApp = AfxGetBaseApp()->GetMasterAddOnApp();
-        //        //                m_NameSpace.SetApplicationName(pApp->m_strAddOnAppName);
-        //        //            }
-        //        //            else
-        //        //                m_NameSpace.SetApplicationName(aNameSpace.GetApplicationName());
-
-        //        //            if (aNameSpace.GetObjectName(CTBNamespace::MODULE).IsEmpty())
-        //        //            {
-        //        //                AddOnApplication* pAddOnApp = AfxGetAddOnApp(m_NameSpace.GetApplicationName());
-        //        //                if (pAddOnApp)
-        //        //                    pMods = pAddOnApp->m_pAddOnModules;
-
-        //        //                if (pMods && pMods->GetSize())
-        //        //                {
-        //        //                    for (int i = 0; i <= pMods->GetUpperBound(); i++)
-        //        //                    {
-        //        //                        //tra i moduli cerca il primo attivo e lo inizializza come primo namespace
-        //        //                        if (!AfxIsActivated(pMods->GetAt(i)->GetApplicationName(), pMods->GetAt(i)->GetModuleName()))
-        //        //                            continue;
-
-        //        //                        m_NameSpace.SetObjectName(CTBNamespace::MODULE, pMods->GetAt(i)->GetModuleName());
-        //        //                        break;
-        //        //                    }
-        //        //                }
-        //        //            }
-        //        //            else
-        //        //                m_NameSpace.SetObjectName(CTBNamespace::MODULE, aNameSpace.GetObjectName(CTBNamespace::MODULE));
-
-        //        //            CTBExplorerCachePtr cache = GetExplorerCache();
-
-        //        //            //memorizza l'ultimo modulo selezionato
-        //        //            if (!cache->m_LastUsedNameSpace.IsEmpty())
-        //        //                m_NameSpace.SetObjectName(CTBNamespace::MODULE, cache->m_LastUsedNameSpace.GetModuleName());
-
-        //        //        }
-        //        //        else
-        //        //            m_NameSpace = aNameSpace;
-        //    //}
-        //}
-
-
+      
     }
 }
