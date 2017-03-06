13888129d2de5eb2d53a3479f1002aa308eb8ae6
simonapodesta <simona.podesta@microarea.it>
2017-03-06 15:18:55 +0100

    refactor radio
    Creata cartella per il controllo radio

---
+++ client/web-form/src/app/shared/controls/radio/radio.component.ts
@@ -0,0 +1,11 @@
+﻿import { ControlComponent } from './../control.component';
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'tb-radio',
+    templateUrl: 'radio.component.html',
+    styleUrls: ['./radio.component.scss']
+})
+
+export class RadioComponent extends ControlComponent {
+}
