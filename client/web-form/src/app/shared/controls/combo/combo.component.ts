26ed5bd08a22bf525ad5f474fc8c211ea6280472
simonapodesta <simona.podesta@microarea.it>
2017-03-06 15:13:39 +0100

    refactor combo
    Creata una cartella per il controllo combo

---
+++ client/web-form/src/app/shared/controls/combo/combo.component.ts
@@ -0,0 +1,11 @@
+﻿import { ControlComponent } from './../control.component';
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'tb-combo',
+    templateUrl: 'combo.component.html',
+    styleUrls: ['./combo.component.scss']
+})
+export class ComboComponent  extends ControlComponent {
+}
