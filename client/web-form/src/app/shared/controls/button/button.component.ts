af11f3a8fe1573defe0b9494e7fb0b5b5db14e76
simonapodesta <simona.podesta@microarea.it>
2017-03-06 15:08:25 +0100

    Refactor button
    Creata cartella per controllo bottone

---
+++ client/web-form/src/app/shared/controls/button/button.component.ts
@@ -0,0 +1,12 @@
+﻿import { ControlComponent } from './../control.component';
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+    selector: 'tb-button',
+    templateUrl: 'button.component.html',
+    styleUrls: ['./button.component.scss']
+})
+
+export class ButtonComponent  extends ControlComponent {
+}
