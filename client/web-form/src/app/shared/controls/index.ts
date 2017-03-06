381c7d1def7c7750c3719e5d09c7f4a898648892
simonapodesta <simona.podesta@microarea.it>
2017-03-06 15:14:48 +0100

    refactor index
    aggiornato l'index.ts dei control

--- client/web-form/src/app/shared/controls/index.ts
+++ client/web-form/src/app/shared/controls/index.ts
@@ -1,5 +1,5 @@
 export * from './edit.component'
 export * from './button/button.component'
 export * from './checkbox/checkbox.component'
-export * from './combo.component'
+export * from './combo/combo.component'
 export * from './radio.component'
