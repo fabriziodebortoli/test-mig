ce70c009aad7a9171467142c9d3f6264fd7c8b7f
simonapodesta <simona.podesta@microarea.it>
2017-03-06 15:16:53 +0100

    refactor edit
    Creata una cartella per il controllo edit

---
+++ client/web-form/src/app/shared/controls/edit/edit.component.ts
@@ -0,0 +1,22 @@
+﻿import { EventDataService } from './../../../core/eventdata.service';
+import { ControlComponent } from './../control.component';
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'tb-edit',
+    templateUrl: 'edit.component.html',
+    styleUrls: ['./edit.component.scss']
+})
+
+export class EditComponent extends ControlComponent{
+    constructor(
+        private eventData: EventDataService
+      ) {
+        super();
+      }
+
+    onBlur() {
+        this.eventData.change.emit(this.cmpId);
+    }
+}
--- client/web-form/src/app/shared/controls/index.ts
+++ client/web-form/src/app/shared/controls/index.ts
@@ -1,4 +1,4 @@
-export * from './edit.component'
+export * from './edit/edit.component'
 export * from './button/button.component'
 export * from './checkbox/checkbox.component'
 export * from './combo/combo.component'
