da5e03b35832dfeeaf0cc024aee9a7de16c10b42
simonapodesta <simona.podesta@microarea.it>
2017-03-13 17:24:46 +0100

    Aggiunta proprietà disabled alla maskedtextbox

--- client/web-form/src/app/shared/controls/masked-text-box/masked-text-box.component.ts
+++ client/web-form/src/app/shared/controls/masked-text-box/masked-text-box.component.ts
@@ -9,4 +9,5 @@ import { Component, Input  } from '@angular/core';
 export class MaskedTextBoxComponent extends ControlComponent  {
  @Input() mask: string;
  @Input() forCmpID: string;
+ @Input() disabled: boolean;
 }
