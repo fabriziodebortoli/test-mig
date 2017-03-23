c770575a022cbed60aff13f1e32d508a61461229
Luca Bruni <luca.bruni@microarea.it>
2017-03-23 16:02:16 +0100

    funzionamento combo in modalità browse

--- client/web-form/src/app/shared/controls/combo/combo.component.ts
+++ client/web-form/src/app/shared/controls/combo/combo.component.ts
@@ -1,5 +1,6 @@
-﻿import { ControlComponent } from './../control.component';
-import { Component, Input, OnInit } from '@angular/core';
+﻿import { AfterViewInit } from 'libclient/node_modules/@angular/core';
+import { ControlComponent } from './../control.component';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { EventDataService } from './../../../core/eventdata.service';
 import { DocumentService } from './../../../core/document.service';
 import { WebSocketService } from './../../../core/websocket.service';
@@ -10,7 +11,7 @@ import { WebSocketService } from './../../../core/websocket.service';
     styleUrls: ['./combo.component.scss']
 })
 
-export class ComboComponent extends ControlComponent {
+export class ComboComponent extends ControlComponent implements AfterViewInit, OnChanges {
 
     @Input()
     itemSourceName: string;
@@ -19,13 +20,14 @@ export class ComboComponent extends ControlComponent {
     @Input()
     itemSourceParameter: string;
 
-    private items: any;
-    private selectedItem: string;
+    private items: Array<any> = [];
+    private selectedItem: any;
 
     constructor(
-        private webSocketService: WebSocketService, 
+        private webSocketService: WebSocketService,
         private eventDataService: EventDataService) {
         super();
+
         this.webSocketService.itemSource.subscribe((result) => {
             console.log(this.model);
             this.items = result.itemSource;
@@ -33,12 +35,32 @@ export class ComboComponent extends ControlComponent {
     }
 
     fillListBox() {
-        let obj = {itemSourceName: this.itemSourceName, itemSourceNamespace: this.itemSourceNamespace, itemSourceParameter: this.itemSourceParameter};
-        this.eventDataService.openDropdown.emit(obj);        
+        this.items.slice(0, this.items.length);
+        let obj = { itemSourceName: this.itemSourceName, itemSourceNamespace: this.itemSourceNamespace, itemSourceParameter: this.itemSourceParameter };
+        this.eventDataService.openDropdown.emit(obj);
     }
 
     onChange() {
         //this.selectedValue = $event;
         console.log(this.selectedItem);
+    }
+
+    ngAfterViewInit() {
+        console.log("ciao");
+        if (this.model != undefined)
+            console.log(this.model.value);
+
+    }
+
+    ngOnChanges(changes: {}) {
+        if (changes['model'] == undefined || changes['model'].currentValue == undefined)
+            return;
+
+        this.items.splice(0, this.items.length);
+        let temp = changes['model'].currentValue.value;
+        let obj = { code: temp, description: temp };
+        this.items.push(obj);
+        console.log( this.items);
+        this.selectedItem = obj;
     }
 }
