b2a81a59e07bdfe245e91bb84c58d9e42df85904
Luca Bruni <luca.bruni@microarea.it>
2017-03-23 10:21:43 +0100

    aggiornato material alla beta 2, completato search menu

--- client/web-form/src/app/menu/components/menu/search/search.component.ts
+++ client/web-form/src/app/menu/components/menu/search/search.component.ts
@@ -1,7 +1,9 @@
+import { Observable } from 'rxjs/Rx';
 import { LocalizationService } from './../../../services/localization.service';
 import { MenuService } from './../../../services/menu.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { AutoCompleteComponent } from '@progress/kendo-angular-dropdowns';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'tb-search',
@@ -9,39 +11,53 @@ import { AutoCompleteComponent } from '@progress/kendo-angular-dropdowns';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  @ViewChild('autocomplete') public autocomplete: AutoCompleteComponent;
-
   public selected: string = '';
-  public selectedItem: any;
-  data: any;
+  inputControl: FormControl;
+  filteredElements: any;
+
+  @Input() maxElements: number = 20;
+  @ViewChild('myInput') myInput:ElementRef;
+
   constructor(
     private menuService: MenuService,
     private localizationService: LocalizationService
   ) {
-    this.data = this.menuService.searchSources.slice();
+    this.inputControl = new FormControl();
+    this.filteredElements = this.inputControl.valueChanges
+      .startWith(null)
+      .map(name => this.filteredElements(name));
   }
 
   ngOnInit() {
+    this.filteredElements = this.inputControl.valueChanges
+      .startWith(null)
+      .map(val => val ? this.filter(val) : this.menuService.searchSources.slice(0, this.maxElements));
+
+    this.inputControl.valueChanges.subscribe(data => {
+      if (this.isObject(data))
+        this.onSelect(data);
+    });
+
   }
 
   onSelect(val) {
     //commentato perchè autocomplete kendo non ritorna l'object selezionato, ma solo la stringa, e con solo il text (ad esempio customers)
     //non ho gli elementi per fare una runfunction sensata
-    // this.menuService.runFunction(val);
-     this.selected = undefined;
+    this.menuService.runFunction(val);
+    this.selected = undefined;
+    this.myInput.nativeElement.value = "";
+  }
+  
+  filter(val: string): string[] {
+    return this.menuService.searchSources.filter(option => new RegExp(val, 'gi').test(option.title)).slice(0, this.maxElements);
   }
 
-  getSearchItems() {
-    return this.menuService.searchSources;
+  displayElement(element: any): string {
+    return element ? element.title : '';
   }
 
-  handleFilter(value) {
-    //if (value.length >= 3) {
-      this.data = this.menuService.searchSources.filter((s) => s.title.toLowerCase().indexOf(value.toLowerCase()) !== -1);
-    //}
-    //else {
-     // this.autocomplete.toggle(false);
-    //}
+  isObject(val) {
+    return val instanceof Object;
   }
 
 }
