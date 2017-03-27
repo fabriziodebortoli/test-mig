cb7df38799bfd2b435b3ca74637077694985de9d
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-03-27 12:27:40 +0200

    feat(tile-manager): selector component

---
+++ client/web-form/src/app/shared/containers/tiles/tile-manager/selector/selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tb-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
