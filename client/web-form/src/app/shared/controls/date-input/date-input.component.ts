16a06378f4015b104104db7796da0fac6dabe689
RobyCresta <roberta.cresta@microarea.it>
2017-03-29 15:13:11 +0200

    DateInput: input e validazione da tastiera

--- client/web-form/src/app/shared/controls/date-input/date-input.component.ts
+++ client/web-form/src/app/shared/controls/date-input/date-input.component.ts
@@ -1,5 +1,5 @@
 import { EventDataService } from './../../../core/eventdata.service';
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit,  Input, ViewChild, OnChanges } from '@angular/core';
 import { ControlComponent } from './../control.component';
 import { Align } from "@progress/kendo-angular-popup/dist/es/models/align.interface";
 
@@ -9,16 +9,25 @@ import { Align } from "@progress/kendo-angular-popup/dist/es/models/align.interf
   styleUrls: ['./date-input.component.scss']
 })
 
-export class DateInputComponent extends ControlComponent implements OnInit {
+export class DateInputComponent extends ControlComponent implements OnInit, OnChanges {
   @Input() forCmpID: string;
-  @Output() clicked = new EventEmitter<string>();
+  @ViewChild('kendoMaskedTextBoxInstance') kendoMasked: any;
+  anchorAlign: Align = { horizontal: 'right', vertical: 'bottom' };
+  popupAlign: Align = { horizontal: 'left', vertical: 'center' };
 
-  private anchorAlign: Align = { horizontal: 'right', vertical: 'bottom' };
-  private popupAlign: Align = { horizontal: 'left', vertical: 'center' };
-  public mask = 'dA / mA / YAAA';
-  private objDate: Date;
-  private switchP = false;
-  value = '__ / __ / ____';
+  placeHolder = '__';
+  separator = '/';
+  defaultValue = this.placeHolder + this.separator + this.placeHolder + this.separator + this.placeHolder + this.placeHolder;
+  value = this.defaultValue;
+
+  selectedDate: Date;
+  switchP = false;
+  showValidationResult = false;
+  doubleEvent = false;
+  validationResult = '';
+
+  public mask = 'dA/mA/YAAA';
+  invalidDateString = 'Invalid Date';
   public rules: { [key: string]: RegExp } = {
     'A': /[0-9]/,
     'd': /[0-3]/,
@@ -30,7 +39,7 @@ export class DateInputComponent extends ControlComponent implements OnInit {
     super();
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.eventData.command.subscribe(data => this.onSave(data));
   }
 
@@ -39,35 +48,82 @@ export class DateInputComponent extends ControlComponent implements OnInit {
     this.onClickM();
   }
 
-  onClickM() {
+  onClickM(): void {
+    this.switchP = !this.switchP;
+    this.doubleEvent = false;
+  }
+
+  onBlur(): void {
+    if (! this.doubleEvent) 
+      return;
     this.switchP = !this.switchP;
   }
 
-  onClose() {
-    this.switchP = false;
+  private press(): void { // necessario per evitare che sul ckick di chiusura, il blur annulli il click
+    this.doubleEvent = true;
   }
 
-  onUpdateModel(newDate: Date) {
-    this.objDate = newDate;
-    this.value = this.objDate.toLocaleDateString('en-GB');
+  onUpdateModel(newDate: Date): void {
+    this.selectedDate = newDate;
+    this.value = this.selectedDate.toLocaleDateString('en-GB');
   }
 
-  onSave(data: string) {
+  onSave(data: string): void {
     if (data !== 'ID_EXTDOC_SAVE') { return; }
-    if(this.objDate === undefined || this.model === null) return;
-    let y = new Date(this.objDate.getFullYear(), this.objDate.getMonth(), this.objDate.getDate(),
-      12, this.objDate.getMinutes(), this.objDate.getSeconds());
+    if (this.selectedDate === undefined || this.model === null) { return; }
+    let y = new Date(this.selectedDate.getFullYear(), this.selectedDate.getMonth(), this.selectedDate.getDate(),
+      12, this.selectedDate.getMinutes(), this.selectedDate.getSeconds());
     this.model.value = y.toJSON().substring(0, 19);
   }
 
-  ngAfterViewInit() {
-    if (this.model !== undefined && this.model !== null)
+  ngAfterViewInit(): void {
+    if (this.model !== undefined && this.model !== null) {
       this.onUpdateModel(new Date(this.model.value));
+    }
   }
 
-  ngOnChanges() {
-    if (this.model !== undefined && this.model !== null)
+  ngOnChanges(): void {
+    if (this.model !== undefined && this.model !== null) {
       this.onUpdateModel(new Date(this.model.value));
+    }
+  }
+
+  resetParams(setTrue:boolean = false): void {
+    this.validationResult = setTrue ? this.invalidDateString : '';
+    this.showValidationResult = setTrue;
+    this.kendoMasked.maskValidation = setTrue;
+  }
+
+  validateDate(): void {
+    if (this.value === this.defaultValue)   return;
+
+    this.resetParams(true);
+
+    let formattedDate: Date = this.formatDate();
+    if(formattedDate === null) return;
+
+    if (Object.prototype.toString.call(formattedDate) !== '[object Date]')
+      return;
+    if (!isNaN(formattedDate.getTime())) {    // date is valid
+        this.selectedDate = formattedDate;
+        this.showValidationResult = false;
+        this.onUpdateModel(formattedDate);
+    }
+    this.validationResult = formattedDate.toLocaleDateString('en-GB');
+
+  }
+
+  formatDate(): Date {
+    let arrayDate: string[] = this.value.split(this.separator);
+    for (let index = 0; index < arrayDate.length; index++) 
+    {
+      let element = arrayDate[index];
+      if(element === this.placeHolder || element === this.placeHolder + this.placeHolder) 
+        return null;
+      if(element.search('_') !== -1)
+        arrayDate[index] = element.replace('_', '0');    
+    }
+    return new Date(arrayDate[2] + this.separator + arrayDate[1] + this.separator + arrayDate[0]);
   }
 
 }
