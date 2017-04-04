019d6cc0749240c59237b40647b890553e0b9c6c
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-04-04 23:01:33 +0200

    fix

--- client/web-form/src/app/shared/report-objects/index.ts
+++ client/web-form/src/app/shared/report-objects/index.ts
@@ -1,8 +1,8 @@
 import { ReportTextrectComponent } from './textrect/textrect.component';
 import { ReportFieldrectComponent } from './fieldrect/fieldrect.component';
-export { ReportObjectFileComponent } from './file/file.component';
+//export { ReportObjectFileComponent } from './file/file.component';
 export { ReportObjectImageComponent } from './image/image.component';
 export { ReportTableComponent } from './table/table.component';
-export { ReportObjectTextComponent } from './text/text.component';
+//export { ReportObjectTextComponent } from './text/text.component';
 
 
