42ccd88c96bd098a371ddf39a6c24dabf8f8bfc1
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-04-10 14:18:55 +0200

    feat(control): ColorPicker simple text, waiting KendoUI Component

--- client/web-form/src/app/shared/controls/color-picker/color-picker.component.ts
+++ client/web-form/src/app/shared/controls/color-picker/color-picker.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { ControlComponent } from '../control.component';
 
 @Component({
   selector: 'tb-color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.scss']
 })
-export class ColorPickerComponent implements OnInit {
+export class ColorPickerComponent extends ControlComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    super();
   }
 
 }
