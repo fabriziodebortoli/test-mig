cc095880f9e2b7e9a110f84ce9e948cbd6d68de2
Luca Bruni <luca.bruni@microarea.it>
2017-04-12 10:38:19 +0200

    tentativo di ripristinare il two way binding

--- client/web-form/src/app/shared/controls/control.component.ts
+++ client/web-form/src/app/shared/controls/control.component.ts
@@ -6,12 +6,27 @@ import { Component, Input } from '@angular/core';
 })
 
 export class ControlComponent extends TbComponent {
+    private _model: any;
+
     @Input()
     public caption: string;
-    @Input()
-    public model: any;
     @Input()
     public args: any;
     @Input()
     public validators: Array<any> = [];
+    @Input()
+    public value: any;
+
+    get model(): any {
+        return this._model;
+    }
+
+    @Input()
+    set model(model: any) {
+        if (model == undefined)
+            return;
+
+        this._model = model;
+        this.value = model.value;
+    }
 }
