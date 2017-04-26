6bd7c7743cceb73fb321e8c8eaeb8c0ac148293c
simonapodesta <simona.podesta@microarea.it>
2017-04-26 09:39:00 +0200

    Aggiunta opzioni di formattazione

--- client/web-form/src/app/shared/controls/numeric-text-box/numeric-text-box.component.ts
+++ client/web-form/src/app/shared/controls/numeric-text-box/numeric-text-box.component.ts
@@ -11,25 +11,56 @@ export class NumericTextBoxComponent extends ControlComponent {
 @Input() forCmpID: string;
 @Input() formatter: string;
 @Input() disabled: boolean;
+@Input() decimals: number;
+public formatOptionsCurrency: any = {
+        style: 'currency',
+        currency: 'EUR',
+        currencyDisplay: 'name'
+    };
+public formatOptionsInteger: any = {
+       style: 'decimal'
+    };
 
-public formatOption: any = {
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 3,
-  useGrouping: true
-};
+public formatOptionsDouble = 'F';
+
+public formatOptionsPercent: any = {
+       style: 'percent'
+    };
+
+ public caseSwitch: string;
+ getDecimalsOptions(formatter: string): number {
+     switch (formatter) {
+       case 'Integer':
+       case 'Long':
+       this.decimals = 0; break;
+
+     case 'Money':
+         this.decimals = 2; break;
+       default: break;
+     }
+     return this.decimals;
+   }
 
 
-
-getFormatOptions(formatter: string): string {
-    switch (formatter) {
+getFormatOptions(): any {
+    switch (this.formatter) {
       case 'Integer':
-        this.formatOption = {
-        style: 'decimal',
-        useGrouping: false
-};
-        break;
-      default: break;
+       case 'Long':
+       this.caseSwitch = '1';
+       return this.formatOptionsInteger ;
+
+      case 'Double':
+        this.caseSwitch = '2';
+        return this.formatOptionsDouble;
+
+        case 'Money':
+        this.caseSwitch = '3';
+        return this.formatOptionsCurrency;
+
+       case 'Percent':
+       this.caseSwitch = '4';
+       return this.formatOptionsPercent;
+     default: break;
     }
-    return this.formatOption;
   }
 }
