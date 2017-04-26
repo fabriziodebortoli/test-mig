a09045e8c711f4f78019408bbc8e52b14b28e7ce
fraricce <fraricce@gmail.com>
2017-04-26 15:03:59 +0200

    AdminServer: IInstance and ICompanyGrants

---
+++ server/admin-server/Model/ICompanyGrants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provisioning_server.Model
+{
+    interface ICompanyGrants
+    {
+        string AccountId { get; }
+        string Company { get; }
+        bool isAdmin { get; }
+    }
+}
---
+++ server/admin-server/Model/Interfaces/IInstance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provisioning_server.Model.Interfaces
+{
+    interface IInstance
+    {
+        string Id { get; }
+        string Name { get; }
+        bool Disabled { get; }
+    }
+}
