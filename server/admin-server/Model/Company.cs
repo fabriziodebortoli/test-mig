6b9386805acde15c8685086dee74ea68b5b2be70
fraricce <fraricce@gmail.com>
2017-04-26 15:42:22 +0200

    AdminServer: code polishing

--- server/admin-server/Model/Company.cs
+++ server/admin-server/Model/Company.cs
@@ -1,8 +1,4 @@
 ﻿using Microarea.AdminServer.Model.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Microarea.AdminServer.Model
 {
