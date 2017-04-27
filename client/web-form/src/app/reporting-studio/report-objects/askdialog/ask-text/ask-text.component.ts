a9e06d77fbc539c9ffd98cfb53189af3062c19c2
HolaChica <anastasia.sarmant@hotmail.com>
2017-04-27 18:44:29 +0200

    rs ask text

--- client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-text/ask-text.component.ts
+++ client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-text/ask-text.component.ts
@@ -10,13 +10,24 @@ export class AskTextComponent implements OnInit {
 
   @Input() text: text;
 
-  isDate:boolean = false;
+  type: string;
 
   constructor() { }
 
   ngOnInit() {
     if (this.text.type === 'DateTime') {
-      this.isDate = true;
+      this.type = 'datetime-local';
+      this.text.value = new Date().toDateString();
+    }
+    if (this.text.type === 'Date') {
+      this.type = 'date';
+      this.text.value = new Date().toDateString();
+    }
+    if (this.text.type === 'Text') {
+      this.type = 'text';
+    }
+    if (this.text.type === 'Double') {
+      this.type = 'number';
     }
   }
 
