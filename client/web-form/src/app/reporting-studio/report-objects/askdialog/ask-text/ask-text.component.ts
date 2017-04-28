90716ed3ea102327481b9055cdf8ed2a97f3e8f4
HolaChica <anastasia.sarmant@hotmail.com>
2017-04-28 16:11:31 +0200

    rs ask text

--- client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-text/ask-text.component.ts
+++ client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-text/ask-text.component.ts
@@ -1,6 +1,5 @@
 import { text } from './../../../reporting-studio.model';
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, Type } from '@angular/core';
 @Component({
   selector: 'rs-ask-text',
   templateUrl: './ask-text.component.html',
@@ -10,25 +9,7 @@ export class AskTextComponent implements OnInit {
 
   @Input() text: text;
 
-  type: string;
-
   constructor() { }
 
-  ngOnInit() {
-    if (this.text.type === 'DateTime') {
-      this.type = 'datetime-local';
-      this.text.value = new Date().toDateString();
-    }
-    if (this.text.type === 'Date') {
-      this.type = 'date';
-      this.text.value = new Date().toDateString();
-    }
-    if (this.text.type === 'Text') {
-      this.type = 'text';
-    }
-    if (this.text.type === 'Double') {
-      this.type = 'number';
-    }
-  }
-
+  ngOnInit() { }
 }
