7b8f0d4ce90d5688a55804357928017d4580b06f
HolaChica <anastasia.sarmant@hotmail.com>
2017-05-02 10:28:10 +0200

    simple combo in shared + ask enum combo

---
+++ client/web-form/src/app/shared/controls/combo-simple/combo-simple.component.ts
@@ -0,0 +1,14 @@
+import { ControlComponent } from './../control.component';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'tb-combo-simple',
+  templateUrl: './combo-simple.component.html',
+  styleUrls: ['./combo-simple.component.scss']
+})
+export class ComboSimpleComponent extends ControlComponent {
+
+  @Input() public items: Array<any> = [];
+  @Input() public defaultItem: any;
+
+}
