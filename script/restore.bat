a5ee6bc95b95e09cc045b1df94248bbbddf82138
RiccardoRaccuglia <riccardo.raccuglia@microarea.it>
2017-05-03 18:08:24 +0200

    Project rename

--- script/restore.bat
+++ script/restore.bat
@@ -8,7 +8,7 @@ dotnet restore ./provisioning-server/project.json --no-cache
 dotnet restore ./menu-gate/project.json --no-cache
 dotnet restore ./data-service/project.json --no-cache
 dotnet restore ./database-service/project.json --no-cache
-dotnet restore ./rs-web/project.json --no-cache
+dotnet restore ./report-service/project.json --no-cache
 dotnet restore ./tbloader-gate/project.json --no-cache
 dotnet restore ./web-server/project.json --no-cache
 dotnet restore ./widgets-service/project.json --no-cache
