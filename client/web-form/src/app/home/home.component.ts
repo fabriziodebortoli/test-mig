9296758c58d7d28edd2f7daa4a3a00415bc3373e
Luca Bruni <luca.bruni@microarea.it>
2017-05-03 11:07:46 +0200

    tabber principale Kendo

--- client/web-form/src/app/home/home.component.ts
+++ client/web-form/src/app/home/home.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { ComponentInfo } from './../shared/models/component.info';
+import { LayoutService } from 'app/core/layout.service';
+import { Component, OnInit,Output,EventEmitter,  ViewChild, OnDestroy, HostListener, ElementRef, AfterContentInit } from '@angular/core';
 
 import { environment } from './../../environments/environment';
 
 import { ComponentService } from './../core/component.service';
 import { LoginSessionService } from './../core/login-session.service';
 import { SidenavService } from './../core/sidenav.service';
+import { TabStripComponent } from "@progress/kendo-angular-layout/dist/es/tabstrip/tabstrip.component";
 
 @Component({
   selector: 'tb-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterContentInit {
 
   @ViewChild('sidenav') sidenav;
   sidenavSubscription: any;
+
+  @ViewChild('kendoTabStripInstance') kendoTabStripInstance: TabStripComponent;
+  @ViewChild('tabberContainer') tabberContainer: ElementRef;
+  viewHeight: number;
 
   private appName = environment.appName;
   private companyName = environment.companyName;
@@ -23,7 +30,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     private sidenavService: SidenavService,
     private loginSession: LoginSessionService,
     private componentService: ComponentService,
-
+    private layoutService: LayoutService
   ) {
     this.sidenavSubscription = sidenavService.sidenavOpened$.subscribe(() => this.sidenav.toggle());
 
@@ -33,6 +40,20 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   }
 
+  ngAfterContentInit() {
+    setTimeout(() => this.calcViewHeight(), 0);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.calcViewHeight();
+  }
+  calcViewHeight() {
+    this.viewHeight = this.tabberContainer ? this.tabberContainer.nativeElement.offsetHeight : 0;
+    this.layoutService.setViewHeight(this.viewHeight);
+    console.log("viewHeight", this.viewHeight);
+  }
+
   ngOnDestroy() {
     this.sidenavSubscription.unsubscribe();
   }
@@ -40,4 +61,11 @@ export class HomeComponent implements OnInit, OnDestroy {
   toggleSidenav() {
     this.sidenavService.toggleSidenav();
   }
+
+  closeTab(info: ComponentInfo) {
+    event.stopImmediatePropagation();
+    this.kendoTabStripInstance.selectTab(0);
+    this.componentService.tryDestroyComponent(info);    
+  }
+
 }
