c94dcceb485f8dae3bcc7085f562a5a1496270f9
fraricce <fraricce@gmail.com>
2017-05-04 15:41:52 +0200

    AdminServer: SQL provider configuration

---
+++ server/admin-server/AppOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microarea.AdminServer
+{
+    //================================================================================
+    public class AppOptions
+    {
+        public DatabaseConnection DatabaseConnection { get; set; }
+    }
+
+    //================================================================================
+    public class DatabaseConnection
+    {
+        public string Value { get; set; }
+    }
+}
--- server/admin-server/Services/SqlAdminDataServiceProvider.cs
+++ server/admin-server/Services/SqlAdminDataServiceProvider.cs
@@ -1,11 +1,27 @@
 ﻿using Microarea.AdminServer.Services.Interfaces;
 using System;
+using System.Data.SqlClient;
 using Microarea.AdminServer.Model.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Microarea.AdminServer.Services
 {
+    //================================================================================
     public class SqlAdminDataServiceProvider : IAdminDataServiceProvider
     {
+        AppOptions _settings;
+        string _connectionString;
+
+        public SqlAdminDataServiceProvider(IOptions<AppOptions> settings)
+        {
+            _settings = settings.Value;
+
+            if (_settings == null)
+                return;
+
+            _connectionString = _settings.DatabaseConnection.Value;
+        }
         public IUserAccount ReadLogin(string userName, string password)
         {
             throw new NotImplementedException();
