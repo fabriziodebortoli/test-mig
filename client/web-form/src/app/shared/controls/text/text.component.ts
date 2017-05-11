df803afc224737f078fc800178f48b01aff5c173
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-05-11 12:03:01 +0200

    Revert "template controls"
    This reverts commit ac8604bc1853a6b2731e67073639ff9516efa096.

---
+++ client/web-form/src/app/shared/controls/text/text.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentRef, OnChanges, AfterContentInit, Output, EventEmitter } from '@angular/core';
+// import { ContextMenuComponent } from './../context-menu/context-menu.component';
+import { EventDataService } from './../../../core/eventdata.service';
+import { ControlComponent } from '../control.component';
+
+@Component({
+  selector: 'tb-text',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.scss']
+})
+export class TextComponent extends ControlComponent /*implements AfterContentInit, OnChanges */ {
+
+  @Input('readonly') readonly: boolean = false;
+  @Input() public hotLink: any = undefined;  
+
+  @ViewChild("contextMenu", { read: ViewContainerRef }) contextMenu: ViewContainerRef;
+  // private contextMenuRef;
+
+  constructor(private eventData: EventDataService, private vcr: ViewContainerRef, private componentResolver: ComponentFactoryResolver) {
+    super();
+  }
+
+  onBlur() {
+    this.eventData.change.emit(this.cmpId);
+    this.blur.emit(this);
+  }
+
+  ngAfterContentInit() {
+
+    // let componentFactory = this.componentResolver.resolveComponentFactory(ContextMenuComponent);
+    // this.contextMenuRef = this.contextMenu.createComponent(componentFactory);
+  }
+
+  // ngOnChanges(changes: Object) {
+  //   console.log("ngOnChanges", changes);
+  // }
+}
