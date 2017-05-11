989bcc67f974fdbed72125c3476556547ff3d0f7
HolaChica <anastasia.sarmant@hotmail.com>
2017-05-11 18:02:52 +0200

    merge

--- client/web-form/src/app/reporting-studio/report-objects/askdialog/askdialog.component.ts
+++ client/web-form/src/app/reporting-studio/report-objects/askdialog/askdialog.component.ts
@@ -74,7 +74,6 @@ export class AskdialogComponent implements OnInit, OnDestroy, OnChanges {
       page: this.rsService.askPage
     };
     this.rsService.doSend(JSON.stringify(message));
-    this.close();
   }
 
   Prev() {
