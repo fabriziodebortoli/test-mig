be3950369bf982ae729d9b9c979296a18189e5f1
fraricce <fraricce@gmail.com>
2017-05-11 14:01:15 +0200

    WebAdmin: Pipe di formattazione json

--- client/web-admin/src/app/app.module.ts
+++ client/web-admin/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AccountService } from './services/account.service';
 import { AppComponent } from './app.component';
+import { JsonVisualizerPipe } from './json-visualizer.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonVisualizerPipe
   ],
   imports: [
     BrowserModule,
