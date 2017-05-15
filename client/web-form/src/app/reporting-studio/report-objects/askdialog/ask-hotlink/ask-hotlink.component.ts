d1f4ca92eced6b9f2245a0bc586d9921c3674120
HolaChica <anastasia.sarmant@hotmail.com>
2017-05-15 12:14:06 +0200

    added rs-hotlink

---
+++ client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-hotlink/ask-hotlink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rs-ask-hotlink',
+  templateUrl: './ask-hotlink.component.html',
+  styleUrls: ['./ask-hotlink.component.scss']
+})
+export class AskHotlinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/web-form/src/app/reporting-studio/reporting-studio.model.ts
+++ client/web-form/src/app/reporting-studio/reporting-studio.model.ts
@@ -4,7 +4,7 @@ export interface Message {
   message?: string;
 }
 
-export enum CommandType {WRONG, NAMESPACE, INITTEMPLATE, TEMPLATE, ASK, UPDATEASK, DATA, STOP, RUNREPORT, ENDREPORT, NONE }
+export enum CommandType {WRONG, NAMESPACE, INITTEMPLATE, TEMPLATE, ASK, UPDATEASK, DATA, STOP, RUNREPORT, ENDREPORT, NONE, HOTLINK }
 
 export enum AskObjectType { text, radio, check, dropdownlist }
 
@@ -436,6 +436,9 @@ export class dropdownlist extends askObj {
   }
 }
 
+export class hotlink extends askObj{
+  
+}
 export class dropdownListPair {
   code: string;
   description: string;
