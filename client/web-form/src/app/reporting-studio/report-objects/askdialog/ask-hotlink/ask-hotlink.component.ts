1e9474343b724f765d1842dea1fdf78b086d8524
HolaChica <anastasia.sarmant@hotmail.com>
2017-05-15 18:26:47 +0200

    ask hotlink

--- client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-hotlink/ask-hotlink.component.ts
+++ client/web-form/src/app/reporting-studio/report-objects/askdialog/ask-hotlink/ask-hotlink.component.ts
@@ -1,20 +1,21 @@
 import { ReportingStudioService } from './../../../reporting-studio.service';
 import { hotlink, CommandType } from './../../../reporting-studio.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'rs-ask-hotlink',
   templateUrl: './ask-hotlink.component.html',
   styleUrls: ['./ask-hotlink.component.scss']
 })
-export class AskHotlinkComponent implements OnInit {
+export class AskHotlinkComponent implements OnChanges{
 
   @Input() hotlink: hotlink;
   value: string = '';
 
   constructor(private rsService: ReportingStudioService) { }
 
-  ngOnInit() {
+  ngOnChanges() {
+    let k=0;
   }
 
   OnOpen(event: any) {
