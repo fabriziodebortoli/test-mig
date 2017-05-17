2f83311f0b31537193df6cfc54f9b4b3a7c41415
RiccardoRaccuglia <riccardo.raccuglia@microarea.it>
2017-05-17 16:57:06 +0200

    Revert "RS"
    This reverts commit c49c490b4b3d1a2250eeeff2b0d140121400bf03.

---
+++ client/web-form/src/app/app.module.ts
@@ -0,0 +1,57 @@
+
+import { routing } from './app.routing';
+
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from '@angular/material';
+
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { MenuModule } from './menu/menu.module';
+
+import { HomeComponent, HomeSidenavComponent } from './home';
+import { UnsupportedFactoryComponent, UnsupportedComponent } from './unsupported.component';
+import { AppComponent } from './app.component';
+
+import { MomentModule } from 'angular2-moment';
+import { ProxyRouteComponent } from './proxy-route/proxy-route.component';
+
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { StandaloneDocumentComponent } from './home/standalone.document/standalone.document.component';
+import { StandaloneReportComponent } from './home/standalone.report/standalone.report.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent, HomeSidenavComponent,
+    UnsupportedFactoryComponent,
+    UnsupportedComponent,
+    ProxyRouteComponent,
+    StandaloneDocumentComponent,
+    StandaloneReportComponent
+
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    MaterialModule,
+    CoreModule.forRoot(),
+    SharedModule,
+    MenuModule.forRoot(),
+    routing,
+    MomentModule,
+    LayoutModule
+    
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    UnsupportedComponent
+  ]
+})
+export class AppModule { }
