99c143d45f5b22c2994d9ff2f92ef4516cbf88f6
simonapodesta <simona.podesta@microarea.it>
2017-05-18 10:47:43 +0200

    localization service

--- client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-app/topbar-menu-app.component.ts
+++ client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-app/topbar-menu-app.component.ts
@@ -5,20 +5,22 @@ import { ConnectionInfoDialogComponent } from './../../../../menu/components/men
 import { ProductInfoDialogComponent } from './../../../../menu/components/menu/product-info-dialog/product-info-dialog.component';
 import { LocalizationService } from './../../../../menu/services/localization.service';
 import { MdDialog, MdDialogRef, MdDialogConfig } from '@angular/material';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'tb-topbar-menu-app',
   templateUrl: './topbar-menu-app.component.html',
   styleUrls: ['./topbar-menu-app.component.css']
 })
-export class TopbarMenuAppComponent implements OnInit {
+export class TopbarMenuAppComponent implements  OnDestroy {
 
   private title: string = "App menu";
   viewProductInfo: string;
   productInfoDialogRef: MdDialogRef<ProductInfoDialogComponent>;
   connectionInfoDialogRef: MdDialogRef<ConnectionInfoDialogComponent>;
   data: Array<any>;
+  localizationsLoadedSubscription: any;
+
   constructor(
     public dialog: MdDialog,
     private httpMenuService: HttpMenuService,
@@ -26,35 +28,41 @@ export class TopbarMenuAppComponent implements OnInit {
     private utilsService: UtilsService,
     private localizationService: LocalizationService
   ) {
+
+    this.localizationsLoadedSubscription = localizationService.localizationsLoaded.subscribe(() => {
+
+      this.data = [{
+        actionName: this.localizationService.getLocalizedElement('ViewProductInfo'),
+        click: (dataItem) => {
+          this.openProductInfoDialog();
+        }
+      }, {
+        actionName: this.localizationService.getLocalizedElement('ConnectionInfo'),
+        click: (dataItem) => {
+          this.openConnectionInfoDialog();
+        }
+      }, {
+        actionName: this.localizationService.getLocalizedElement('ClearCachedData'),
+        click: (dataItem) => {
+          this.clearCachedData();
+        }
+      }, {
+        actionName: this.localizationService.getLocalizedElement('ActivateViaSMS'),
+        click: (dataItem) => {
+          this.activateViaSMS();
+        }
+      }, {
+        actionName: this.localizationService.getLocalizedElement('ActivateViaInternet'),
+        click: (dataItem) => {
+          this.activateViaInternet();
+        }
+      }];
+
+    });
   }
 
-  ngOnInit() {
-      this.data = [{
-        actionName: "'{{" + this.localizationService.getLocalizedElement('ViewProductInfo') + "}}'",
-        click: (dataItem) => {
-           this.openProductInfoDialog();
-        }
-    }, {
-        actionName:  this.localizationService.getLocalizedElement('ConnectionInfo') ,
-        click: (dataItem) => {
-           this.openConnectionInfoDialog();
-        }
-    }, {
-        actionName: this.localizationService.getLocalizedElement('ClearCachedData') ,
-         click: (dataItem) => {
-           this.clearCachedData();
-        }
-    }, {
-        actionName: this.localizationService.getLocalizedElement('ActivateViaSMS') ,
-         click: (dataItem) => {
-           this.activateViaSMS();
-        }
-    }, {
-        actionName:  this.localizationService.getLocalizedElement('ActivateViaInternet') ,
-        click: (dataItem) => {
-           this.activateViaInternet();
-        }
-    }];
+  ngOnDestroy() {
+    this.localizationsLoadedSubscription.unsubscribe();
   }
 
   //---------------------------------------------------------------------------------------------
@@ -79,11 +87,11 @@ export class TopbarMenuAppComponent implements OnInit {
   }
 
   openProductInfoDialog() {
-    this.productInfoDialogRef = this.dialog.open(ProductInfoDialogComponent, <MdDialogConfig>{ });
+    this.productInfoDialogRef = this.dialog.open(ProductInfoDialogComponent, <MdDialogConfig>{});
   }
-   
-   openConnectionInfoDialog() {
-    this.connectionInfoDialogRef = this.dialog.open(ConnectionInfoDialogComponent, <MdDialogConfig>{ });
+
+  openConnectionInfoDialog() {
+    this.connectionInfoDialogRef = this.dialog.open(ConnectionInfoDialogComponent, <MdDialogConfig>{});
   }
 
 }
