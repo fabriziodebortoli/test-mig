9cc5e6240374fd2367c08e05f24d62b7d54e500e
simonapodesta <simona.podesta@microarea.it>
2017-05-22 10:37:26 +0200

    trasformate le dropdownlist in kendopopup e button per allineamento a sinistra

--- client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-test/topbar-menu-test.component.ts
+++ client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-test/topbar-menu-test.component.ts
@@ -1,7 +1,8 @@
+import { text } from './../../../../reporting-studio/reporting-studio.model';
 import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { Collision } from '@progress/kendo-angular-popup/dist/es/models/collision.interface';
 import { Align } from '@progress/kendo-angular-popup/dist/es/models/align.interface';
-
+import { MenuItem } from './../../../context-menu/menu-item.model';
 import { ComponentService } from './../../../../core/component.service';
 
 @Component({
@@ -14,39 +15,41 @@ export class TopbarMenuTestComponent implements OnInit {
  anchorAlign: Align = { horizontal: 'right', vertical: 'bottom' };
   popupAlign: Align = { horizontal: 'right', vertical: 'top' };
   private collision: Collision = { horizontal: 'flip', vertical: 'fit' };
- 
-data: Array<any> = [{
-        actionName: 'Data Service',
-        click: (dataItem) => {
-           this.openDataService();
-        }
-    }, {
-        actionName: 'Reporting Studio',
-        click: (dataItem) => {
-           this.openRS();
-        }
-    }, {
-        actionName: 'TB Explorer',
-         click: (dataItem) => {
-           this.openTBExplorer();
-        }
-    }, {
-        actionName: 'Test Grid Component',
-         click: (dataItem) => {
-           this.openTestGrid();
-        }
-    }, {
-        actionName: 'Test Icons',
-        click: (dataItem) => {
-           this.openTestIcons();
-        }
-    }];
+  contextMenu: MenuItem[] = new Array<MenuItem>();
 
   private title: string = "Test menu";
 
-  constructor(private componentService: ComponentService) { }
+  constructor(private componentService: ComponentService) { 
+  const item1 = new MenuItem('Data Service', 'idDataServiceButton', false, false);
+  const item2 = new MenuItem('Reporting Studio', 'idReportingStudioButton', false,  false);
+  const item3 = new MenuItem('TB Explorer', 'idTBExplorerButton', false,  false);
+  const item4 = new MenuItem( 'Test Grid Component', 'idTBExplorerButton', false,  false);
+  const item5 = new MenuItem('Test Icons', 'idTestIconsButton', false,  false);
+ this.contextMenu.push(item1, item2, item3, item4, item5);
+  }
+
   ngOnInit() {
   }
+
+  chooseAction(buttonName: string){
+switch (buttonName) {
+  case 'idDataServiceButton':
+    return this.openDataService();
+  case 'idReportingStudioButton':
+    return this.openRS();
+      case 'idTBExplorerButton':
+    return this.openTBExplorer();
+      case 'idTBExplorerButton':
+    return this.openTestGrid();
+      case 'idTestIconsButton':
+    return this.openTestIcons();
+
+  default:
+    break;
+}
+
+  }
+
 
   openDataService() {
     this.componentService.createComponentFromUrl('test/dataservice', true);
--- client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-user/topbar-menu-user.component.ts
+++ client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-user/topbar-menu-user.component.ts
@@ -1,5 +1,8 @@
 import { LoginSessionService } from './../../../../core/login-session.service';
 import { Component, OnInit } from '@angular/core';
+import { Collision } from '@progress/kendo-angular-popup/dist/es/models/collision.interface';
+import { Align } from '@progress/kendo-angular-popup/dist/es/models/align.interface';
+import { MenuItem } from './../../../context-menu/menu-item.model';
 
 @Component({
   selector: 'tb-topbar-menu-user',
@@ -7,26 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar-menu-user.component.css']
 })
 export class TopbarMenuUserComponent implements OnInit {
+ private title: string = "User menu";
+ anchorAlign: Align = { horizontal: 'right', vertical: 'bottom' };
+  popupAlign: Align = { horizontal: 'right', vertical: 'top' };
+  private collision: Collision = { horizontal: 'flip', vertical: 'fit' };
+  contextMenu: MenuItem[] = new Array<MenuItem>();
 
-  private title: string = "User menu";
-data: Array<any> = [{
-         actionName: 'Refresh',
-    }, {
-        actionName: 'Settings',
-    }, {
-        actionName: 'Help',
-    }, {
-        actionName: 'Sign Out',
-         click: (dataItem) => {
-           this.logout();
-        }
+  constructor(private loginSessionService: LoginSessionService) { 
+    const item1 = new MenuItem('Refresh', 'idRefreshButton', false, false);
+    const item2 = new MenuItem('Settings', 'idSettingsButton', false,  false);
+    const item3 = new MenuItem('Help', 'idHelpButton', false,  false);
+    const item4 = new MenuItem( 'Sign Out', 'idSignOutButton', false,  false);
+   this.contextMenu.push(item1, item2, item3, item4);
     }
-];
-
-
-  constructor(private loginSessionService: LoginSessionService) { }
 
   ngOnInit() {
+  }
+
+  chooseAction(buttonName: string){
+switch (buttonName) {
+  case 'idSignOutButton':
+    return this.logout();
+  // case 'idReportingStudioButton':
+  //   return this.openRS();
+  //     case 'idTBExplorerButton':
+  //   return this.openTBExplorer();
+  //     case 'idTBExplorerButton':
+  //   return this.openTestGrid();
+  //     case 'idTestIconsButton':
+  //   return this.openTestIcons();
+
+  default:
+    break;
+    }
   }
 
   logout() {
