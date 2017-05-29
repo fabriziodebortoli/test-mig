dd15945f7f77af752d073fa49843578c36974179
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-05-29 10:03:39 +0200

    refactor menu topbar scss

--- client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-test/topbar-menu-test.component.ts
+++ client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu-test/topbar-menu-test.component.ts
@@ -1,5 +1,5 @@
 import { text } from './../../../../reporting-studio/reporting-studio.model';
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Collision } from '@progress/kendo-angular-popup/dist/es/models/collision.interface';
 import { Align } from '@progress/kendo-angular-popup/dist/es/models/align.interface';
 import { MenuItem } from './../../../context-menu/menu-item.model';
@@ -8,45 +8,45 @@ import { ComponentService } from './../../../../core/component.service';
 @Component({
   selector: 'tb-topbar-menu-test',
   templateUrl: './topbar-menu-test.component.html',
-  styleUrls: ['./topbar-menu-test.component.css']
+  styleUrls: ['./topbar-menu-test.component.scss']
 })
 export class TopbarMenuTestComponent implements OnInit {
 
- anchorAlign: Align = { horizontal: 'right', vertical: 'bottom' };
+  anchorAlign: Align = { horizontal: 'right', vertical: 'bottom' };
   popupAlign: Align = { horizontal: 'right', vertical: 'top' };
   private collision: Collision = { horizontal: 'flip', vertical: 'fit' };
   contextMenu: MenuItem[] = new Array<MenuItem>();
-   private show = false;
+  private show = false;
   private title: string = "Test menu";
 
-  constructor(private componentService: ComponentService) { 
-  const item1 = new MenuItem('Data Service', 'idDataServiceButton', false, false);
-  const item2 = new MenuItem('Reporting Studio', 'idReportingStudioButton', false,  false);
-  const item3 = new MenuItem('TB Explorer', 'idTBExplorerButton', false,  false);
-  const item4 = new MenuItem( 'Test Grid Component', 'idTBExplorerButton', false,  false);
-  const item5 = new MenuItem('Test Icons', 'idTestIconsButton', false,  false);
- this.contextMenu.push(item1, item2, item3, item4, item5);
+  constructor(private componentService: ComponentService) {
+    const item1 = new MenuItem('Data Service', 'idDataServiceButton', false, false);
+    const item2 = new MenuItem('Reporting Studio', 'idReportingStudioButton', false, false);
+    const item3 = new MenuItem('TB Explorer', 'idTBExplorerButton', false, false);
+    const item4 = new MenuItem('Test Grid Component', 'idTBExplorerButton', false, false);
+    const item5 = new MenuItem('Test Icons', 'idTestIconsButton', false, false);
+    this.contextMenu.push(item1, item2, item3, item4, item5);
   }
 
   ngOnInit() {
   }
 
-  chooseAction(buttonName: string){
-switch (buttonName) {
-  case 'idDataServiceButton':
-    return this.openDataService();
-  case 'idReportingStudioButton':
-    return this.openRS();
+  chooseAction(buttonName: string) {
+    switch (buttonName) {
+      case 'idDataServiceButton':
+        return this.openDataService();
+      case 'idReportingStudioButton':
+        return this.openRS();
       case 'idTBExplorerButton':
-    return this.openTBExplorer();
+        return this.openTBExplorer();
       case 'idTBExplorerButton':
-    return this.openTestGrid();
+        return this.openTestGrid();
       case 'idTestIconsButton':
-    return this.openTestIcons();
+        return this.openTestIcons();
 
-  default:
-    break;
-}
+      default:
+        break;
+    }
 
   }
 
--- client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu.component.ts
+++ client/web-form/src/app/shared/topbar/topbar-menu/topbar-menu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'tb-topbar-menu',
   templateUrl: './topbar-menu.component.html',
-  styleUrls: ['./topbar-menu.component.css'],
+  styleUrls: ['./topbar-menu.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class TopbarMenuComponent implements OnInit {
