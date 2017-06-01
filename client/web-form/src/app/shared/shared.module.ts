64fbdd485c2b9ed41ecd0e15a389d4de34039973
simonapodesta <simona.podesta@microarea.it>
2017-06-01 17:42:08 +0200

    Creato il tb-card component

--- client/web-form/src/app/shared/shared.module.ts
+++ client/web-form/src/app/shared/shared.module.ts
@@ -60,6 +60,9 @@ import { MessageDialogComponent } from './containers/message-dialog/message-dial
 
 import { IconComponent } from './icon.component';
 import { TopbarMenuElementsComponent } from './topbar/topbar-menu/topbar-menu-element/topbar-menu-elements.component';
+import { TbCardComponent } from './controls/tb-card/tb-card.component';
+import { TbCardTitleComponent } from './controls/tb-card/tb-card-title/tb-card-title.component';
+import { TbCardContentComponent } from './controls/tb-card/tb-card-content/tb-card-content.component';
 
 const KENDO_UI_MODULES = [
   GridModule,
@@ -86,7 +89,7 @@ const TB_COMPONENTS = [
   PlaceholderComponent, PasswordComponent, MaskedTextBoxComponent, NumericTextBoxComponent, ContextMenuComponent, ImageComponent, ColorPickerComponent,
   BoolEditComponent, BodyEditComponent, LinkComponent, LinearGaugeComponent,
   PhoneComponent, EmailComponent, SectionTitleComponent, TextareaComponent, FileComponent, ComboSimpleComponent, MessageDialogComponent,
-  IconComponent
+  IconComponent, TopbarMenuElementsComponent, TbCardComponent, TbCardTitleComponent, TbCardContentComponent
 ];
 
 const TB_DIRECTIVES = [
@@ -106,7 +109,7 @@ const TB_DIRECTIVES = [
     RouterModule,
     KENDO_UI_MODULES
   ],
-  declarations: [TB_COMPONENTS, TB_DIRECTIVES, MessageDialogComponent, TopbarMenuElementsComponent],
+  declarations: [TB_COMPONENTS, TB_DIRECTIVES, MessageDialogComponent],
   exports: [TB_COMPONENTS, TB_DIRECTIVES],
   entryComponents: [OpenComponent, SaveComponent, ContextMenuComponent]
 
