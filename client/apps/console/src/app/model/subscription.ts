0645dc281099f7c826a44801cc8b4b3423edf7c2
mdelbene <michela.delbene@microarea.it>
2017-06-09 14:48:23 +0200

    Provisioning Front-end: model strutture dati

---
+++ client/apps/console/src/app/model/subscription.ts
@@ -0,0 +1,11 @@
+export class Subscription {
+    
+    subscriptionId: number;
+    name: string;
+    // ActivationToken // ??
+    purchaseId: string;
+    instanceId: number;
+
+    constructor() {
+    }
+}
