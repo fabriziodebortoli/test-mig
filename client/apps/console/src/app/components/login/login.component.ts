481547aa24bc9a005ff4f1842b0339c4bb2715f1
fraricce <fraricce@gmail.com>
2017-06-15 16:57:39 +0200

    Provisioning: login template

--- client/apps/console/src/app/components/login/login.component.ts
+++ client/apps/console/src/app/components/login/login.component.ts
@@ -14,6 +14,12 @@ export class LoginComponent implements OnInit {
     this.loginInfo = new LoginInfo();
   }
 
+  submitLogin() {
+    if (this.loginInfo.accountName == '' || this.loginInfo.password == '') {
+      return;
+    }
+  }
+
   ngOnInit() {
   }
 
