4fa13f13ff6457f7eaf316186d50a1f7089be93d
simonapodesta <simona.podesta@microarea.it>
2017-06-15 12:41:41 +0200

    Spostato tb-card component

--- client/web-form/src/app/shared/controls/body-edit/body-edit.component.ts
+++ client/web-form/src/app/shared/controls/body-edit/body-edit.component.ts
@@ -1,6 +1,15 @@
 import { ControlComponent } from './../control.component';
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+/*scommentare quanto ci sarà il binding on demand
+ import { Observable } from 'rxjs/Rx';
+ import { State } from '@progress/kendo-data-query';
+ import {
+     GridDataResult,
+     DataStateChangeEvent
+ } from '@progress/kendo-angular-grid';
+ */
+
 
 @Component({
   selector: 'tb-body-edit',
@@ -9,11 +18,38 @@ import { Subscription } from 'rxjs';
 })
 export class BodyEditComponent extends ControlComponent {
   @Input() columns: Array<any>;
+  private gridData: Array<any>;
+/*  private gridView: GridDataResult;*/
 
-public pageSize: number = 10;
-public skip: number = 0;
-
+/* SORTING
+private sort: SortDescriptor[] = [];
+    private gridView: GridDataResult;
+ */
   constructor() {
     super();
   }
+
+  /* SORTING
+  protected sortChange(sort: SortDescriptor[]): void {
+        this.sort = sort;
+        this.loadProducts();
+    }
+    */
+
+    /* BINDING
+    private loadProducts(): void {
+      this.gridData = this.gridData.slice(this.skip, this.skip + this.pageSize),
+          this.gridView = {
+            data: orderBy(this.gridData, this.sort),
+            total: this.gridData.length
+        };
+    }
+     */
+    /* PAGING
+    protected pageChange({ skip, take }: PageChangeEvent): void {
+        this.skip = skip;
+        this.pageSize = take;
+        this.loadProducts();
+    }
+    */
 }
