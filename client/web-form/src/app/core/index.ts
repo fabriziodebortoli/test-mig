1d1388896f2f439d39d8d2283fbdf85e453262ea
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-06-26 09:13:09 +0200

    core: OperationResult model

--- client/web-form/src/app/core/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './operation.result';
-export * from './eventdata.service';
-export * from './core.guard';
-export * from './data.service';
-export * from './explorer.service';
-
-export * from './core.module';
