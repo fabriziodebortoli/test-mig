330225f350cf0bfa2fdfac63ef34a34779f78918
marco.perasso <marco.perasso@gmail.com>
2017-07-10 11:08:58 +0200

    uso una promise per gestire il fatto che la rundocument potrebbe fallire la chiamata

--- client/core/src/core/services/websocket.service.ts
+++ client/core/src/core/services/websocket.service.ts
@@ -63,7 +63,7 @@ export class WebSocketService {
         this.logger.debug('wsConnecting... ' + url);
 
         this.connection = new WebSocket(url);
-        this.connection.onmessage = function(e) {
+        this.connection.onmessage = function (e) {
             if (typeof (e.data) === 'string') {
                 try {
                     const obj = JSON.parse(e.data);
@@ -81,7 +81,7 @@ export class WebSocketService {
                         case 'SetServerWebSocketName': $this.connection.send(JSON.stringify({ cmd: 'getOpenDocuments' })); break;
                         case 'ButtonsState': $this.buttonsState.emit(obj.args); break;
                         case 'RadarQuery': $this.radarQuery.emit(obj.args); break;
-                        
+
                         default: break;
                     }
 
@@ -143,24 +143,27 @@ export class WebSocketService {
             }
         });
     }
-    safeSend(data: any) {
-        const subs = this.checkForOpenConnection().subscribe(valid => {
-            if (subs) {
-                subs.unsubscribe();
-            }
-            if (valid) {
-                this.connection.send(JSON.stringify(data));
-            }
-            else {
-                this.logger.info("Cannot use web socket, perhaps it is connecting");
-            }
+    safeSend(data: any) : Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const subs = this.checkForOpenConnection().subscribe(valid => {
+                if (subs) {
+                    subs.unsubscribe();
+                }
+                if (valid) {
+                    this.connection.send(JSON.stringify(data));
+                    resolve();
+                }
+                else {
+                    this.logger.info("Cannot use web socket, perhaps it is connecting");
+                    reject();
+                }
+            });
 
         });
-
     }
-    runDocument(ns: String, args: string = ''): void {
+    runDocument(ns: String, args: string = ''): Promise<void> {
         const data = { cmd: 'runDocument', ns: ns, sKeyArgs: args };
-        this.safeSend(data);
+        return this.safeSend(data);
     }
 
     doFillListBox(cmpId: String, obj: any): void {
@@ -181,12 +184,12 @@ export class WebSocketService {
         const data = { cmd: 'doValueChanged', cmpId: cmpId, id: id, model: modelData };
         this.safeSend(data);
     }
- 
+
     getRadarQuery(cmpId: String) {
         const data = { cmd: 'getRadarQuery', cmpId: cmpId };
         this.safeSend(data);
     }
-    
+
 
     /* doValueChanged(cmpId: String, id: String, modelData?: any): void {
          const data = { cmd: 'doValueChanged', cmpId: cmpId, id: id, model: modelData };
