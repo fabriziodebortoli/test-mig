521eb7ec7b9d876459efde602d5a04c3056e76ef
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-07-11 15:16:22 +0200

    core v0.2.1

--- client/core/src/core/core.module.ts
+++ client/core/src/core/core.module.ts
@@ -1,37 +1,53 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+
 import 'hammerjs';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 
 /**
  * Servizi
  * 
  * Tutti i servizi condivisi TB (http, websocket, 
  */
-import {
-    BOService, BOHelperService, ComponentService, DocumentService, DataService, EnumsService,
-    EventDataService, ExplorerService, HttpService, InfoService, LayoutService, Logger,
-    LoginSessionService, SidenavService, TabberService, UrlService, UtilsService, WebSocketService
-} from './services';
+import { BOService } from './services/bo.service';
+import { BOClient } from './services/bo.service';
+import { BOHelperService } from './services/bohelper.service';
+import { ComponentService } from './services/component.service';
+import { DataService } from './services/data.service';
+import { DocumentService } from './services/document.service';
+import { EnumsService } from './services/enums.service';
+import { EventDataService } from './services/eventdata.service';
+import { ExplorerService } from './services/explorer.service';
+import { HttpService } from './services/http.service';
+import { InfoService } from './services/info.service';
+import { LayoutService } from './services/layout.service';
+import { Logger } from './services/logger.service';
+import { LoginSessionService } from './services/login-session.service';
+import { SidenavService } from './services/sidenav.service';
+import { TabberService } from './services/tabber.service';
+import { UrlService } from './services/url.service';
+import { UtilsService } from './services/utils.service';
+import { WebSocketService } from './services/websocket.service';
 
-import { CookieService } from 'angular2-cookie/services/cookies.service';
-
-export * from './services/bo.service';
-export * from './services/bohelper.service';
-export * from './services/component.service';
-export * from './services/data.service';
-export * from './services/document.service';
-export * from './services/enums.service';
-export * from './services/eventdata.service';
-export * from './services/explorer.service';
-export * from './services/http.service';
-export * from './services/info.service';
-export * from './services/layout.service';
-export * from './services/logger.service';
-export * from './services/login-session.service';
-export * from './services/sidenav.service';
-export * from './services/tabber.service';
-export * from './services/url.service';
-export * from './services/utils.service';
-export * from './services/websocket.service';
+export { BOService } from './services/bo.service';
+export { BOClient } from './services/bo.service';
+export { BOHelperService } from './services/bohelper.service';
+export { ComponentService } from './services/component.service';
+export { DataService } from './services/data.service';
+export { DocumentService } from './services/document.service';
+export { EnumsService } from './services/enums.service';
+export { EventDataService } from './services/eventdata.service';
+export { ExplorerService } from './services/explorer.service';
+export { HttpService } from './services/http.service';
+export { InfoService } from './services/info.service';
+export { LayoutService } from './services/layout.service';
+export { Logger } from './services/logger.service';
+export { LoginSessionService } from './services/login-session.service';
+export { SidenavService } from './services/sidenav.service';
+export { TabberService } from './services/tabber.service';
+export { UrlService } from './services/url.service';
+export { UtilsService } from './services/utils.service';
+export { WebSocketService } from './services/websocket.service';
 
 export const TB_SERVICES = [
     BOService, BOHelperService, ComponentService, DocumentService, DataService, EnumsService,
@@ -39,8 +55,8 @@ export const TB_SERVICES = [
     LoginSessionService, SidenavService, TabberService, UrlService, UtilsService, WebSocketService
 ];
 
-import { CoreGuard } from './guards';
-export * from './guards';
+import { CoreGuard } from './guards/core.guard';
+export { CoreGuard } from './guards/core.guard';
 export const TB_GUARDS = [CoreGuard];
 
 @NgModule({
