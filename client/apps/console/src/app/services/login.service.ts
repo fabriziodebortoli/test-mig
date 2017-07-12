922d1d3798a0a7b0881ae7154095742c6569b86d
fraricce <fraricce@gmail.com>
2017-07-12 09:19:59 +0200

    Provisioning: FrontEnd: using new login helpers

--- client/apps/console/src/app/services/login.service.ts
+++ client/apps/console/src/app/services/login.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Credentials } from './../components/login/credentials';
+import { Credentials } from './../authentication/credentials';
 import { Observable } from "rxjs/Observable";
 import { OperationResult } from './operationResult';
+import { AuthInfo } from "app/authentication/auth-info";
 
 @Injectable()
 export class LoginService {
@@ -28,7 +29,49 @@ export class LoginService {
       data =>
       {
         if (data.Result)
-          localStorage.setItem('jwt-token', data.jwttoken)
+        {
+          if (data.JwtToken == '')
+          {
+            alert('Empty token');
+            return;
+          }
+
+          try
+          {
+            let tokenParts: Array<string> = data.JwtToken.split('.');
+
+            if (tokenParts.length != 3)
+            {
+              alert('Invalid token');
+              return;
+            }  
+
+            let decodedToken: string = atob(tokenParts[1]);
+            let parsedToken = JSON.parse(decodedToken);
+            let authInfo: AuthInfo = new AuthInfo(data.JwtToken,
+              parsedToken.AccountName);
+            authInfo.SetSubscriptions(parsedToken.Subscriptions);
+            authInfo.SetServerUrls(parsedToken.Urls);
+            authInfo.SetTokens(parsedToken.UserTokens);
+
+            // creating roles array;; waiting for roles object migration
+            let roles: Array<string> = new Array<string>();
+            if (parsedToken.ProvisioningAdmin) {
+              roles.push("ProvisioningAdmin");
+            }
+            if (parsedToken.CloudAdmin) {
+              roles.push("CloudAdmin");
+            }            
+            authInfo.SetRoles(roles);
+            
+            localStorage.setItem('auth-info', JSON.stringify(authInfo));
+          }
+          catch (exc)
+          {
+              alert('Error while decoding jwt token ' + exc + '. Login failed.');
+              return;
+          }
+        }
         else
           alert('Cannot do the login ' + data.Message);  
       },
