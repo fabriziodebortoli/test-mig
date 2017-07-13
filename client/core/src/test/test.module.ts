a4a3e4eef9067b9c0d9da9e00e00b9fecbc946f3
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-07-13 12:58:37 +0200

    core test module grid

--- client/core/src/test/test.module.ts
+++ client/core/src/test/test.module.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
 
 import { DataServiceComponent, DataServiceFactoryComponent } from './data-service/data-service.component';
 // import { ExplorerComponent, ExplorerFactoryComponent } from './explorer/explorer.component';
-// import { GridTestComponent, GridTestFactoryComponent } from './grid-test/grid-test.component';
+import { GridTestComponent, GridTestFactoryComponent } from './grid-test/grid-test.component';
 // import { IconsTestComponent, IconsTestFactoryComponent } from './icons-test/icons-test.component';
 
 
@@ -19,7 +19,7 @@ import { DataServiceComponent, DataServiceFactoryComponent } from './data-servic
         RouterModule.forChild([
             { path: 'dataservice', component: DataServiceFactoryComponent },
             // { path: 'explorer', component: ExplorerFactoryComponent },
-            // { path: 'grid', component: GridTestFactoryComponent },
+            { path: 'grid', component: GridTestFactoryComponent },
             // { path: 'icons', component: IconsTestFactoryComponent },
         ]),
         // IconsModule
@@ -27,9 +27,9 @@ import { DataServiceComponent, DataServiceFactoryComponent } from './data-servic
     declarations: [
         DataServiceComponent, DataServiceFactoryComponent,
         // ExplorerComponent, ExplorerFactoryComponent,
-        // GridTestComponent, GridTestFactoryComponent,
+        GridTestComponent, GridTestFactoryComponent,
         // IconsTestComponent, IconsTestFactoryComponent
     ],
-    entryComponents: [DataServiceComponent, /*ExplorerComponent, GridTestComponent/*, IconsTestComponent*/]
+    entryComponents: [DataServiceComponent, GridTestComponent, /*ExplorerComponent/*, IconsTestComponent*/]
 })
 export class TbTestModule { }
