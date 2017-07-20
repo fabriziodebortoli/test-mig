b18ef5b205c4abec44a328910cfe6f658bd37d91
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-07-20 15:23:23 +0200

    Primo collegamento tra radar e dataservice (riccardo facci sognare)

--- client/core/src/shared/components/radar/radar.component.ts
+++ client/core/src/shared/components/radar/radar.component.ts
@@ -7,6 +7,21 @@ import { URLSearchParams } from '@angular/http';
 import { NgForm } from "@angular/forms";
 import { animate, transition, trigger, state, style, keyframes, group } from "@angular/animations";
 
+export const sampleProducts = [
+    { "ProductID": 1, "ProductName": "Chai", "QuantityPerUnit": "10 boxes x 20 bags", },
+    { "ProductID": 2, "ProductName": "Chang", "QuantityPerUnit": "24 - 12 oz bottles", },
+    { "ProductID": 3, "ProductName": "Aniseed Syrup", "QuantityPerUnit": "12 - 550 ml bottles", },
+    { "ProductID": 1, "ProductName": "Chai", "QuantityPerUnit": "10 boxes x 20 bags", },
+    { "ProductID": 2, "ProductName": "Chang", "QuantityPerUnit": "24 - 12 oz bottles", },
+    { "ProductID": 3, "ProductName": "Aniseed Syrup", "QuantityPerUnit": "12 - 550 ml bottles", },
+    { "ProductID": 1, "ProductName": "Chai", "QuantityPerUnit": "10 boxes x 20 bags", },
+    { "ProductID": 2, "ProductName": "Chang", "QuantityPerUnit": "24 - 12 oz bottles", },
+    { "ProductID": 3, "ProductName": "Aniseed Syrup", "QuantityPerUnit": "12 - 550 ml bottles", },
+    { "ProductID": 1, "ProductName": "Chai", "QuantityPerUnit": "10 boxes x 20 bags", },
+    { "ProductID": 2, "ProductName": "Chang", "QuantityPerUnit": "24 - 12 oz bottles", },
+    { "ProductID": 3, "ProductName": "Aniseed Syrup", "QuantityPerUnit": "12 - 550 ml bottles", }
+];
+
 @Component({
     selector: 'tb-radar',
     templateUrl: './radar.component.html',
@@ -21,36 +36,33 @@ import { animate, transition, trigger, state, style, keyframes, group } from "@a
 })
 export class RadarComponent {
 
-    public radarData: any[] = [];
     public state: string = 'closed';
-    private query: string;
 
-    public columns: string[] = [
-        "CompanyName", "ContactName", "ContactTitle"
+    public radarData: any[] = sampleProducts;
+
+    public radarColumns: string[] = [
+        "ProductID", "ProductName", "QuantityPerUnit"
     ];
 
-    constructor(private dataService: DataService, private logger: Logger) {
-        this.query = 'select * from belincheneso';
-        //this.radarData = [{ "Id": "ALFKI", "CompanyName": "Alfreds Futterkiste", "ContactName": "Maria Anders", "ContactTitle": "Sales Representative", "City": "Berlin" }, { "Id": "ANATR", "CompanyName": "Ana Trujillo Emparedados y helados", "ContactName": "Ana Trujillo", "ContactTitle": "Owner", "City": "México D.F." }];
-    }
+    constructor(private dataService: DataService, private logger: Logger) { }
 
     toggle() {
         this.state = this.state === 'opened' ? 'closed' : 'opened';
     }
 
-    onSubmit(form: NgForm) {
-        console.log('submitted', form.value);
-    }
-
     init(radarInfo) {
-        this.toggle();
-        if (this.state === 'closed') return;
+        this.logger.info('radarInfo', radarInfo);
+
+        this.toggle(); if (this.state === 'closed') return;
 
         let params: URLSearchParams = new URLSearchParams();
         params.set('query', radarInfo.query);
+        params.set('columnInfos', radarInfo.columnInfos);
 
         this.logger.debug('radar', params);
         this.dataService.getRadarData(params).subscribe((data) => {
+            console.log(data)
+
             this.radarData = data;
         });
     }
