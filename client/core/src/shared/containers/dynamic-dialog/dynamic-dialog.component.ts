a0292215695ca71cc53713edc0169a46d637b7d4
Marco Perasso <marco.perasso@gmail.com>
2017-07-20 17:37:00 +0200

    chiusura dialog modale

--- client/core/src/shared/containers/dynamic-dialog/dynamic-dialog.component.ts
+++ client/core/src/shared/containers/dynamic-dialog/dynamic-dialog.component.ts
@@ -1,30 +1,41 @@
+import { WebSocketService } from './../../../core/services/websocket.service';
 import { EventDataService } from './../../../core/services/eventdata.service';
 import { DocumentComponent } from './../../components/document.component';
 import { DocumentService } from './../../../core/services/document.service';
 import { ComponentInfo } from './../../models/component-info.model';
-import { Component, OnInit, Type, Input, ViewChild, ViewContainerRef, ComponentRef, OnDestroy  } from '@angular/core';
+import { Component, OnInit, Type, Input, ViewChild, ViewContainerRef, ComponentRef, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'tb-dynamic-dialog',
     templateUrl: './dynamic-dialog.component.html',
-    styleUrls: ['./dynamic-dialog.component.scss'] 
+    styleUrls: ['./dynamic-dialog.component.scss']
 })
-export class DynamicDialogComponent { 
+export class DynamicDialogComponent implements OnDestroy {
 
     opened = false;
     componentInfo: ComponentInfo;
-    
+    subscriptions = [];
 
-    constructor() { }
+    constructor(private webSocketService: WebSocketService) {
+
+
+    }
 
     open(componentInfo: ComponentInfo) {
         this.componentInfo = componentInfo;
         this.opened = true;
+
+        this.subscriptions.push(this.webSocketService.windowClose.subscribe(data => {
+            if (this.componentInfo && data && data.id && data.id === this.componentInfo.id) {
+                this.opened = false;
+                this.componentInfo = null;
+                this.subscriptions.forEach(subs => subs.unsubscribe());
+            }
+        }));
     }
 
     close() {
-        this.opened = false;
-        this.componentInfo = null;
+        this.webSocketService.doClose(this.componentInfo.id);
     }
-
+    
 }
