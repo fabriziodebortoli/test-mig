5cbccfdb48847d1ddd44720c66935fd5ab564be1
Marco Perasso <marco.perasso@gmail.com>
2017-07-20 15:58:00 +0200

    fix apertura dialog dinamica

--- client/core/src/shared/containers/dynamic-dialog/dynamic-dialog.component.ts
+++ client/core/src/shared/containers/dynamic-dialog/dynamic-dialog.component.ts
@@ -10,43 +10,20 @@ import { Component, OnInit, Type, Input, ViewChild, ViewContainerRef, ComponentR
     styleUrls: ['./dynamic-dialog.component.scss'] 
 })
 export class DynamicDialogComponent { 
-    cmpRef: ComponentRef<DocumentComponent>;
 
     opened = false;
     componentInfo: ComponentInfo;
-    document: DocumentService; 
-    eventData: EventDataService;
-    cmpContainer: ViewContainerRef;
-
-    @ViewChild('cmpContainer', { read: ViewContainerRef }) set content(content: ViewContainerRef) {
-        this.cmpContainer = content;
-        if (this.cmpContainer) {
-            const me = this;
-             setTimeout(function () {
-                 //TODO fare nella ngAfterViewChecked, ma bisogna updradare angular
-                 me.cmpRef = me.cmpContainer.createComponent(me.componentInfo.factory);
-                 me.cmpRef.instance.cmpId = me.componentInfo.id; //assegno l'id al componente
- 
-                 //documento ed eventi sono condivisi col componente master
-                 me.cmpRef.instance.document = me.document;
-                 me.cmpRef.instance.eventData = me.eventData;
-             }, 1);
-        }
-    }
+    
 
     constructor() { }
 
-    open(componentInfo: ComponentInfo, document: DocumentService, eventData: EventDataService) {
+    open(componentInfo: ComponentInfo) {
         this.componentInfo = componentInfo;
-        this.document = document;
-        this.eventData = eventData;
-
         this.opened = true;
     }
 
     close() {
         this.opened = false;
-        this.cmpRef = null;
         this.componentInfo = null;
     }
 
