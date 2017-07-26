5b8aa2cc1ab6c557d06f8ede1c644ab58d84197f
Francesco Ricceri <fraricce@gmail.com>
2017-07-26 14:57:35 +0200

    Provisioning: FrontEnd: AccountsHome now reads parentAccount localStorage

--- client/apps/console/src/app/components/accounts-home/accounts-home.component.ts
+++ client/apps/console/src/app/components/accounts-home/accounts-home.component.ts
@@ -1,6 +1,7 @@
 import { ModelService } from '../../services/model.service';
 import { Account } from '../../model/account';
 import { Component, OnInit } from '@angular/core';
+import { AuthorizationProperties } from "app/authentication/auth-info";
 
 
 @Component({
@@ -15,7 +16,17 @@ export class AccountsHomeComponent implements OnInit {
   constructor(private modelService: ModelService) {}
 
   ngOnInit() {
-    this.modelService.getAccounts({ parentAccount: 'fricceri@m4.com'})
+
+    let authorizationStored = localStorage.getItem('auth-info');
+
+    if (authorizationStored === null) {
+      alert('User must be logged in.');
+      return;
+    }
+
+    let authorizationProperties: AuthorizationProperties = JSON.parse(authorizationStored);
+
+    this.modelService.getAccounts({ parentAccount: authorizationProperties.accountName })
       .subscribe(
         accounts => {
           this.accounts = accounts['Content'];
