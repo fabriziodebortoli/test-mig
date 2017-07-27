3ef9c9cfedec82caae63a9cebabf2ea209f49839
Francesco Ricceri <fraricce@gmail.com>
2017-07-27 15:05:18 +0200

    Provisioning: FrontEnd: AdminSideNavContainer: appTitle
    extracted as @Input

--- client/console/src/components/admin-sidenav-container/admin-sidenav-container.component.ts
+++ client/console/src/components/admin-sidenav-container/admin-sidenav-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'admin-sidenav-container',
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSidenavContainerComponent implements OnInit {
 
-    title:string = 'M4 Cloud Provisioning';
+    @Input() appTitle:string = '';
 
     constructor() { }
 
     ngOnInit() {
+      console.log(this.appTitle);
     }
 
 }
