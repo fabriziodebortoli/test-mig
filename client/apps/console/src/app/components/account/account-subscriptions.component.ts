df52af295e08128be79861e65da65f63bf5b23a1
Francesco Ricceri <fraricce@gmail.com>
2017-08-01 11:23:12 +0200

    Provisioning: FrontEnd: added account-subscription

---
+++ client/apps/console/src/app/components/account/account-subscriptions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-subscriptions',
+  templateUrl: './account-subscriptions.component.html',
+  styleUrls: ['./account-subscriptions.component.css']
+})
+export class AccountSubscriptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
