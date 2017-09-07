f153dc44fdb3af265ea7726fd511704200595096
fabriziodebortoli <fabrizio.debortoli@microarea.it>
2017-09-07 09:17:47 +0200

    app upd

--- client/web-form/src/app/shared/shared.module.ts
+++ client/web-form/src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TaskbuilderCoreModule } from '@taskbuilder/core';
+import { TbIconsModule } from '@taskbuilder/icons';
 
 @NgModule({
     imports: [
         CommonModule,
-        TaskbuilderCoreModule
+        TaskbuilderCoreModule,
+        TbIconsModule
     ],
     exports: [TaskbuilderCoreModule]
 })
