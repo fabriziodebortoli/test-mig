5b1cdcfde7ed508c7263ac240fb7872f756148fb
HolaChica <anastasia.sarmant@hotmail.com>
2017-09-08 16:44:09 +0200

    chart type : Bar, Area, Line and variations full

--- client/reporting-studio/src/report-objects/layout/layout.component.ts
+++ client/reporting-studio/src/report-objects/layout/layout.component.ts
@@ -205,7 +205,7 @@ export class ReportLayoutComponent implements OnChanges, OnInit, OnDestroy {
           obj.value = value;
         }
         else if (element.chart !== undefined) {
-          id = element.chart.baseobj.id;
+          id = element.chart.baserect.baseobj.id;
           let obj = this.FindObj(id);
           element.chart.series.forEach(element => {
             obj.series.push(new series(element));
