9f00498da57151b725a61ae46f5b65937febd161
SilvanoParodi-Microarea <silvano.parodi@microarea.it>
2017-09-15 12:03:51 +0200

    RsWeb - Run/stop button enabling, reRun button removed

--- client/reporting-studio/src/reporting-studio.service.ts
+++ client/reporting-studio/src/reporting-studio.service.ts
@@ -20,6 +20,7 @@ export class ReportingStudioService extends DocumentService {
     [x: string]: any;
     public pageNum: number = 1;
     public running: boolean = false;
+    public runEnabled: boolean = true;
     public showAsk = false;
 
     private rsServer: string = ''
