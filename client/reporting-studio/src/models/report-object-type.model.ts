33ebfa291e08c8661a6a104982cafdddea062330
HolaChica <anastasia.sarmant@hotmail.com>
2017-09-19 12:45:35 +0200

    enum layout object type in htmk

--- client/reporting-studio/src/models/report-object-type.model.ts
+++ client/reporting-studio/src/models/report-object-type.model.ts
@@ -1,1 +1,5 @@
 export enum ReportObjectType { textrect, fieldrect, table, graphrect, sqrrect, chart, repeater, cell, link}
+
+export function ReportObjectTypeDecorator(constructor: Function) {
+    constructor.prototype.ReportObjectType = ReportObjectType;
+}
