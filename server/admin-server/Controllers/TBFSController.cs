e2a1077bea1aa6b0a0cb5b6312462a92049ebb29
Francesco Ricceri <fraricce@gmail.com>
2017-10-03 11:42:32 +0200

    TBFS: BackEnd: basic controller skeleton

---
+++ server/admin-server/Controllers/TBFSController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microarea.AdminServer.Controllers.Helpers;
+using Microsoft.AspNetCore.Hosting;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Microarea.AdminServer.Controllers
+{
+    public class TBFSController : Controller
+    {
+		private IHostingEnvironment _env;
+		IJsonHelper jsonHelper;
+
+		//-----------------------------------------------------------------------------	
+		public TBFSController(IHostingEnvironment env, IJsonHelper jsonHelper)
+		{
+			_env = env;
+			this.jsonHelper = jsonHelper;
+		}
+
+		[HttpPost("api/tbfs/init")]
+		//-----------------------------------------------------------------------------	
+		public async Task<IActionResult> ApiTBFSInit(string instanceKey, string path)
+		{
+			jsonHelper.AddJsonCouple<string>("message", "Welcome to Microarea Admin-Server API");
+			return new ContentResult { Content = jsonHelper.WriteFromKeysAndClear(), ContentType = "application/json" };
+		}
+	}
+}
