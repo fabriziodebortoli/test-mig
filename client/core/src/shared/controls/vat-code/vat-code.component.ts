aac33111ad5c6e7b825eead68045ba0040671f6a
fabrizio.debortoli@microarea.it <fabrizio.debortoli@microarea.it>
2017-10-03 11:44:33 +0200

    validazione VAT Code by TS

--- client/core/src/shared/controls/vat-code/vat-code.component.ts
+++ client/core/src/shared/controls/vat-code/vat-code.component.ts
@@ -1,3 +1,7 @@
+import { EventDataService } from './../../../core/services/eventdata.service';
+import { TbComponentService } from './../../../core/services/tbcomponent.service';
+import { LayoutService } from './../../../core/services/layout.service';
+import { Logger } from './../../../core/services/logger.service';
 import { Component, Input } from '@angular/core';
 
 import { ControlComponent } from './../control.component';
@@ -8,7 +12,36 @@ import { ControlComponent } from './../control.component';
     styleUrls: ['./vat-code.component.scss']
 })
 export class VATCodeComponent extends ControlComponent {
-    @Input() forCmpID: string;
     @Input() disabled: boolean;
     @Input() mask: string;
+
+    @Input() params: string;
+
+    errorMessage: string;
+
+    constructor(
+        private logger: Logger,
+        public layoutService: LayoutService,
+        public tbComponentService: TbComponentService,
+        public eventDataService: EventDataService
+    ) {
+        super(layoutService, tbComponentService);
+    }
+
+    onBlur(change) {
+        this.logger.log(change.target.value);
+        this.isValid(change.target.value);
+    }
+
+    isValid(fiscalCode: string) {
+        let p = this.params.split(',');
+
+        this.logger.log("p", p)
+        this.logger.log("model", this.eventDataService.model);
+        this.logger.log("model", this.eventDataService.model["Contacts"]["FiscalCode"].value);
+
+        // logica di validazione
+        this.errorMessage = "Uffa!";
+    }
+
 }
