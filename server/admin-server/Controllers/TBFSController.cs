69fe13b70b1e3013059e5e676ee8f9e3953f97a9
laragrillo <lara.grillo@microarea.it>
2017-10-04 15:39:25 +0200

    provisioning 6756 back end Inserimento in db da APi

--- server/admin-server/Controllers/TBFSController.cs
+++ server/admin-server/Controllers/TBFSController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microarea.AdminServer.Controllers.Helpers;
 using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using Microarea.Common.NameSolver;
+using Microarea.Common;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,5 +32,17 @@ namespace Microarea.AdminServer.Controllers
 			jsonHelper.AddJsonCouple<string>("message", "Welcome to Microarea Admin-Server API");
 			return new ContentResult { Content = jsonHelper.WriteFromKeysAndClear(), ContentType = "application/json" };
 		}
-	}
+
+        [HttpPost("api/tbfs/Create")]
+        //-----------------------------------------------------------------------------	
+        public async Task<IActionResult> ApiTBFSCreate()
+        {
+           MetaDataManagerTool metadata = new MetaDataManagerTool("I-M4");
+            metadata.InsertAllStandardMetaDataInDB();
+
+            jsonHelper.AddJsonCouple<string>("message", "Welcome to Microarea Admin-Server API");
+            return new ContentResult { Content = jsonHelper.WriteFromKeysAndClear(), ContentType = "application/json" };
+        }
+
+    }
 }
