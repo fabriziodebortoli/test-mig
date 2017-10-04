cf83a8bbd354a353e4e77d1dfdb866c48e14f9df
Francesco Ricceri <fraricce@gmail.com>
2017-10-04 11:58:43 +0200

    Provisioning: BackEnd: TBFS Controller (empty)

--- server/admin-server/Controllers/TBFSController.cs
+++ server/admin-server/Controllers/TBFSController.cs
@@ -5,12 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microarea.AdminServer.Controllers.Helpers;
 using Microsoft.AspNetCore.Hosting;
+using System.Runtime.Serialization;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Microarea.AdminServer.Controllers
 {
-    public class TBFSController : Controller
+	[Consumes("application/json", otherContentTypes: "multipart/form-data")]
+	public class TBFSController : Controller
     {
 		private IHostingEnvironment _env;
 		IJsonHelper jsonHelper;
@@ -24,10 +29,39 @@ namespace Microarea.AdminServer.Controllers
 
 		[HttpPost("api/tbfs/init")]
 		//-----------------------------------------------------------------------------	
-		public async Task<IActionResult> ApiTBFSInit(string instanceKey, string path)
+		public async Task<IActionResult> ApiTBFSInit(IFormFile file)
 		{
-			jsonHelper.AddJsonCouple<string>("message", "Welcome to Microarea Admin-Server API");
-			return new ContentResult { Content = jsonHelper.WriteFromKeysAndClear(), ContentType = "application/json" };
+			if (file == null || file.Length == 0)
+				return Content("file not selected");
+
+			string uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\uploads");
+
+			try
+			{
+				if (!Directory.Exists(uploadFolder))
+				{
+					Directory.CreateDirectory(uploadFolder);
+				}
+
+				var path = Path.Combine(
+							Directory.GetCurrentDirectory(), "wwwroot\\uploads",
+							file.FileName);
+
+				using (var stream = new FileStream(path, FileMode.Create))
+				{
+					await file.CopyToAsync(stream);
+				}
+			}
+			catch (Exception e)
+			{
+				this.jsonHelper.AddJsonCouple<bool>("result", false);
+				return new ContentResult { Content = jsonHelper.WriteFromKeysAndClear(), StatusCode = 500, ContentType = "application/json" };
+			}
+
+			this.jsonHelper.AddJsonCouple<bool>("result", true);
+			this.jsonHelper.AddJsonCouple<string>("message", String.Format("File {0} has been uploaded", file.FileName));
+			return new ContentResult { Content = jsonHelper.WriteFromKeysAndClear(), StatusCode = 200, ContentType = "application/json" };
 		}
 	}
+
 }
