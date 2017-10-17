563e8defc9ba490f027cb6d9ae28d3f4cf5ae7d8
fabrizio.debortoli@microarea.it <fabrizio.debortoli@microarea.it>
2017-10-17 11:34:44 +0200

    erp library - rimossi kendo e aggiunte forms

--- client/erp/src/shared/shared.module.ts
+++ client/erp/src/shared/shared.module.ts
@@ -1,27 +1,6 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { TaskbuilderCoreModule } from '@taskbuilder/core';
-
-import { DialogModule } from '@progress/kendo-angular-dialog';
-import { LayoutModule } from '@progress/kendo-angular-layout';
-import { PopupModule } from '@progress/kendo-angular-popup';
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { InputsModule } from '@progress/kendo-angular-inputs';
-import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { GridModule, GridComponent } from '@progress/kendo-angular-grid';
-import { ChartsModule } from '@progress/kendo-angular-charts';
-
-const KENDO_UI_MODULES = [
-    GridModule,
-    ChartsModule,
-    DialogModule,
-    DateInputsModule,
-    DropDownsModule,
-    InputsModule,
-    LayoutModule,
-    PopupModule,
-    ButtonsModule
-];
 
 import { NoSpacesEditComponent } from './controls/no-spaces/no-spaces.component';
 import { VatComponent } from './controls/vat/vat.component';
@@ -32,7 +11,7 @@ export { VatComponent } from './controls/vat/vat.component';
 const ERP_COMPONENTS = [NoSpacesEditComponent, VatComponent];
 
 @NgModule({
-    imports: [TaskbuilderCoreModule, KENDO_UI_MODULES],
+    imports: [FormsModule, TaskbuilderCoreModule],
     declarations: [ERP_COMPONENTS],
     exports: [ERP_COMPONENTS]
 })
