94a6058843199a5d0dede15eb87b3050704232b4
Francesco Ricceri <fraricce@gmail.com>
2017-10-17 12:13:56 +0200

    Provisioning: FrontEnd: AdminCheckBox added id and readonly

--- client/console/src/components/admin-checkbox/admin-checkbox.component.ts
+++ client/console/src/components/admin-checkbox/admin-checkbox.component.ts
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class AdminCheckBoxComponent {
 
+  @Input() checkId: string;
   @Input() checkText: string;
   @Input() checkValue: boolean;
   @Input() readOnly: boolean;
