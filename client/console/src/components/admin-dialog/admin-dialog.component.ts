5b1e0f2482f00f066be3278688ad77346917cf29
Francesco Ricceri <fraricce@gmail.com>
2017-10-19 16:40:59 +0200

    Provisioning: FrontEnd: Library: added AdminDialogComponent

--- client/console/src/components/admin-dialog/admin-dialog.component.ts
+++ client/console/src/components/admin-dialog/admin-dialog.component.ts
@@ -8,6 +8,21 @@ import { FormsModule } from '@angular/forms';
 })
 export class AdminDialogComponent {
 
+  @Input() dialogTitle: string;
+  @Input() status: boolean;
+
+  public opened: boolean;
+
   constructor() { 
+    this.dialogTitle = '';
+    this.opened = false;
+  }
+
+  public close(status) {
+    this.opened = false;
+  }
+
+  ngOnChange() {
+    
   }
 }
