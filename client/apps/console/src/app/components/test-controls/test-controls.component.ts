5cca73aa2d68d3754e992458bc24522bf7188a27
Francesco Ricceri <fraricce@gmail.com>
2017-10-19 15:48:43 +0200

    Provisioning: FrontEnd: Controls Test Page: AdminDropDown

--- client/apps/console/src/app/components/test-controls/test-controls.component.ts
+++ client/apps/console/src/app/components/test-controls/test-controls.component.ts
@@ -7,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestControlsComponent implements OnInit {
 
-  // used for checkbox
+  //--------------------------------------------------------------------------------
+  // checkbox auxiliary variables
+
   chkTest: boolean;
+
+  //--------------------------------------------------------------------------------
+  // dropdown auxiliary variables
+
+  artWorks: Array<{name:string, value:string}> = [
+    { name: 'Monna Lisa', value:'leo-monna' },
+    { name: 'The Virgin of The Rocks', value:'leo-vrgocks' },
+    { name: 'Lady with an Ermine', value:'leo-ladyermine' },
+    { name: 'The Last Supper', value:'leo-lastsupp' }
+  ];
+
+  selectedItem: string = 'leo-monna';
 
   constructor() { 
     this.chkTest = false;
