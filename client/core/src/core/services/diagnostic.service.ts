fa6a1a2a5d5a9c573173624344fbf948bbe3d3fb
fabrizio.debortoli@microarea.it <fabrizio.debortoli@microarea.it>
2017-10-23 09:46:14 +0200

    dialog diagnostic sync

--- client/core/src/core/services/diagnostic.service.ts
+++ client/core/src/core/services/diagnostic.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from '../../rxjs.imports';
 
 @Injectable()
 export class DiagnosticService {
 
     messages = [];
     isVisible: boolean = false;
-    constructor() {
 
-    }
+    observer: Subject<any>;
 
-    showDiagnostic(messages){
+    constructor() { }
+
+    showDiagnostic(messages) {
+        this.observer = new Subject<any>();
         this.messages = messages;
         this.isVisible = true;
+        return this.observer;
     }
-    
-    resetDiagnostic(){
+
+    resetDiagnostic() {
         this.messages = [];
         this.isVisible = false;
+
+        this.observer.next(true);
+        this.observer.complete();
     }
 }
