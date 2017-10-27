0b5500c993bf31a56191072bdfab3932df413e82
mdelbene <michela.delbene@microarea.it>
2017-10-27 09:37:00 +0200

    Provisioning front-end: fix open url for new subscription

--- client/apps/console/src/app/components/subscription/subscription.component.ts
+++ client/apps/console/src/app/components/subscription/subscription.component.ts
@@ -1,5 +1,5 @@
 import { element } from 'protractor';
-import {AccountInfo} from '../../authentication/account-info';
+import { AccountInfo } from '../../authentication/account-info';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AppSubscription } from '../../model/subscription';
@@ -37,71 +37,70 @@ export class SubscriptionComponent implements OnInit {
 
     let subscriptionKey: string = this.route.snapshot.queryParams['subscriptionToEdit'];
 
-    if (subscriptionKey === undefined) 
-      return;
-    
-    this.editing = true;
-    this.readingData = true;
+    if (subscriptionKey !== undefined) {
+      this.editing = true;
+      this.readingData = true;
 
-    // first I load the subscription 
+      // first I load the subscription 
 
-    let accountName: string;
-    let authorizationStored = localStorage.getItem('auth-info');
-    
-    if (authorizationStored !== null) {
-      let authorizationProperties: AuthorizationProperties = JSON.parse(authorizationStored);    
-      accountName = authorizationProperties.accountName;
-    }
+      let accountName: string;
+      let authorizationStored = localStorage.getItem('auth-info');
 
-    let localAccountInfo = localStorage.getItem(accountName);
-    let instanceKey: string = '';
-    
-    if (localAccountInfo != null && localAccountInfo != '') {
-      let accountInfo: AccountInfo = JSON.parse(localAccountInfo);
-      instanceKey = accountInfo.instanceKey;
-    }    
-
-    this.modelService.getSubscriptions(accountName, instanceKey, subscriptionKey)
-      .subscribe(
-      res => {
-        let subscriptions: AppSubscription[] = res['Content'];
-
-        if (subscriptions.length == 0) {
-          this.readingData = true;
-          return;
-        }
-
-        this.model = subscriptions[0];
-
-        // then I load the databases of selected subscription
-
-        this.modelService.getDatabases(subscriptionKey)
-          .subscribe(
-          res => {
-            this.databases = res['Content'];
-            this.readingData = false;
-            this.existDatabases = this.databases.length > 0;
-            
-            for (var index = 0; index < this.databases.length; index++) {
-              var db = this.databases[index];
-              if (db.UnderMaintenance) {
-                this.underMaintenance = true;
-                break;
-              }
-            }
-         },
-          err => {
-            alert(err);
-            this.readingData = false;
-          }
-          )
-      },
-      err => {
-        alert(err);
-        this.readingData = false;
+      if (authorizationStored !== null) {
+        let authorizationProperties: AuthorizationProperties = JSON.parse(authorizationStored);
+        accountName = authorizationProperties.accountName;
       }
-      )
+
+      let localAccountInfo = localStorage.getItem(accountName);
+      let instanceKey: string = '';
+
+      if (localAccountInfo != null && localAccountInfo != '') {
+        let accountInfo: AccountInfo = JSON.parse(localAccountInfo);
+        instanceKey = accountInfo.instanceKey;
+      }
+
+      this.modelService.getSubscriptions(accountName, instanceKey, subscriptionKey)
+        .subscribe(
+        res => {
+          let subscriptions: AppSubscription[] = res['Content'];
+
+          if (subscriptions.length == 0) {
+            this.readingData = true;
+            return;
+          }
+
+          this.model = subscriptions[0];
+
+          // then I load the databases of selected subscription
+
+          this.modelService.getDatabases(subscriptionKey)
+            .subscribe(
+            res => {
+              this.databases = res['Content'];
+              this.readingData = false;
+              this.existDatabases = this.databases.length > 0;
+
+              for (var index = 0; index < this.databases.length; index++) {
+                var db = this.databases[index];
+                if (db.UnderMaintenance) {
+                  this.underMaintenance = true;
+                  break;
+                }
+              }
+            },
+            err => {
+              alert(err);
+              this.readingData = false;
+            }
+            )
+        },
+        err => {
+          alert(err);
+          this.readingData = false;
+        }
+        )
     }
+  }
 
   //--------------------------------------------------------------------------------------------------------
   submitSubscription() {
@@ -137,7 +136,7 @@ export class SubscriptionComponent implements OnInit {
 
   //--------------------------------------------------------------------------------------------------------
   configureDatabase() {
-     // route to add database
-     this.router.navigate(['/database/configuration'], { queryParamsHandling: "preserve" } );
-    }
+    // route to add database
+    this.router.navigate(['/database/configuration'], { queryParamsHandling: "preserve" });
+  }
 }
