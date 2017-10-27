306e02543bee7576a7599c3783af676e1a8ecf67
Francesco Ricceri <fraricce@gmail.com>
2017-10-27 16:59:52 +0200

    Provisioning: BackEnd: introducing PostMan

---
+++ server/admin-server/Services/PostMan/PostMan.cs
@@ -0,0 +1,24 @@
+﻿namespace Microarea.AdminServer.Services.PostMan
+{
+	//================================================================================
+	public interface IPostManActuator
+	{
+		OperationResult Send();
+	}
+
+	//================================================================================
+	public class PostMan
+    {
+		IPostManActuator postManActuator;
+
+		public PostMan(IPostManActuator iPostManActuator)
+		{
+			this.postManActuator = iPostManActuator;
+		}
+
+		public OperationResult Send()
+		{
+			return this.postManActuator.Send();
+		}
+	}
+}
