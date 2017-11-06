d905579ad4b84d8a2578eafabd17266f953d6c56
fabrizio.debortoli@microarea.it <fabrizio.debortoli@microarea.it>
2017-11-06 10:02:26 +0100

    Check Server down

--- client/core/src/core/guards/core.guard.ts
+++ client/core/src/core/guards/core.guard.ts
@@ -3,6 +3,7 @@ import { CanActivate, UrlSegment, Router, ActivatedRouteSnapshot, RouterStateSna
 import { Observable } from '../../rxjs.imports';
 
 import { Logger } from './../services/logger.service';
+import { HttpService } from './../services/http.service';
 import { AuthService } from './../services/auth.service';
 import { InfoService } from './../services/info.service';
 
@@ -13,30 +14,46 @@ export class CoreGuard implements CanActivate {
         public authService: AuthService,
         public router: Router,
         public logger: Logger,
-        public infoService: InfoService
+        public infoService: InfoService,
+        public httpService: HttpService
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        let url: string = state.url;
-        this.logger.info('canActivate => Url: ' + url);
 
-        return this.authService.isLogged().map(isLogged => {
-            if (isLogged) {
-                if (url === this.authService.getLoginUrl()) {
-                    this.router.navigate([this.authService.getDefaultUrl()]);
+        this.httpService.isServerUp().map(isServerUp => {
+            let url: string = state.url;
+            this.logger.info('canActivate => Url: ' + url);
+
+            return this.authService.isLogged().map(isLogged => {
+                if (isLogged) {
+                    if (url === this.authService.getLoginUrl()) {
+                        this.router.navigate([this.authService.getDefaultUrl()]);
+                        return false;
+                    }
+                    return true;
+                } else {
+                    this.authService.setRedirectUrl(url);
+                    this.router.navigate([this.authService.getLoginUrl()]);
                     return false;
                 }
-                return true;
-            } else {
+            }).catch(() => {
                 this.authService.setRedirectUrl(url);
                 this.router.navigate([this.authService.getLoginUrl()]);
-                return false;
-            }
-        }).catch(() => {
-            this.authService.setRedirectUrl(url);
-            this.router.navigate([this.authService.getLoginUrl()]);
-            return Observable.of(false);
+                return Observable.of(false);
+            });
+
+        }).catch((error: any) => {
+            let errMsg = (error.message) ? error.message :
+                error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+            if (this.logger)
+                this.logger.error(errMsg);
+
+            this.router.navigate([this.authService.getServerDownPageUrl()]);
+
+            return Observable.throw(errMsg);
         });
+
+
     }
 
 }
