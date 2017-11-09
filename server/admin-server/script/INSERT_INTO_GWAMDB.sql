0a4f876d916141b571c7df38a762f94e2bc01296
imanzoni <ilaria.manzoni@microarea.it>
2017-11-09 12:50:24 +0100

    provisioning: colonna verification code diventa stringa e viene creata con hashing solido

--- server/admin-server/script/INSERT_INTO_GWAMDB.sql
+++ server/admin-server/script/INSERT_INTO_GWAMDB.sql
@@ -1,6 +1,6 @@
 -- Istanza e urls
-INSERT [dbo].[MP_Instances] ([InstanceKey], [Description], [Disabled], [PendingDate], [VerificationCode], [Ticks]) VALUES (N'I-M4', N'istanza cloud M4', 0, CAST(N'2027-06-26 15:04:57.713' AS DateTime),-893924235, 999)
-INSERT [dbo].[MP_Instances] ([InstanceKey], [Description], [Disabled], [PendingDate], [VerificationCode], [Ticks]) VALUES (N'I-M4-ENT', N'istanza M4 Enterprise', 0, CAST(N'2027-06-26 15:04:57.713' AS DateTime), -893924235,999)
+INSERT [dbo].[MP_Instances] ([InstanceKey], [Description], [Disabled], [PendingDate], [VerificationCode], [Ticks]) VALUES (N'I-M4', N'istanza cloud M4', 0, CAST(N'2027-06-26 15:04:57.713' AS DateTime),'NjM5NDk2MTkwOTc3MTMwMDAwNg==', 999)
+INSERT [dbo].[MP_Instances] ([InstanceKey], [Description], [Disabled], [PendingDate], [VerificationCode], [Ticks]) VALUES (N'I-M4-ENT', N'istanza M4 Enterprise', 0, CAST(N'2027-06-26 15:04:57.713' AS DateTime), 'NjM5NDk2MTkwOTc3MTMwMDAwNg==',999)
 
 INSERT [dbo].[MP_ServerURLs] ([InstanceKey], [URLType], [URL]) VALUES (N'I-M4', 0, N'http://test.m4app.com')
 INSERT [dbo].[MP_ServerURLs] ([InstanceKey], [URLType], [URL]) VALUES (N'I-M4', 1, N'https://app.m4cloud.netcore.com')
@@ -87,7 +87,3 @@ VALUES (N'I-M4', N'M4-MDC', N'AzDigital', N'Digital communications', N'', N'', N
 INSERT INTO [dbo].[MP_SubscriptionDatabases] ([InstanceKey],[SubscriptionKey],[Name],[Description],[DBServer],[DBName],[DBOwner],[DBPassword],[Disabled],[DatabaseCulture],[IsUnicode],[Provider],[UseDMS],[DMSDBServer],[DMSDBName],[DMSDBOwner],[DMSDBPassword],[Test], [UnderMaintenance]) 
 VALUES (N'I-M4', N'M4-MDC', N'AzDigitalPlus', N'azienda digitale plus', N'', N'', N'', N'', 0, N'', 0, N'', 0, N'', N'', N'', N'', 0, 0)
 
--- Registeredapps --SOLO GWAM????
-INSERT INTO [dbo].[MP_RegisteredApps] ([AppId],[Name],[Description],[URL],[SecurityValue]) VALUES (N'M-PAI', N'PAI', N'Microarea PAI', N'', N'ju23ff-KOPP-0911-ww2')
-INSERT INTO [dbo].[MP_RegisteredApps] ([AppId],[Name],[Description],[URL],[SecurityValue]) VALUES (N'I-M4', N'Istanza M4 Cloud', N'Microarea Mago4', N'', N'ju23ff-KOPP-0911-ila')
-INSERT INTO [dbo].[MP_RegisteredApps] ([AppId],[Name],[Description],[URL],[SecurityValue]) VALUES (N'I-M4-ENT', N'Istanza M4 Enterprise', N'Microarea Mago4 Enterprise', N'', N'ju23ff-KOPP-0911-mic')
