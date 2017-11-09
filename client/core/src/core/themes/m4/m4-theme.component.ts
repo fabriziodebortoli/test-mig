952fb27e2feeab8ec32380e7a5bb48d865899ec6
Luca Bruni <luca.bruni@microarea.it>
2017-11-09 16:01:40 +0100

    Adeguati temi

--- client/core/src/core/themes/m4/m4-theme.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-    template: '',
-    styleUrls: ['./m4-theme.component.scss'],
-    encapsulation: ViewEncapsulation.None
-})
-export class M4Theme { }
-
