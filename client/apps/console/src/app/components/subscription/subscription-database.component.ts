7e683ebb68c193b2967780dfe437454e68a0e02b
mdelbene <michela.delbene@microarea.it>
2017-11-10 16:37:01 +0100

    Provisioning front-end: new process in SubscriptionDatabase component

--- client/apps/console/src/app/components/subscription/subscription-database.component.ts
+++ client/apps/console/src/app/components/subscription/subscription-database.component.ts
@@ -20,21 +20,27 @@ export class SubscriptionDatabaseComponent implements OnInit {
   @Input() originalModel: SubscriptionDatabase;
 
   editing: boolean = false;
+  step: number;
+
+  // opendialog variables
   openMsgDialog: boolean = false;
   msgDialog: string;
 
+  // operationslist to confirm
   operationsList: OperationResult[];
   extendedSubDatabase: ExtendedSubscriptionDatabase;
 
-  elaborationCompletedWithErrors: boolean;
-  checkDatabaseIsStarted: boolean = false;
+  // for check elaboration
+  checkDbIsStarted: boolean = false;
+  checkDbIsRunning: boolean = false;
+  checkDbHasErrors: boolean = false;
+  saveDbIsRunning:  boolean = false;
 
   // dropdown auxiliary variables
   providers: Array<{ name: string, value: string }> = [
     { name: 'SQL Azure', value: 'SQLAzure' },
     { name: 'SQL Server', value: 'SQLServer' }
   ];
-
   selectedProvider: { name: string, value: string };
 
   //--------------------------------------------------------------------------------------------------------
@@ -42,6 +48,7 @@ export class SubscriptionDatabaseComponent implements OnInit {
     private modelService: ModelService,
     private databaseService: DatabaseService,
     private route: ActivatedRoute) {
+    this.step = 1;
   }
 
   //--------------------------------------------------------------------------------------------------------
@@ -55,8 +62,11 @@ export class SubscriptionDatabaseComponent implements OnInit {
       this.model.Provider = this.databaseService.dbCredentials.Provider;
       this.model.DBName = this.model.Name + '_DB';
       this.model.DBOwner = prefix + '_Admin';
+      this.model.DBPassword = '';
       this.model.DMSDBName = this.model.Name + '_DBDMS';
       this.model.DMSDBOwner = prefix + '_Admin';
+      this.model.DMSDBPassword = '';
+
       // to initialize the provider in dropdown
       this.initProviderValueDropDown();
       return;
@@ -102,32 +112,52 @@ export class SubscriptionDatabaseComponent implements OnInit {
     return result ? 'Operation successfully completed' : 'Operation ended with errors';
   }
 
+  // evento intercettato sul click del pulsante Back
   //--------------------------------------------------------------------------------------------------------
-  save() {
+  returnToMainData() {
+    // reimposto il numero di step = 1
+    this.step = 1;
+    // su questo booleano si basa *ngif per mostrare i control
+    this.checkDbIsStarted = false;
+    // svuoto la lista dei msg
+    this.operationsList = [];
+  }
+
+  // evento intercettato sul click del pulsante Save
+  //--------------------------------------------------------------------------------------------------------
+  submit() {
     // first I check input values 
     if (!this.validateInput())
       return;
 
+    // se sono in modifica tutti i campi sono readonly, quindi vado a salvare solo l'anagrafica senza check
     if (this.editing) {
-      // se sono in modifica tutti i campi sono readonly, quindi vado a salvare solo l'anagrafica senza check
+
       if (!this.onlyMainDataHasChanged()) {
+
         this.msgDialog = 'No data has changed, so no save operation is needed';
         this.openMsgDialog = true;
         return;
       }
-      else {
-        this.saveDatabase();
-      }
+      else
+        this.updateDatabase();
     }
     else {
-      // se sto inserendo i dati manualmente devo eseguire il check preventivo di tutti i dati
-      this.saveDatabaseWithCheck();
+      switch (this.step) {
+        case 1:
+          this.checkDatabase();
+          break;
+
+        case 2:
+          this.saveDatabase();
+          break;
+      }
     }
   }
 
   // esegue il salvataggio dell'anagrafica senza controlli
   //--------------------------------------------------------------------------------------------------------
-  saveDatabase() {
+  updateDatabase() {
 
     let runUpdate = this.modelService.saveDatabase(this.model).
       subscribe(
@@ -148,7 +178,232 @@ export class SubscriptionDatabaseComponent implements OnInit {
 
   // esegue il salvataggio dell'anagrafica CON i vari controlli preventivi sui valori inseriti
   //--------------------------------------------------------------------------------------------------------
-  saveDatabaseWithCheck() {
+  saveDatabase() {
+
+    this.checkDbIsRunning = true; // mostro la gif di elaborazione 
+    this.operationsList = []; // pulisco i msg
+    this.saveDbIsRunning = true;
+
+    let test = this.modelService.testConnection(this.model.SubscriptionKey, this.databaseService.dbCredentials).
+      subscribe(
+      testResult => {
+
+        if (testResult.Result) {
+
+          this.extendedSubDatabase = new ExtendedSubscriptionDatabase(this.databaseService.dbCredentials, this.model);
+
+          let update = this.modelService.updateDatabase(this.model.SubscriptionKey, this.extendedSubDatabase).
+            subscribe(
+            updateResult => {
+
+              this.msgDialog = this.getMessageDialog(updateResult.Result);
+              this.openMsgDialog = true;
+
+              this.checkDbIsRunning = false;
+              this.saveDbIsRunning = false;
+              
+              if (updateResult.Result) {
+                this.returnToMainData();
+                this.editing = true;
+                this.originalModel.assign(this.model);
+              }
+              else {
+                // se il check ha ritornato degli errori mostro nuovamente i controlli con i dati
+                this.operationsList = updateResult['Content'];
+              }
+
+              update.unsubscribe();
+            },
+            updateError => {
+              console.log(updateError);
+              alert(updateError);
+              update.unsubscribe();
+            }
+            )
+        }
+        else {
+          alert('Unable to connect! ' + testResult.Message);
+          this.checkDbIsRunning = false;
+          this.saveDbIsRunning = false;
+        }
+        test.unsubscribe();
+      },
+      error => {
+        console.log(error);
+        alert(error);
+        test.unsubscribe();
+      }
+      );
+  }
+
+  // esegue il check dell'anagrafica CON i vari controlli preventivi sui valori inseriti
+  //--------------------------------------------------------------------------------------------------------
+  checkDatabase() {
+
+    this.checkDbIsRunning = this.checkDbIsStarted = true;
+    this.checkDbHasErrors = false;
+    this.step = 2;
+
+    let test = this.modelService.testConnection(this.model.SubscriptionKey, this.databaseService.dbCredentials).
+      subscribe(
+      testResult => {
+
+        if (testResult.Result) {
+
+          this.extendedSubDatabase = new ExtendedSubscriptionDatabase(this.databaseService.dbCredentials, this.model);
+
+          let checkDb = this.modelService.checkDatabase(this.model.SubscriptionKey, this.extendedSubDatabase).
+            subscribe(
+            checkDbResult => {
+
+              // lo step rimane a 1 perche' ci sono degli errori irrisolvibili
+              this.step = checkDbResult.Result ? 2 : 1;
+
+              // l'elaborazione e' terminata
+              this.checkDbIsRunning = false;
+              this.checkDbHasErrors = !checkDbResult.Result;
+
+              this.operationsList = checkDbResult['Content'];
+
+              checkDb.unsubscribe();
+            },
+            checkDbError => {
+              console.log(checkDbError);
+              alert(checkDbError);
+              checkDb.unsubscribe();
+            }
+            )
+        }
+        else {
+          this.step = 1;
+          alert('Unable to connect! ' + testResult.Message);
+        }
+        test.unsubscribe();
+      },
+      error => {
+        console.log(error);
+        alert(error);
+        test.unsubscribe();
+      }
+      );
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  validateInput(): boolean {
+    if (this.model.SubscriptionKey == undefined || this.model.Name == undefined) {
+      this.showAlertMessage('Mandatory fields are empty! Check subscription key / database name!');
+      return false;
+    }
+
+    if (this.model.Provider === '' ||
+      this.model.DBServer === '' || this.model.DBName === '' || this.model.DBOwner === '' ||
+      this.model.DMSDBServer === '' || this.model.DMSDBName === '' || this.model.DMSDBOwner === '') {
+      this.showAlertMessage('Mandatory fields are empty! Check databases information!');
+      return false;
+    }
+
+    if (this.model.DBServer !== this.model.DMSDBServer) {
+      this.showAlertMessage('Both databases must be in the same server!');
+      return false;
+    }
+
+    if (this.model.DBName === this.model.DMSDBName) {
+      this.showAlertMessage('The databases names must be different!');
+      return false;
+    }
+
+    if (this.model.DBOwner === this.model.DMSDBOwner) {
+      if (this.model.DBPassword !== this.model.DMSDBPassword) {
+        this.showAlertMessage('Passwords different for same users!');
+        return false;
+      }
+    }
+
+    // per il provider Azure devo controllare se le password rispettano le policy di sicurezza
+    if (this.model.Provider === 'SQLAzure') {
+
+      let dbPasswordIsValid = this.meetAzurePasswordPolicy(this.model.DBPassword, this.model.DBOwner);
+      let dmsDbPasswordIsValid = (this.model.DBPassword !== this.model.DMSDBPassword)
+        ? this.meetAzurePasswordPolicy(this.model.DMSDBPassword, this.model.DMSDBOwner) : dbPasswordIsValid;
+
+      let msg = '';
+      if (dbPasswordIsValid && dmsDbPasswordIsValid) {
+        return true;
+      }
+      else {
+        if (!dbPasswordIsValid && !dmsDbPasswordIsValid) {
+          msg = 'The password specified for both dbowner does not meet the policy requirements';
+        }
+        else {
+          msg = 'The password specified for ' + ((!dbPasswordIsValid) ? 'ERP' : 'DMS') + ' dbowner does not meet the policy requirements';
+        }
+      }
+      if (msg !== '')
+        this.showAlertMessage(msg);
+      return dbPasswordIsValid && dmsDbPasswordIsValid;
+    }
+
+    return true;
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  showAlertMessage(message: string) {
+    this.msgDialog = message;
+    this.openMsgDialog = true;
+  }
+
+  //--------------------------------------------------------------------------------------------------------
+  meetAzurePasswordPolicy(password: string, user: string): boolean {
+
+    // https://docs.microsoft.com/en-us/sql/relational-databases/security/password-policy
+
+    let forbiddenPasswords = /passw.*|password.*|admin.*|administrator.*|sa.*|sysadmin.*/;
+
+    if (
+      password == null || password == undefined || password === '' ||
+      password.length < 8 ||
+      password.includes(user) ||
+      password === user ||
+      forbiddenPasswords.test(password)
+    )
+      return false;
+
+    // Build up the strength of our password
+    let numberOfElements = 0;
+    numberOfElements = /.*[a-z].*/.test(password) ? ++numberOfElements : numberOfElements;      // Lowercase letters
+    numberOfElements = /.*[A-Z].*/.test(password) ? ++numberOfElements : numberOfElements;      // Uppercase letters
+    numberOfElements = /.*[0-9].*/.test(password) ? ++numberOfElements : numberOfElements;      // Numbers
+    numberOfElements = /[^a-zA-Z0-9]/.test(password) ? ++numberOfElements : numberOfElements;   // Special characters (inc. space)
+
+    return numberOfElements >= 3;
+  }
+
+  // ritorna true solo se solo i pochi dati anagrafici sono variati (cosi si procede con un semplice update senza controlli)
+  //--------------------------------------------------------------------------------------------------------
+  onlyMainDataHasChanged(): boolean {
+
+    if (this.originalModel.Description !== this.model.Description ||
+      this.originalModel.Disabled !== this.model.Disabled) {
+
+      if (this.originalModel.Provider === this.model.Provider &&
+        this.originalModel.IsUnicode === this.model.IsUnicode &&
+        this.originalModel.DBServer === this.model.DBServer &&
+        this.originalModel.DBName === this.model.DBName &&
+        this.originalModel.DBOwner === this.model.DBOwner &&
+        this.originalModel.DBPassword === this.model.DBPassword &&
+        this.originalModel.DMSDBServer === this.model.DMSDBServer &&
+        this.originalModel.DMSDBName === this.model.DMSDBName &&
+        this.originalModel.DMSDBOwner === this.model.DMSDBOwner &&
+        this.originalModel.DMSDBPassword === this.model.DMSDBPassword
+      )
+        return true;
+    }
+    return false;
+  }
+
+  // vecchio codice con test + check + save
+  //--------------------------------------------------------------------------------------------------------
+ /* oldSaveDatabaseWithCheck() {
 
     this.checkDatabaseIsStarted = true;
     this.elaborationCompletedWithErrors = false;
@@ -219,62 +474,5 @@ export class SubscriptionDatabaseComponent implements OnInit {
         test.unsubscribe();
       }
       );
-  }
-
-  //--------------------------------------------------------------------------------------------------------
-  validateInput(): boolean {
-    if (this.model.SubscriptionKey == undefined || this.model.Name == undefined) {
-      alert('Mandatory fields are empty! Check subscription key / database name!');
-      return false;
-    }
-
-    if (this.model.Provider === '' ||
-      this.model.DBServer === '' || this.model.DBName === '' || this.model.DBOwner === '' ||
-      this.model.DMSDBServer === '' || this.model.DMSDBName === '' || this.model.DMSDBOwner === '') {
-      alert('Mandatory fields are empty! Check databases information!');
-      return false;
-    }
-
-    if (this.model.DBServer !== this.model.DMSDBServer) {
-      alert('Both databases must be in the same server!');
-      return false;
-    }
-
-    if (this.model.DBName === this.model.DMSDBName) {
-      alert('The databases names must be different!');
-      return false;
-    }
-
-    if (this.model.DBOwner === this.model.DMSDBOwner) {
-      if (this.model.DBPassword !== this.model.DMSDBPassword) {
-        alert('Passwords different for same users!');
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  // ritorna true solo se solo i pochi dati anagrafici sono variati (cosi si procede con un semplice update senza controlli)
-  //--------------------------------------------------------------------------------------------------------
-  onlyMainDataHasChanged(): boolean {
-
-    if (this.originalModel.Description !== this.model.Description ||
-      this.originalModel.Disabled !== this.model.Disabled) {
-
-      if (this.originalModel.Provider === this.model.Provider &&
-        this.originalModel.IsUnicode === this.model.IsUnicode &&
-        this.originalModel.DBServer === this.model.DBServer &&
-        this.originalModel.DBName === this.model.DBName &&
-        this.originalModel.DBOwner === this.model.DBOwner &&
-        this.originalModel.DBPassword === this.model.DBPassword &&
-        this.originalModel.DMSDBServer === this.model.DMSDBServer &&
-        this.originalModel.DMSDBName === this.model.DMSDBName &&
-        this.originalModel.DMSDBOwner === this.model.DMSDBOwner &&
-        this.originalModel.DMSDBPassword === this.model.DMSDBPassword
-      )
-        return true;
-    }
-    return false;
-  }
+  }*/
 }
