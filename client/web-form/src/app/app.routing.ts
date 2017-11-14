e4099d8ad1996eb1c1400f4466a8c08320a262cf
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2017-11-14 08:34:47 +0100

    rimosse routes inutilizzate in desktop

--- client/web-form/src/app/app.routing.ts
+++ client/web-form/src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { BPMStandaloneComponent } from '@taskbuilder/bpm';
+// import { BPMStandaloneComponent } from '@taskbuilder/bpm';
 
 import { environment } from './../environments/environment';
 
@@ -10,15 +10,15 @@ import {
     LoginComponent,
     PageNotFoundComponent,
     UnsupportedFactoryComponent,
-    ProxyRouteComponent,
+    // ProxyRouteComponent,
     HomeComponent,
-    StandaloneDocumentComponent,
-    StandaloneReportComponent
+    // StandaloneDocumentComponent,
+    // StandaloneReportComponent
 } from '@taskbuilder/core';
-import { ReportingStudioFactoryComponent } from '@taskbuilder/reporting-studio';
+// import { ReportingStudioFactoryComponent } from '@taskbuilder/reporting-studio';
 
-import { appRoutes } from './applications/app.routing';
-
+// import { appRoutes } from './applications/app.routing';
+/*
 let webOnlyRoutes = [
     { path: 'rs', loadChildren: '@taskbuilder/reporting-studio#ReportingStudioModule' },
     { path: 'test', loadChildren: '@taskbuilder/core#TbTestModule' },
@@ -29,22 +29,22 @@ let allEnvRoutes = [
     { path: 'bpm', loadChildren: '@taskbuilder/bpm#BPMModule' }
 ];
 let childrenRoutes = environment.desktop ? [...allEnvRoutes] : [...allEnvRoutes, ...webOnlyRoutes];
-
+*/
 export const routing: ModuleWithProviders = RouterModule.forRoot([
     { path: '', component: HomeComponent, canActivate: [CoreGuard] },
     { path: 'login', component: LoginComponent },
-    { path: 'bpmTG', component: BPMStandaloneComponent, canActivate: [CoreGuard] },
-    { path: 'home', component: HomeComponent, canActivate: [CoreGuard] },
-    { path: 'document/:ns', component: StandaloneDocumentComponent, canActivate: [CoreGuard] },
+    // { path: 'bpmTG', component: BPMStandaloneComponent, canActivate: [CoreGuard] },
+    // { path: 'home', component: HomeComponent, canActivate: [CoreGuard] },
+    // { path: 'document/:ns', component: StandaloneDocumentComponent, canActivate: [CoreGuard] },
     // { path: 'rs/:ns', component: StandaloneReportComponent }, // TODO riabilitare quando risolveremo problemi AOT
-    {
-        path: 'proxy',
-        outlet: 'dynamic',
-        component: ProxyRouteComponent,
-        children: [
-            ...childrenRoutes
-        ],
-    },
+    // {
+    //     path: 'proxy',
+    //     outlet: 'dynamic',
+    //     component: ProxyRouteComponent,
+    //     children: [
+    //         ...childrenRoutes
+    //     ],
+    // },
     // { path: 'server-down', component: ServerDownComponent },
     { path: '**', component: PageNotFoundComponent },
 ]);
