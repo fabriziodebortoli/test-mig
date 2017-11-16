3f5930535386ea64785ea54189439184082211c8
simonapodesta <simona.podesta@microarea.it>
2017-11-16 15:27:08 +0100

    Messo view encapsulation

--- client/core/src/shared/components/header-strip/header-strip.component.ts
+++ client/core/src/shared/components/header-strip/header-strip.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ViewEncapsulation } from '../../../../../web-form/node_modules/@angular/core/src/metadata/view';
 
 @Component({
   selector: 'tb-header-strip',
   templateUrl: './header-strip.component.html',
-  styleUrls: ['./header-strip.component.scss']
+  styleUrls: ['./header-strip.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderStripComponent implements OnInit {
 
