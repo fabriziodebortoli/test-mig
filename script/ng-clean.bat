d6631af7b871eaf41b6bf4b832a469a6c84debc2
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2017-11-21 08:33:34 +0100

    package-lock.json non piu cancellato

--- script/ng-clean.bat
+++ script/ng-clean.bat
@@ -1,40 +1,29 @@
 cd ..\client\core
-del package-lock.json
 call rimraf node_modules
 cd src
-del package-lock.json
 call rimraf node_modules
 
 cd ..\..\reporting-studio
-del package-lock.json
 call rimraf node_modules
 cd src
-del package-lock.json
 call rimraf node_modules
 
 cd ..\..\icons
-del package-lock.json
 call rimraf node_modules
 cd src
-del package-lock.json
 call rimraf node_modules
 
 cd ..\..\erp
-del package-lock.json
 call rimraf node_modules
 cd src
-del package-lock.json
 call rimraf node_modules
 
 cd ..\..\bpm
-del package-lock.json
 call rimraf node_modules
 cd src
-del package-lock.json
 call rimraf node_modules
 
 cd ..\..\web-form
-del package-lock.json
 call rimraf node_modules
 
 call npm cache verify
