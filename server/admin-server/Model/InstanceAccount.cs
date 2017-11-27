7ced2c39652b65e8c88b1f46faddd7e21d64ecac
Francesco Ricceri <fraricce@gmail.com>
2017-11-27 15:29:03 +0100

    Provisioning: BackEnd: InstanceAccount: added Ticks

--- server/admin-server/Model/InstanceAccount.cs
+++ server/admin-server/Model/InstanceAccount.cs
@@ -10,14 +10,17 @@ namespace Microarea.AdminServer.Model
 	//================================================================================
 	public class InstanceAccount : IInstanceAccount, IModelObject
 	{
-		public string accountName;
-		public string instanceKey;
+		string accountName;
+		string instanceKey;
+		int ticks;
 		//---------------------------------------------------------------------
 		public string AccountName { get { return this.accountName; } set { this.accountName = value; } }
 		public string InstanceKey { get { return this.instanceKey; } set { this.instanceKey = value; } }
+		public int Ticks { get { return this.ticks; } set { this.ticks = value; } }
 		//---------------------------------------------------------------------
 		public InstanceAccount()
 		{
+			this.ticks = 0;
 		}
 
 		//---------------------------------------------------------------------
@@ -27,9 +30,11 @@ namespace Microarea.AdminServer.Model
 			List<BurgerDataParameter> burgerDataParameters = new List<BurgerDataParameter>();
 			BurgerDataParameter keyColumnAccountName = new BurgerDataParameter("@AccountName", this.accountName);
 			BurgerDataParameter keyColumnInstanceKey = new BurgerDataParameter("@InstanceKey", this.instanceKey);
+			BurgerDataParameter columnTicks = new BurgerDataParameter("@Ticks", this.ticks);
 
 			burgerDataParameters.Add(keyColumnAccountName);
 			burgerDataParameters.Add(keyColumnInstanceKey);
+			burgerDataParameters.Add(columnTicks);
 
 			BurgerDataParameter[] keyColumnsParameter = new BurgerDataParameter[]
 			{
