6cdcc78a9aacdcb8b786d31e40bacab461785945
MauroRogledi <mauro.rogledi@microarea.it>
2017-12-05 17:18:52 +0100

    Aggiunti import per lingue supportate da Mago

--- client/core/src/shared/kendo/kendo.module.ts
+++ client/core/src/shared/kendo/kendo.module.ts
@@ -10,6 +10,33 @@ import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { GridModule, GridComponent } from '@progress/kendo-angular-grid';
 import { ChartsModule } from '@progress/kendo-angular-charts';
 
+import '@progress/kendo-angular-intl/locales/bg/numbers';
+import '@progress/kendo-angular-intl/locales/bg/calendar';
+import '@progress/kendo-angular-intl/locales/de-CH/numbers';
+import '@progress/kendo-angular-intl/locales/de-CH/calendar';
+import '@progress/kendo-angular-intl/locales/el/numbers';
+import '@progress/kendo-angular-intl/locales/el/calendar';
+import '@progress/kendo-angular-intl/locales/en/numbers';
+import '@progress/kendo-angular-intl/locales/en/calendar';
+import '@progress/kendo-angular-intl/locales/es-CL/numbers';
+import '@progress/kendo-angular-intl/locales/es-CL/calendar';
+import '@progress/kendo-angular-intl/locales/hu/numbers';
+import '@progress/kendo-angular-intl/locales/hu/calendar';
+import '@progress/kendo-angular-intl/locales/it-CH/numbers';
+import '@progress/kendo-angular-intl/locales/it-CH/calendar';
+
+import '@progress/kendo-angular-intl/locales/it/numbers';
+import '@progress/kendo-angular-intl/locales/it/calendar';
+import '@progress/kendo-angular-intl/locales/pl/numbers';
+import '@progress/kendo-angular-intl/locales/pl/calendar';
+import '@progress/kendo-angular-intl/locales/ro/numbers';
+import '@progress/kendo-angular-intl/locales/ro/calendar';
+import '@progress/kendo-angular-intl/locales/si/numbers';
+import '@progress/kendo-angular-intl/locales/si/calendar';
+import '@progress/kendo-angular-intl/locales/tr/numbers';
+import '@progress/kendo-angular-intl/locales/tr/calendar';
+
+
 const KENDO_UI_MODULES = [
     GridModule,
     ChartsModule,
