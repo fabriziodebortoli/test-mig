9f44d8995c9b8120f83e8d4f76edae7d0cea79ef
Francesco Ricceri <fraricce@gmail.com>
2017-12-11 14:13:45 +0100

    Provisioning: FrontEnd: Instance: added Server list

--- client/apps/console/src/app/components/instance/instance.component.ts
+++ client/apps/console/src/app/components/instance/instance.component.ts
@@ -20,21 +20,20 @@ export class InstanceComponent implements OnInit, OnDestroy {
   editing: boolean = false;
   subscription: Subscription;
 
+  // dialog
+  fields: Array<{label:string, value:string, hide: boolean}>;
+  openToggle: boolean;
+
   //--------------------------------------------------------------------------------------------------------
   constructor(private modelService: ModelService, private router: Router, private route: ActivatedRoute) {
     this.model = new Instance();
     this.serverURLs = [];
+    this.openToggle = false;
+    this.initDialogFields();
   }
 
   //--------------------------------------------------------------------------------------------------------
   ngOnInit() {
-    let urlType: UrlType;
-    urlType = UrlType.TBLOADER;
-    
-    let surl: ServerUrl = new ServerUrl(urlType, "http://test.tbloader.net", "TBLoader Server");
-
-    this.serverURLs.push(surl);
-
     let instanceKey: string = this.route.snapshot.queryParams['instanceToEdit'];
     
     if (instanceKey !== undefined) {
@@ -58,8 +57,51 @@ export class InstanceComponent implements OnInit, OnDestroy {
     }
   }
 
-   //--------------------------------------------------------------------------------------------------------
-   ngOnDestroy() {
+  //--------------------------------------------------------------------------------
+  initDialogFields() {
+    this.fields = [
+      { label: 'appName', value: '', hide: false },
+      { label: 'url', value: '', hide: false }
+    ];    
+  }
+
+  //--------------------------------------------------------------------------------
+  openAddServerDialog() {
+    this.openToggle = true;
+  }
+
+  //--------------------------------------------------------------------------------
+  onCloseCredentialsDialog() {
+    let serverUrl: ServerUrl = this.getServerInfoFromDialog(this.fields);
+    this.serverURLs.push(serverUrl);
+    this.initDialogFields();
+  }
+
+  //--------------------------------------------------------------------------------
+  getServerInfoFromDialog(formFields: Array<{label:string, value:string, hide: boolean}>) {
+  
+    let appName: string;
+    let url: string;
+
+    for (let i=0;i<formFields.length;i++) {
+      let item = formFields[i];
+
+      if (item.label === 'appName') {
+        appName  = item.value;
+        continue;
+      }
+
+      if (item.label === 'url') {
+        url = item.value;
+        continue;
+      }
+    }
+
+    return new ServerUrl(UrlType.TBLOADER, url, appName);
+  }
+
+  //--------------------------------------------------------------------------------
+  ngOnDestroy() {
     if (this.subscription === undefined)
       return;
 
