c679d14ef0412ee78d00491e947da0eaac056ba5
fgiomi <filippo.giomi@microarea.it>
2017-12-11 15:18:05 +0100

    gestione parametri lato server

---
+++ server/data-service/Managers/Interfaces/IParameterManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microarea.DataService.Managers.Interfaces
+{
+    public interface IParameterManager
+    {
+        Dictionary<string, string> GetParameters(IEnumerable<string> parameters);
+    }
+}
