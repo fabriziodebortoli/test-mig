9d6a6c7a550aa492b8cfe9c1ac32f55739d9430e
Francesco Ricceri <fraricce@gmail.com>
2017-12-11 11:37:34 +0100

    Provisioning: FrontEnd: Added server URLs to Instance component

--- client/apps/console/src/app/components/instance/instance.component.ts
+++ client/apps/console/src/app/components/instance/instance.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Instance } from '../../model/instance';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ServerUrl } from 'app/authentication/server-url';
 
 @Component({
   selector: 'app-instance',
@@ -15,6 +16,7 @@ import { Subscription } from 'rxjs';
 export class InstanceComponent implements OnInit, OnDestroy {
 
   model: Instance;
+  serverURLs: ServerUrl[];
   editing: boolean = false;
   subscription: Subscription;
 
