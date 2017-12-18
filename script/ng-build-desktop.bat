e48c0b98c6bb75a2ef875c8d491ffca9097fb71f
Unknown <fabrizio.debortoli@microarea.it>
2017-12-18 09:04:49 +0100

    path fix

--- script/ng-build-desktop.bat
+++ script/ng-build-desktop.bat
@@ -2,4 +2,4 @@ cd ..\client\web-form
 REM node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --env=desktop --target=production --build-optimizer --aot --no-sourcemaps --preserve-symlinks --output-path="..\..\..\TaskBuilder\WebFramework\M4Client"
 node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --prod --build-optimizer --preserve-symlinks --output-path="..\..\..\TaskBuilder\WebFramework\M4Client"
 cd ..\..
-..\Apps\ClickOnceDeployer\ClickOnceDeployer.exe
+..\..\..\Apps\ClickOnceDeployer\ClickOnceDeployer.exe
