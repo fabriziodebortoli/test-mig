6bc13a202f9d5382be25d0fedc5db6681a0c6bbb
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2017-12-20 10:29:35 +0100

    ng build desktop script mod

--- script/ng-build-desktop.bat
+++ script/ng-build-desktop.bat
@@ -1,5 +1,2 @@
 cd ..\client\web-form
-REM node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --env=desktop --target=production --build-optimizer --aot --no-sourcemaps --preserve-symlinks --output-path="..\..\..\TaskBuilder\WebFramework\M4Client"
-node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --prod --build-optimizer --preserve-symlinks --output-path="..\..\..\TaskBuilder\WebFramework\M4Client"
-cd ..\..
-..\..\..\Apps\ClickOnceDeployer\ClickOnceDeployer.exe
+node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --env=desktop --preserve-symlinks --output-path="..\..\WebFramework\M4Client"
