a3fd07980ebec71c34b477806a04015003907082
HolaChica <anastasia.sarmant@hotmail.com>
2017-12-21 12:50:55 +0100

    fix locale

--- client/web-form/src/main.ts
+++ client/web-form/src/main.ts
@@ -3,7 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { enableDebugTools } from '@angular/platform-browser';
+import localeIt from '@angular/common/locales/it';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeIt);
 if (environment.production) {
   enableProdMode();
 }
