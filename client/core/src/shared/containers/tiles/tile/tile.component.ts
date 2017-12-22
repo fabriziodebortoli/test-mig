0c664194caef9c834d170921eacd45c482835196
simona.podesta <simona.podesta@microarea.it>
2017-12-22 15:12:54 +0100

    tb-card-title visibile se il titolo non è vuoto e se la tile è collapsible

--- client/core/src/shared/containers/tiles/tile/tile.component.ts
+++ client/core/src/shared/containers/tiles/tile/tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'tb-tile',
@@ -10,21 +10,16 @@ export class TileComponent implements OnInit {
   @Input() title: string;
   public _isCollapsed: boolean = false;
   public _isCollapsible: boolean = true;
-  public hasTitle: boolean = true;
-
+  
   constructor() { }
 
-  ngOnInit() { }
-
-  getArrowIcon() {
-    return this._isCollapsed ? 'keyboard_arrow_down' : 'keyboard_arrow_up';
+  ngOnInit() { 
+   
   }
 
   toggleCollapse(event: MouseEvent): void {
     if (!this._isCollapsible)
       return;
-
-    // event.preventDefault();
     this._isCollapsed = !this._isCollapsed;
   }
 
