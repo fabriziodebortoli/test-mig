d579016a1e8862706e67c76b6fc051bd38bc4238
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2017-12-28 14:52:22 +0100

    modificata label static (da completare)

--- client/core/src/shared/controls/label-static/label-static.component.ts
+++ client/core/src/shared/controls/label-static/label-static.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 
-import { TextComponent } from './../text/text.component';
+import { TbComponentService } from './../../../core/services/tbcomponent.service';
+import { LayoutService } from './../../../core/services/layout.service';
+
 import { ControlComponent } from './../control.component';
 
 @Component({
@@ -8,9 +10,16 @@ import { ControlComponent } from './../control.component';
   templateUrl: './label-static.component.html',
   styleUrls: ['./label-static.component.scss']
 })
-export class LabelStaticComponent extends TextComponent implements OnInit {
-  @Input() caption = '';
-  ngOnInit() {
+export class LabelStaticComponent extends ControlComponent {
+
+  @Input() public hotLink: { namespace: string, name: string};
+
+  constructor(
+    layoutService: LayoutService,
+    tbComponentService: TbComponentService,
+    changeDetectorRef: ChangeDetectorRef
+  ) {
+    super(layoutService, tbComponentService, changeDetectorRef);
   }
 
 }
