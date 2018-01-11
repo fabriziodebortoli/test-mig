89264b2ef19707b5631bc68a137555df7d55e883
fraricce-microarea <francesco.ricceri@microarea.it>
2018-01-11 17:22:58 +0100

    Provisioning: BackEnd:  added WebAppConfigurator

---
+++ server/provisioning-database-service/Infrastructure/WebAppConfigurator.cs
@@ -0,0 +1,31 @@
+﻿using Microarea.Common;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microarea.ProvisioningDatabase.Controllers.Helpers;
+
+namespace Microarea.ProvisioningDatabase.Infrastructure
+{
+    public class WebAppConfigurator : IWebAppConfigurator
+	{
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IConfiguration configuration)
+		{
+        }
+
+        public void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+			services.AddTransient<IJsonHelper, JsonHelper>();
+		}
+
+        public void MapRoutes(IRouteBuilder routes)
+		{
+		}
+    }
+}
