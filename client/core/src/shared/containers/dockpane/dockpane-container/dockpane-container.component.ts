9cd8da0c7a9fc1df82001f95c4401508e93d723a
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-01-15 17:42:07 +0100

    dockingpanes collapsible - primo step

--- client/core/src/shared/containers/dockpane/dockpane-container/dockpane-container.component.ts
+++ client/core/src/shared/containers/dockpane/dockpane-container/dockpane-container.component.ts
@@ -1,57 +1,97 @@
+import { Component, OnInit, ElementRef, ViewEncapsulation, AfterContentInit, ContentChildren, QueryList, ViewChild, trigger, transition, style, animate, state, HostBinding } from '@angular/core';
+
 import { TabStripComponent } from '@progress/kendo-angular-layout/dist/es/tabstrip/tabstrip.component';
+
 import { TabberComponent } from './../../tabs/tabber/tabber.component';
 import { DockpaneComponent } from './../dockpane.component';
-import { Component, OnInit, ElementRef, ViewEncapsulation, AfterContentInit, ContentChildren, QueryList, ViewChild, trigger, transition, style, animate, state } from '@angular/core';
+
+const resolvedPromise = Promise.resolve(null); //fancy setTimeout
 
 @Component({
   selector: 'tb-dockpane-container',
   templateUrl: './dockpane-container.component.html',
   styleUrls: ['./dockpane-container.component.scss'],
   animations: [
-    trigger('slideInOut', [
-      state('closed', style({ width: '0' })),
-      state('opened', style({ width: '400px' })),
-      transition('closed <=> opened', animate('250ms ease-in-out'))
-    ]),
-  ],
-  encapsulation: ViewEncapsulation.None
+    trigger('collapsing', [
+      state('expanded', style({ width:'400px', overflow:'hidden' })),
+      state('collapsed', style({ width:'40px', overflow:'hidden' })),
+      transition('expanded <=> collapsed', animate('250ms ease-in-out')),
+    ])
+  ]
 })
 export class DockpaneContainerComponent implements AfterContentInit {
-  public dockState: string = 'closed';
 
-  @ViewChild('dockpaneTabber') dockpaneTabber: TabStripComponent;
+  @ViewChild('kendoTabStripInstance') kendoTabStripInstance: TabStripComponent;
+
   @ContentChildren(DockpaneComponent) dockpanes: QueryList<DockpaneComponent>;
-
-  ngAfterContentInit() {
-    let activeTiles = this.dockpanes.filter((tile) => tile.active);
-
-    //if there is no active tab set, activate the first
-    if (activeTiles.length === 0 && this.dockpanes.toArray().length > 0) {
-      this.selectDock(this.dockpanes.first);
-    }
-  }
-
-  selectDock(dockpane: DockpaneComponent) {
-    if (dockpane.active) return;
-
-    // deactivate all tabs
-    this.dockpanes.toArray().forEach(dockpane => dockpane.active = false);
-
-    // activate the tab the user has clicked on.
-    dockpane.active = true;
-  }
-
   getDockpanes() {
     return this.dockpanes.toArray();
   }
 
-  toggleDockpane(dockpane: DockpaneComponent) {
-
-    if (dockpane.active) {
-      this.dockState = 'opened';
-    }
+  dockState:string = 'expanded';
+  idxActive:number;
 
 
-    this.dockState = dockpane.active ? 'opened' : this.dockState === 'opened' ? 'closed' : 'opened';
+  ngAfterContentInit() {
+    resolvedPromise.then(() => {
+      let dockpanes = this.dockpanes.toArray();
+      let internalTabComponents = [];
+      for (let i = 0; i < dockpanes.length; i++) {
+        internalTabComponents.push(dockpanes[i].tabComponent);
+      }
+      this.kendoTabStripInstance.tabs.reset(internalTabComponents);
+    });
   }
+
+  changeDockpaneByIndex(i){
+
+    if(this.idxActive === i){
+      this.idxActive = null;
+      this.dockState = 'collapsed';
+    }else{
+      this.dockState = 'expanded';
+      this.idxActive = i;
+      this.kendoTabStripInstance.selectTab(i);
+      console.log("this.idxActive", this.idxActive);
+    }
+    
+  }
+
+  // public dockState: string = 'closed';
+
+  // @ViewChild('dockpaneTabber') dockpaneTabber: TabStripComponent;
+  // @ContentChildren(DockpaneComponent) dockpanes: QueryList<DockpaneComponent>;
+
+  // ngAfterContentInit() {
+  //   let activeTiles = this.dockpanes.filter((tile) => tile.active);
+
+  //   //if there is no active tab set, activate the first
+  //   if (activeTiles.length === 0 && this.dockpanes.toArray().length > 0) {
+  //     this.selectDock(this.dockpanes.first);
+  //   }
+  // }
+
+  // selectDock(dockpane: DockpaneComponent) {
+  //   if (dockpane.active) return;
+
+  //   // deactivate all tabs
+  //   this.dockpanes.toArray().forEach(dockpane => dockpane.active = false);
+
+  //   // activate the tab the user has clicked on.
+  //   dockpane.active = true;
+  // }
+
+  // getDockpanes() {
+  //   return this.dockpanes.toArray();
+  // }
+
+  // toggleDockpane(dockpane: DockpaneComponent) {
+
+  //   if (dockpane.active) {
+  //     this.dockState = 'opened';
+  //   }
+
+
+  //   this.dockState = dockpane.active ? 'opened' : this.dockState === 'opened' ? 'closed' : 'opened';
+  // }
 }
--- client/core/src/shared/containers/dockpane/dockpane.component.ts
+++ client/core/src/shared/containers/dockpane/dockpane.component.ts
@@ -1,5 +1,5 @@
-
-import { Component, NgModule, trigger, transition, style, animate, state, Input } from '@angular/core';
+import { Component, NgModule, trigger, transition, style, animate, state, Input, ViewChild } from '@angular/core';
+import { TabStripTabComponent } from '@progress/kendo-angular-layout/dist/es/tabstrip/tabstrip-tab.component';
 
 @Component({
   selector: 'tb-dockpane',
@@ -7,6 +7,9 @@ import { Component, NgModule, trigger, transition, style, animate, state, Input 
   styleUrls: ['./dockpane.component.scss']
 })
 export class DockpaneComponent {
+  @ViewChild(TabStripTabComponent) tabComponent;
   @Input() title: string = '???';
+  @Input() iconType: string = 'M4';
+  @Input() icon: string = 'erp-purchaseorder';
   active: boolean;
 }
