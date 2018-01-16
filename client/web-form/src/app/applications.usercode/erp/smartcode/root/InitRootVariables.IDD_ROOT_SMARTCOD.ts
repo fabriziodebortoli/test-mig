7bcae034a302d5e491cf5cccd2d402798391fa6b
GianlucaAnedda <gianluca.anedda@microarea.it>
2018-01-16 17:02:15 +0100

    Smart Code Root: document customization
    Smart Code Root: document customization

--- client/web-form/src/app/applications.usercode/erp/smartcode/root/InitRootVariables.IDD_ROOT_SMARTCOD.ts
+++ client/web-form/src/app/applications.usercode/erp/smartcode/root/InitRootVariables.IDD_ROOT_SMARTCOD.ts
@@ -11,21 +11,6 @@ export class InitRootVariables extends BOClient {
     onCommand(id: string): Observable<boolean> {
         return Observable.create(observer => {
             if (id === 'ID_EXTDOC_SAVE') {
-                if (this.boService.eventData.model.PaymentTerms.Description.value.length < 3) {
-                    const args = new MessageDlgArgs();
-                    args.text = 'Description is too short, go on anyway?';
-                    args.yes = true;
-                    args.no = true;
-
-                    this.boService.eventData.openMessageDialog.emit(args);
-                    const subs = this.boService.eventData.closeMessageDialog.subscribe(
-                        result => {
-                            observer.next(result.yes === true);
-                            observer.complete();
-                            subs.unsubscribe();
-                        });
-                    return;
-                }
             }
             observer.next(true);
             observer.complete();
