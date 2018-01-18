24c55288446cc99f68f12f260443f55d1b651535
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-01-18 10:27:19 +0100

    section-title fix

--- client/core/src/shared/controls/section-title/section-title.component.ts
+++ client/core/src/shared/controls/section-title/section-title.component.ts
@@ -1,14 +1,10 @@
-import { ControlComponent } from './../control.component';
 import { Component, OnInit } from '@angular/core';
+
+import { ControlComponent } from './../control.component';
 
 @Component({
   selector: 'tb-section-title',
   templateUrl: './section-title.component.html',
   styleUrls: ['./section-title.component.scss']
 })
-export class SectionTitleComponent extends ControlComponent implements OnInit   {
-
-  ngOnInit() {
-  }
-
-}
+export class SectionTitleComponent extends ControlComponent {}
