24fa6d3d5ec5f12a6eb691e00b58845dab54da8e
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-01-22 09:09:17 +0100

    chiusura tab test page

--- client/web-form/src/app/layout/document-menu/document-menu.service.ts
+++ client/web-form/src/app/layout/document-menu/document-menu.service.ts
@@ -1,9 +1,17 @@
-﻿import { BOServiceParams, BOService, EventDataService  } from '@taskbuilder/core';
+﻿import { BOServiceParams, BOService, EventDataService, ComponentService } from '@taskbuilder/core';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DocumentMenuService extends BOService {
-	constructor(params: BOServiceParams, eventData: EventDataService) {
+	constructor(
+		params: BOServiceParams, 
+		eventData: EventDataService,
+		public componentService: ComponentService) {
 		super(params, eventData);
+	}
+	
+	close() {
+        super.close();
+        this.componentService.removeComponentById(this.mainCmpId);
     }
 }
