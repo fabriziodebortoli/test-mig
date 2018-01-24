4075c9529a4402e659a840690c7a11b57fa6b5b4
Luca Bruni <luca.bruni@microarea.it>
2018-01-24 12:20:05 +0100

    An. 26112

--- client/core/src/shared/kendo/kendo.module.ts
+++ client/core/src/shared/kendo/kendo.module.ts
@@ -15,17 +15,20 @@ import '@progress/kendo-angular-intl/locales/bg/numbers';
 import '@progress/kendo-angular-intl/locales/bg/calendar';
 import '@progress/kendo-angular-intl/locales/de-CH/numbers';
 import '@progress/kendo-angular-intl/locales/de-CH/calendar';
+import '@progress/kendo-angular-intl/locales/de/numbers';
+import '@progress/kendo-angular-intl/locales/de/calendar';
 import '@progress/kendo-angular-intl/locales/el/numbers';
 import '@progress/kendo-angular-intl/locales/el/calendar';
 import '@progress/kendo-angular-intl/locales/en/numbers';
 import '@progress/kendo-angular-intl/locales/en/calendar';
 import '@progress/kendo-angular-intl/locales/es-CL/numbers';
 import '@progress/kendo-angular-intl/locales/es-CL/calendar';
+import '@progress/kendo-angular-intl/locales/es/numbers';
+import '@progress/kendo-angular-intl/locales/es/calendar';
 import '@progress/kendo-angular-intl/locales/hu/numbers';
 import '@progress/kendo-angular-intl/locales/hu/calendar';
 import '@progress/kendo-angular-intl/locales/it-CH/numbers';
 import '@progress/kendo-angular-intl/locales/it-CH/calendar';
-
 import '@progress/kendo-angular-intl/locales/it/numbers';
 import '@progress/kendo-angular-intl/locales/it/calendar';
 import '@progress/kendo-angular-intl/locales/pl/numbers';
@@ -36,7 +39,28 @@ import '@progress/kendo-angular-intl/locales/si/numbers';
 import '@progress/kendo-angular-intl/locales/si/calendar';
 import '@progress/kendo-angular-intl/locales/tr/numbers';
 import '@progress/kendo-angular-intl/locales/tr/calendar';
+import '@progress/kendo-angular-intl/locales/zh/numbers';
+import '@progress/kendo-angular-intl/locales/zh/calendar';
+import '@progress/kendo-angular-intl/locales/hr/numbers';
+import '@progress/kendo-angular-intl/locales/hr/calendar';
+import '@progress/kendo-angular-intl/locales/pt/numbers';
+import '@progress/kendo-angular-intl/locales/pt/calendar';
+import '@progress/kendo-angular-intl/locales/fr/numbers';
+import '@progress/kendo-angular-intl/locales/fr/calendar';
+import '@progress/kendo-angular-intl/locales/sr/numbers';
+import '@progress/kendo-angular-intl/locales/sr/calendar';
 
+import '@progress/kendo-angular-intl/locales/sr-Cyrl/numbers';
+import '@progress/kendo-angular-intl/locales/sr-Cyrl/calendar';
+
+import '@progress/kendo-angular-intl/locales/sr-Latn/numbers';
+import '@progress/kendo-angular-intl/locales/sr-Latn/calendar';
+
+import '@progress/kendo-angular-intl/locales/sl/numbers';
+import '@progress/kendo-angular-intl/locales/sl/calendar';
+
+import '@progress/kendo-angular-intl/locales/ru/numbers';
+import '@progress/kendo-angular-intl/locales/ru/calendar';
 
 const KENDO_UI_MODULES = [
     GridModule,
