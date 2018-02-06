d1c7f838a031a39753a7e6e1df9267a8a94f264d
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-02-06 10:05:10 +0100

    fix merge

--- Framework/TbNameSolver/PathFinder.cpp
+++ Framework/TbNameSolver/PathFinder.cpp
@@ -170,8 +170,6 @@ enum EncodingType { ANSI, UTF8, UTF16_BE, UTF16_LE };	//UTF16_BE: Big Endian (sw
 
 static const TCHAR szTemplate[] = _T("Template");
 
-static const TCHAR szTemplate[] = _T("Template");
-
 //============================================================================
 //	Static Objects & general functions
 //============================================================================
