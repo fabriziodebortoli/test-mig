0cd9d115be62a233adabcc5af360017d4d1856fd
Luca Bruni <luca.bruni@microarea.it>
2018-02-21 21:31:58 +0100

    aggiunto parametro per specificare porta

--- client/web-form/run.bat
+++ client/web-form/run.bat
@@ -1,6 +1,9 @@
+set port=4200
+IF [%~1]==[port] IF NOT [%~2]==[] set port=%~2
+
 start ..\..\..\TaskBuilder\TaskBuilderNet\Microarea.TaskBuilderNet.TBLoaderService\bin\Debug\TbLoaderService.exe
 cd ..\..\server\web-server
 start dotnet run
 cd ..\..\client\web-form
 call npm run postinstall
-node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" serve --preserve-symlinks --port=4200
+node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" serve --preserve-symlinks --port=%port%
