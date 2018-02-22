a0a52514fc838018b812a914574f193f68f37363
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-02-22 16:31:03 +0100

    ng: sfm run.bat

---
+++ client/apps/sfm/run.bat
@@ -0,0 +1,8 @@
+set port=4200
+IF [%~1]==[port] IF NOT [%~2]==[] set port=%~2
+
+cd ..\..\..\server\web-server
+start dotnet run
+cd ..\..\client\apps\sfm
+call npm run postinstall
+ng serve --preserve-symlinks --port=%port%
