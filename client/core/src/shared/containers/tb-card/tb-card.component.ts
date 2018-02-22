63a1abe64b237e7887dc28ad7955009cabe96c2a
simona.podesta <simona.podesta@microarea.it>
2018-02-22 12:54:03 +0100

    Tolto ViewEncapsulation.None da tb-card

--- client/core/src/shared/containers/tb-card/tb-card.component.ts
+++ client/core/src/shared/containers/tb-card/tb-card.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'tb-card',
   templateUrl: './tb-card.component.html',
-  styleUrls: ['./tb-card.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./tb-card.component.scss']
 })
 export class TbCardComponent {
   @Input() isCollapsed: boolean;
