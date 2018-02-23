d3b4bcfb43173d1910945d30c45b05806edbeb21
simona.podesta <simona.podesta@microarea.it>
2018-02-23 14:20:44 +0100

    Tolto ViewEncapsulation.None da header-strip

--- client/core/src/shared/components/header-strip/header-strip.component.ts
+++ client/core/src/shared/components/header-strip/header-strip.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'tb-header-strip',
   templateUrl: './header-strip.component.html',
-  styleUrls: ['./header-strip.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./header-strip.component.scss']
 })
 export class HeaderStripComponent implements OnInit {
 
