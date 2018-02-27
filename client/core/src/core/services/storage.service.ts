bafe2a670be1d03c24a9d83c67ac534026ca62b9
pd <paolo.dellepiane@microarea.it>
2018-02-27 10:36:37 +0100

    add localstorage error handling

--- client/core/src/core/services/storage.service.ts
+++ client/core/src/core/services/storage.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Logger } from './../../core/services/logger.service';
 
 export type StorageOptions = { scope?: KeyScope, componentInfo?: { cmpId?: string, type: string, app?: string, mod?: string } };
 export enum KeyScope { Component, Document, Global };
 
 @Injectable()
 export class StorageService {
-    private storageOpt: StorageOptions = { scope: KeyScope.Component };
+    public options: StorageOptions = { scope: KeyScope.Component };
 
-    get options(): StorageOptions { return this.storageOpt; }
-    set options(opt: StorageOptions) {
-        this.storageOpt = { ...this.storageOpt, ...opt };
-    }
+    constructor(private log: Logger) { }
 
     get<T>(key: string): T;
     get(key: string): any {
-        return JSON.parse(localStorage.getItem(this.uniqueKey(key, this.storageOpt.scope)));
+        return JSON.parse(localStorage.getItem(this.uniqueKey(key, this.options.scope)));
     }
 
     getOrDefault<T>(key: string, def: T): T {
@@ -26,12 +24,21 @@ export class StorageService {
     }
 
     remove(key: string): void {
-        localStorage.removeItem(this.uniqueKey(key, this.storageOpt.scope));
+        localStorage.removeItem(this.uniqueKey(key, this.options.scope));
     }
 
     set<T>(key: string, data: T): T;
     set(key: string, data: any): any {
-        localStorage.setItem(this.uniqueKey(key, this.storageOpt.scope), JSON.stringify(data));
+        try {
+            localStorage.setItem(this.uniqueKey(key, this.options.scope), JSON.stringify(data));
+        } catch (e) {
+            if (e.name === 'QUOTA_EXCEEDED_ERR' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED' ||
+                e.name === 'QuotaExceededError' || e.name === 'W3CException_DOM_QUOTA_EXCEEDED_ERR') {
+                this.log.error('LocalStorage is full');
+            } else {
+                this.log.error('error saving to LocalStorage: ' + e.message);
+            }
+        }
         return data;
     }
 
@@ -40,12 +47,13 @@ export class StorageService {
     // tslint:disable-next-line:member-ordering
     private uniqueKeyPer = {
         [KeyScope.Component]: key => [
+            'storage',
             localStorage.getItem('_user'),
             localStorage.getItem('_company'),
-            this.storageOpt.componentInfo.app,
-            this.storageOpt.componentInfo.mod,
-            this.storageOpt.componentInfo.type,
-            this.storageOpt.componentInfo.cmpId,
+            this.options.componentInfo.app,
+            this.options.componentInfo.mod,
+            this.options.componentInfo.type,
+            this.options.componentInfo.cmpId,
             key].filter(x => x).join('_'),
         [KeyScope.Document]: key => '',
         [KeyScope.Global]: key => ''
