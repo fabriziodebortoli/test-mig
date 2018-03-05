d9377ce52396aae52cb4af70821ea34f4a8f3042
Luca Bruni <luca.bruni@microarea.it>
2018-03-05 15:45:41 +0100

    aggiunto change.emit anche per la checkbox

--- client/core/src/shared/controls/checkbox/checkbox.component.ts
+++ client/core/src/shared/controls/checkbox/checkbox.component.ts
@@ -1,4 +1,8 @@
-﻿import { Component, Input } from '@angular/core';
+﻿import { Component, Input, ChangeDetectorRef, SimpleChanges } from '@angular/core';
+
+import { TbComponentService } from './../../../core/services/tbcomponent.service';
+import { LayoutService } from './../../../core/services/layout.service';
+import { EventDataService } from './../../../core/services/eventdata.service';
 
 import { ControlComponent } from './../control.component';
 
@@ -8,5 +12,18 @@ import { ControlComponent } from './../control.component';
     styleUrls: ['./checkbox.component.scss']
 })
 
-export class CheckBoxComponent  extends ControlComponent{
+export class CheckBoxComponent extends ControlComponent {
+    constructor(
+        public eventData: EventDataService,
+        layoutService: LayoutService,
+        tbComponentService: TbComponentService,
+        changeDetectorRef: ChangeDetectorRef
+    ) {
+        super(layoutService, tbComponentService, changeDetectorRef);
+    }
+
+    modelChanged(event) {
+        this.model.value = event;
+        this.eventData.change.emit(this.cmpId);
+    }
 }
