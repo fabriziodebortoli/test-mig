a0dc107f4bd1af6265684ed8f506e0809f76fb49
Bruna Calandrini <bruna.calandrini@microarea.it>
2018-03-06 17:09:34 +0100

    prog. 7047: implementato filtri WebApi per presenza authentication token

---
+++ server/easystudio-service/AspNetCore/AuthenticationFilters.cs
@@ -0,0 +1,26 @@
+﻿
+using Microarea.Common;
+using Microarea.Common.Applications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Microarea.EasyStudio.AspNetCore
+{
+    //=========================================================================
+    public class AuthenticationFilters : ActionFilterAttribute
+    {
+        const string MissingAuthentication = "Missing authentication token!";
+        //---------------------------------------------------------------------
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string authToken = AutorizationHeaderManager.GetAuthorizationElement(context.HttpContext.Request, UserInfo.AuthenticationTokenKey);
+            if (string.IsNullOrEmpty(authToken))
+            {
+                context.HttpContext.Response.StatusCode = 401;
+                context.Result = new ForbidResult(/*MissingAuthentication*/);
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
--- server/easystudio-service/Controllers/BaseController.cs
+++ server/easystudio-service/Controllers/BaseController.cs
@@ -5,7 +5,7 @@ using TaskBuilderNetCore.EasyStudio.Interfaces;
 namespace Microarea.EasyStudio.Controllers
 {
     //=========================================================================
-    [RequestResultFilters]
+    [/* Controllo di authtoken AuthenticationFilters,*/ RequestResultFilters]
     public class BaseController : Microsoft.AspNetCore.Mvc.Controller
     {
         protected IServiceManager Services { get; set; }
