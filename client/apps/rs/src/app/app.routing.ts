e090960c3437cd05bf1e96dc4305e062991b1c04
Elisa Costa <elisa.costa@microarea.it>
2018-03-14 11:32:37 +0100

    dev-next: Modifiche con Fabrizio per RS stand-alone

--- client/apps/rs/src/app/app.routing.ts
+++ client/apps/rs/src/app/app.routing.ts
@@ -5,11 +5,19 @@ import { ReportingStudioFactoryComponent } from '@taskbuilder/reporting-studio';
 
 import { environment } from './../environments/environment';
 
-import { CoreGuard, LoginComponent, PageNotFoundComponent } from '@taskbuilder/core';
+import { CoreGuard, LoginComponent, PageNotFoundComponent, HomeComponent, ProxyRouteComponent } from '@taskbuilder/core';
 
 export const routing: ModuleWithProviders = RouterModule.forRoot([
-  { path: '', component: ReportingStudioFactoryComponent, canActivate: [CoreGuard] },
+  { path: '', component: HomeComponent, canActivate: [CoreGuard] },
   { path: 'login', component: LoginComponent },
+  {
+    path: 'proxy',
+    outlet: 'dynamic',
+    component: ProxyRouteComponent,
+    children: [
+      { path: 'rs', loadChildren: '@taskbuilder/reporting-studio#ReportingStudioModule' },
+    ],
+  },
   { path: '**', component: PageNotFoundComponent },
 ]);
 
---
+++ client/apps/rs/src/environments/environment.desktop.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  loggerDebug: false,
+  desktop: true,
+  test: false
+};
