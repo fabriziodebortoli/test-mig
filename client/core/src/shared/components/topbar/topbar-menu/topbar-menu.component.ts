2c6986b71cd92d3d5788d66fddd17dad73fbbfd6
simona.podesta <simona.podesta@microarea.it>
2018-03-14 16:26:56 +0100

    Aggiunto synch alla top bar

--- client/core/src/shared/components/topbar/topbar-menu/topbar-menu.component.ts
+++ client/core/src/shared/components/topbar/topbar-menu/topbar-menu.component.ts
@@ -1,18 +1,22 @@
 import { Subscription } from 'rxjs/Subscription';
-import { Component, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Component, ViewEncapsulation, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { TbComponent } from './../../../../shared/components/tb.component';
+
 
 import { InfoService } from './../../../../core/services/info.service';
 import { SettingsService } from './../../../../core/services/settings.service';
 import { HttpService } from './../../../../core/services/http.service';
 
 import { EasyStudioContextComponent } from './../../../../shared/components/easystudio-context/easystudio-context.component';
+import { MenuService } from './../../../../menu/services/menu.service';
+import { TbComponentService } from './../../../../core/services/tbcomponent.service';
 
 @Component({
   selector: 'tb-topbar-menu',
   templateUrl: './topbar-menu.component.html',
   styleUrls: ['./topbar-menu.component.scss']
 })
-export class TopbarMenuComponent implements OnDestroy{
+export class TopbarMenuComponent extends TbComponent implements OnDestroy{
 
   isDesktop: boolean;
   isESActivated: boolean;
@@ -22,7 +26,14 @@ export class TopbarMenuComponent implements OnDestroy{
   constructor(
     public infoService: InfoService,
     public settingsService: SettingsService,
-    public httpService: HttpService) {
+    public httpService: HttpService,
+    public menuService: MenuService,
+    tbComponentService: TbComponentService,
+    changeDetectorRef: ChangeDetectorRef
+  ) {
+    super(tbComponentService, changeDetectorRef);
+    this.enableLocalization();
+
     this.isDesktop = infoService.isDesktop;
     this.isESActivated = settingsService.IsEasyStudioActivated;
     this.subscription= httpService.isActivated('BPM','Connector').subscribe( res => {
@@ -32,5 +43,9 @@ export class TopbarMenuComponent implements OnDestroy{
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-  }        
+  }
+  
+  clearCachedData() {
+    this.menuService.invalidateCache();
+}
 }
