5489a00d5f9f1eeb7a213d56c4a4ecb9f828da12
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-03-15 15:58:16 +0100

    fix script tbjson

--- script/tbjson.bat
+++ script/tbjson.bat
@@ -1,11 +1,13 @@
 @echo off
 
+taskkill /f /im TbJsonCacheServer.exe
+
 set VisualStudioPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\
 set BuildSorgenti="%VisualStudioPath%devenv.com"
 
 %BuildSorgenti% /SafeMode "..\Tools\TbJson\TbJson.sln" /rebuild Release
 
-cd ../
-rd /s /q .\client\web-form\src\app\applications
-.\Framework\TbUtility\TbJson\TbJson.exe /ts .
-cd .\script
+cd ../../
+rd /s /q .\Taskbuilder\client\web-form\src\app\applications
+.\Taskbuilder\Framework\TbUtility\TbJson\TbJson.exe /ts .
+cd .\Taskbuilder\script
