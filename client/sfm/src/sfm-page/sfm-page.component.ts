6342f876643880c6fbcf0e0942794293a7a5dfd3
maurorogledi <mauro.rogledi@microarea.it>
2018-03-26 12:35:04 +0200

    sfm - Pagina di prova con lettura da DB

--- client/sfm/src/sfm-page/sfm-page.component.ts
+++ client/sfm/src/sfm-page/sfm-page.component.ts
@@ -1,5 +1,5 @@
-import { ComponentService, DocumentComponent, EventDataService, LayoutService } from '@taskbuilder/core';
-import { Component, OnInit, ComponentFactoryResolver, ChangeDetectorRef } from '@angular/core';
+import { ComponentService, DocumentComponent, EventDataService, LayoutService, DataService } from '@taskbuilder/core';
+import { Component, Input, OnInit, ComponentFactoryResolver, ChangeDetectorRef } from '@angular/core';
 import { URLSearchParams, Http } from '@angular/http';
 
 import { SFMService } from './../sfm.service';
@@ -12,16 +12,31 @@ import { SFMService } from './../sfm.service';
 })
 export class SFMPageComponent extends DocumentComponent implements OnInit {
 
+    @Input() prova: any[] = [];
+
+    WorkerID : number = 1;
+
     constructor(
         public sfmService: SFMService,
-         eventData: EventDataService,
-         changeDetectorRef:ChangeDetectorRef) {
+        eventData: EventDataService,
+        private dataService: DataService,
+        changeDetectorRef: ChangeDetectorRef) {
         super(sfmService, eventData, null, changeDetectorRef);
     }
 
     ngOnInit() {
         super.ngOnInit();
         this.eventData.model = { 'Title': { 'value': "SFM" } };
+
+        this.FillData();
+    }
+
+    async FillData() {
+        let p = new URLSearchParams();
+        p.set('filter', this.WorkerID.toString());
+        let data = await this.dataService.getData('SFM.SFMLabourPlanner.Dbl.LabourAssignmentQuery', 'direct', p).take(1).toPromise();
+        if (data !== undefined)
+            this.prova.push(...data.rows);
     }
 
 }
