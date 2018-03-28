5950ef5bc5695beb78c3236193cf3cf5c5a2db5c
Matteo Canessa <matteo.canessa@microarea.it>
2018-03-28 16:24:08 +0200

    Sostituito reference a Nustache per .NET fx con libreria dotnetcore ricompilta da noi con i sorgenti di Nustache

---
+++ server/library/NustacheNetCore/NustacheNetCore/Nustache.Core/RenderContextBehaviour.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Nustache.Core
+{
+    public class RenderContextBehaviour
+    {
+        public bool RaiseExceptionOnDataContextMiss { get; set; }
+        public bool RaiseExceptionOnEmptyStringValue { get; set; }
+
+        public Encoders.HtmlEncoder HtmlEncoder { get; set; }
+
+        public static RenderContextBehaviour GetDefaultRenderContextBehaviour()
+        {
+            return new RenderContextBehaviour
+                        {
+                            RaiseExceptionOnDataContextMiss = false, 
+                            RaiseExceptionOnEmptyStringValue  = false
+                        }; 
+        }
+    }
+}
