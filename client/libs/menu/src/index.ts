90f8aa8984dd778000ede550e477d263a8ecbb82
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-03-29 08:49:52 +0200

    ng menu lib base

---
+++ client/libs/menu/src/index.ts
@@ -0,0 +1,24 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+
+  ],
+  exports: [
+
+  ]
+})
+export class TbMenuModule {
+  // static forRoot(): ModuleWithProviders {
+  //   return {
+  //     ngModule: SampleModule,
+  //     providers: [SampleService]
+  //   };
+  // }
+}
