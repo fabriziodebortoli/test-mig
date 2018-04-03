f5060c5a126a318454ba2d30085d8b22806a3e18
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-04-03 12:57:49 +0200

    mod script ng-build-desktop --no-sourcemaps

--- script/ng-build-desktop.bat
+++ script/ng-build-desktop.bat
@@ -1,3 +1,2 @@
 cd ..\client\web-form
-node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --env=desktop --preserve-symlinks --output-path="..\..\WebFramework\M4Client"
-REM node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --prod --build-optimizer --preserve-symlinks --output-path="..\..\..\TaskBuilder\WebFramework\M4Client"
+node --max_old_space_size=9120 "node_modules\@angular\cli\bin\ng" build --env=desktop --preserve-symlinks --no-sourcemaps --output-path="..\..\WebFramework\M4Client"
