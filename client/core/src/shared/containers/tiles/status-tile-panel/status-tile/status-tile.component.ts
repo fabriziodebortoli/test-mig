996df071ddd7b54448188c04dac2ad050f06ed52
Luca Bruni <luca.bruni@microarea.it>
2018-04-04 16:22:48 +0200

    aggiunte proprietà per dataobj visible, clickable, backgroundColor

--- client/core/src/shared/containers/tiles/status-tile-panel/status-tile/status-tile.component.ts
+++ client/core/src/shared/containers/tiles/status-tile-panel/status-tile/status-tile.component.ts
@@ -11,9 +11,34 @@ export class StatusTileComponent {
   active: boolean = true;
 
   @Input() title: string;
-  @Input() clickable: boolean;
-  @Input() visible: boolean;
-  @Input() backgroundColor: string;
+  @Input() _clickable: any;
+  @Input() _visible: any;
+  @Input() _backgroundColor: any;
+
+
+  @Input()
+  set clickable(clickable: any) {
+    this._clickable = clickable instanceof Object ? clickable.value : clickable;
+  }
+  get clickable() {
+    return this._clickable;
+  }
+
+  @Input()
+  set visible(visible: any) {
+    this._visible = visible instanceof Object ? visible.value : visible;
+  }
+  get visible() {
+    return this._visible;
+  }
+
+  @Input()
+  set backgroundColor(backgroundColor: any) {
+    this._backgroundColor = backgroundColor instanceof Object ? backgroundColor.value : backgroundColor;
+  }
+  get backgroundColor() {
+    return this._backgroundColor;
+  }
 
   constructor() { }
 
