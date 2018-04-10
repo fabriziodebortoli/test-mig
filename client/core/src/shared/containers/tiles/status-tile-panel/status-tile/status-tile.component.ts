7fd33786cdbf20256d7160fd376d830e61e55610
Valerio Gattero <valerio.gattero@microarea.it>
2018-04-10 14:57:32 +0200

    Add status tile

--- client/core/src/shared/containers/tiles/status-tile-panel/status-tile/status-tile.component.ts
+++ client/core/src/shared/containers/tiles/status-tile-panel/status-tile/status-tile.component.ts
@@ -1,46 +1,74 @@
-import { Component, Input } from '@angular/core';
+import { TbComponent } from '../../../../components/tb.component';
+import { EventDataService } from './../../../../../core/services/eventdata.service';
+import { TbComponentService } from './../../../../../core/services/tbcomponent.service';
+import { LayoutService } from './../../../../../core/services/layout.service';
+import { ControlComponent } from './../../../../controls/control.component';
+import { Component, Input, ChangeDetectorRef, OnDestroy, Injector, ViewContainerRef, QueryList, ContentChild, TemplateRef,
+         ElementRef, AfterContentInit } from '@angular/core';
+
+
 
 @Component({
-  selector: 'tb-status-tile',
+  selector: 'tb-status-tile',  
   templateUrl: './status-tile.component.html',
   styleUrls: ['./status-tile.component.scss']
 })
-export class StatusTileComponent {
 
+export class StatusTileComponent extends ControlComponent implements OnDestroy, AfterContentInit{
 
   active: boolean = true;
 
   @Input() title: string;
-  @Input() _clickable: any;
-  @Input() _visible: any;
-  @Input() _backgroundColor: any;
+  @Input() clickable: boolean;
+  @Input() visible: boolean;
+  @Input() backgroundColor: any;
+  _backGroundHexColors : string;
+  DEFAULT_TILE_COLOR;
 
-
-  @Input()
-  set clickable(clickable: any) {
-    this._clickable = clickable instanceof Object ? clickable.value : clickable;
-  }
-  get clickable() {
-    return this._clickable;
-  }
-
-  @Input()
-  set visible(visible: any) {
-    this._visible = visible instanceof Object ? visible.value : visible;
-  }
-  get visible() {
-    return this._visible;
+  constructor(layoutService: LayoutService, 
+    changeDetectorRef: ChangeDetectorRef,
+    tbComponentService: TbComponentService,
+    public eventData: EventDataService,
+    elRef: ElementRef) {
+    super(layoutService,tbComponentService,changeDetectorRef);
+    this.DEFAULT_TILE_COLOR = "RGB(255, 255, 255)";
   }
 
-  @Input()
-  set backgroundColor(backgroundColor: any) {
-    this._backgroundColor = backgroundColor instanceof Object ? backgroundColor.value : backgroundColor;
-  }
-  get backgroundColor() {
-    return this._backgroundColor;
+  ngOnInit() {  
   }
 
-  constructor() { }
+  ngAfterContentInit(): void {          
+     this._backGroundHexColors = this.backgroundColor ? this.ExtractRGBValues(this.backgroundColor.value) : this.DEFAULT_TILE_COLOR;       
+  }
 
-  ngOnInit() { }
+   inputStyle(){
+
+    return {      
+        'border-radius': '5px',
+        'border-color' : 'Black',
+        'border-width' : 'thick',
+        'margin-top' : '2px',
+        'margin-right' : '8px',
+        'margin-bottom' : '2px',
+        'margin-left' : '8px',     
+        'background': this._backGroundHexColors
+      };
+   }
+ExtractRGBValues(RGBvalues){  
+    try{
+        var digits = /(.*?)RGB\((\d+), (\d+), (\d+)\)/.exec(RGBvalues);            
+        return "#" + this.RGBComponentToHex(digits[2]) + this.RGBComponentToHex(digits[3]) + this.RGBComponentToHex(digits[4]);        
+    }
+    catch(e){
+        console.log("Error while converting from RGB to Hex, default background color " + this.DEFAULT_TILE_COLOR +  " will be used. Error: " + e);
+        return this.DEFAULT_TILE_COLOR;
+    }
 }
+RGBComponentToHex(c) {   
+    let number = new Number(c);   
+    var hex = number.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+}
+
