ce4ceb4e4126a3191cb7efad94736e4379b6b880
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-04-10 08:45:45 +0200

    tbjson script check if process exist and messages

--- script/tbjson.bat
+++ script/tbjson.bat
@@ -1,22 +1,28 @@
 @echo off
 
-echo TbJson.bat - Killing process TbJsonCacheServer.exe
-echo.
-taskkill /f /im TbJsonCacheServer.exe
+tasklist /FI "IMAGENAME eq TbJsonCacheServer.exe" 2>NUL | find /I /N "TbJsonCacheServer.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo.
+    echo TbJson.bat - Killing process TbJsonCacheServer.exe
+    taskkill /f /im TbJsonCacheServer.exe
+    echo.
+)
 
 set VisualStudioPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\
 set BuildSorgenti="%VisualStudioPath%devenv.com"
 
-echo TbJson.bat - Build TbJson.sln
+echo.
+echo TbJson.bat - Building TbJson.sln
 echo.
 %BuildSorgenti% /SafeMode "..\Tools\TbJson\TbJson.sln" /rebuild Release
 
 cd ../../
-echo TbJson.bat - Remove web-form applications folder
 echo.
+echo TbJson.bat - Removing applications folder of web-form project
 rd /s /q .\Taskbuilder\client\web-form\src\app\applications
+echo.
 
 echo TbJson.bat - TS Generation...
-echo.
 .\Taskbuilder\Framework\TbUtility\TbJson\TbJson.exe /ts .
 cd .\Taskbuilder\script
+echo.
