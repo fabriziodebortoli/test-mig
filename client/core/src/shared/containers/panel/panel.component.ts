ac8b2da144372d38400aeb527ae5d8e32d2a649b
Anna Bonzi <anna.bonzi@microarea.it>
2018-04-10 15:14:50 +0200

    Perasso: aggiunti event listener per titolo tile
    aggiunti event listener per ricalcolare il titolo della tile al cambiamento del model

--- client/core/src/shared/containers/panel/panel.component.ts
+++ client/core/src/shared/containers/panel/panel.component.ts
@@ -1,3 +1,4 @@
+import { Subscription } from 'rxjs';
 import { TbComponentService } from './../../../core/services/tbcomponent.service';
 import { BOService } from './../../../core/services/bo.service';
 import { Component, OnInit, Input, Output, ViewEncapsulation, EventEmitter, OnDestroy, ChangeDetectorRef, HostBinding } from '@angular/core';
@@ -5,82 +6,113 @@ import { TbComponent } from '../../components/tb.component';
 
 
 @Component({
-  selector: 'tb-panel',
-  templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.scss']
+    selector: 'tb-panel',
+    templateUrl: './panel.component.html',
+    styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent extends TbComponent implements OnInit, OnDestroy {
 
-  _title: string;
-  _collapsedTitle: string;
+    _title: any;
+    _collapsedTitle: any;
 
-  @HostBinding('class.collapsed') @Input() isCollapsed: boolean = false;
-  @Input() isCollapsible: boolean = false;
-  realTitle = ""
-  @Output() toggle = new EventEmitter<boolean>();
+    subscriptions: Subscription[] = new Array<Subscription>();
 
-  constructor(
-    private boService: BOService,
-    tbComponentService: TbComponentService,
-    changeDetectorRef: ChangeDetectorRef
-  ) {
-    super(tbComponentService, changeDetectorRef);
-  }
-  ngOnInit() {
-    this.boService.activateContainer(this.cmpId, true, false);
-  }
-  ngOnDestroy() {
+    @HostBinding('class.collapsed') @Input() isCollapsed: boolean = false;
+    @Input() isCollapsible: boolean = false;
+    realTitle = ""
+    @Output() toggle = new EventEmitter<boolean>();
 
-    this.boService.activateContainer(this.cmpId, false, false);
-  }
+    constructor(
+        private boService: BOService,
+        tbComponentService: TbComponentService,
+        changeDetectorRef: ChangeDetectorRef
+    ) {
+        super(tbComponentService, changeDetectorRef);
+    }
+    ngOnInit() {
+        this.boService.activateContainer(this.cmpId, true, false);
+    }
+    ngOnDestroy() {
 
-  @Input() public set title(val: any) {
-    this._title = val instanceof Object ? val.value : val;
-    this.calculateRealTitle();
-  }
-  public get title(): any {
-    return this._title;
-  }
-  @Input() public set collapsedTitle(val: string) {
-    this._collapsedTitle = val;
-    this.calculateRealTitle();
-  }
-  public get collapsedTitle(): string {
-    return this._collapsedTitle;
-  }
-
-  toggleCollapse(): void {
-    if (!this.isCollapsible)
-      return;
-
-    this.isCollapsed = !this.isCollapsed;
-    this.calculateRealTitle();
-
-    this.toggle.emit(this.isCollapsed);
-  }
-
-  collapse(): void {
-    if (!this.isCollapsible)
-      return;
-
-    this.isCollapsed = true;
-    this.calculateRealTitle();
-  }
-
-  expand(): void {
-    if (!this.isCollapsible)
-      return;
-
-    this.isCollapsed = false;
-    this.calculateRealTitle();
-  }
-
-  calculateRealTitle() {
-    if (this.isCollapsed && this._collapsedTitle) {
-      this.realTitle = this._collapsedTitle;
-    } else {
-      this.realTitle = this._title;
+        this.boService.activateContainer(this.cmpId, false, false);
+        this.subscriptions.forEach(element => {
+            element.unsubscribe();
+        });
     }
 
-  }
+    @Input() public set title(val: any) {
+        this._title = val;
+        if (this._title) {
+            if (this._title.valueChanged) {
+                this.subscriptions.push(this._title.valueChanged.subscribe(sender => this.calculateRealTitle()));
+            }
+            if (this._title.modelChanged) {
+                this.subscriptions.push(this._title.modelChanged.subscribe(sender => this.calculateRealTitle()));
+            }
+        }
+        this.calculateRealTitle();
+    }
+    public get title(): any {
+        return this._title;
+    }
+    @Input() public set collapsedTitle(val: string) {
+        this._collapsedTitle = val;
+        if (this._collapsedTitle) {
+            if (this._collapsedTitle.valueChanged) {
+                this.subscriptions.push(this._collapsedTitle.valueChanged.subscribe(sender => this.calculateRealTitle()));
+            } if (this._collapsedTitle.modelChanged) {
+                this.subscriptions.push(this._collapsedTitle.modelChanged.subscribe(sender => this.calculateRealTitle()));
+            }
+        }
+        this.calculateRealTitle();
+    }
+    public get collapsedTitle(): string {
+        return this._collapsedTitle;
+    }
+
+    toggleCollapse(): void {
+        if (!this.isCollapsible)
+            return;
+
+        this.isCollapsed = !this.isCollapsed;
+        this.calculateRealTitle();
+
+        this.toggle.emit(this.isCollapsed);
+    }
+
+    collapse(): void {
+        if (!this.isCollapsible)
+            return;
+
+        this.isCollapsed = true;
+        this.calculateRealTitle();
+    }
+
+    expand(): void {
+        if (!this.isCollapsible)
+            return;
+
+        this.isCollapsed = false;
+        this.calculateRealTitle();
+    }
+
+    calculateRealTitle() {
+        if (this.isCollapsed && this._collapsedTitle) {
+            if (this._collapsedTitle.hasOwnProperty('value')) {
+                this.realTitle = this._collapsedTitle['value'];
+            }
+            else {
+                this.realTitle = this._collapsedTitle;
+            }
+
+        } else {
+            if (this._title.hasOwnProperty('value')) {
+                this.realTitle = this._title['value'];
+            }
+            else {
+                this.realTitle = this._title;
+            }
+        }
+
+    }
 }
