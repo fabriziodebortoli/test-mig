9beb1d4dd1f76505c64382f045a841dd7c995709
Silvano Parodi <silvano.parodi@microarea.it>
2018-04-10 14:16:42 +0200

    dev-next : RsWeb standalone - app.routing

--- client/apps/rs/src/app/app.routing.ts
+++ client/apps/rs/src/app/app.routing.ts
@@ -1,24 +1,21 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { ReportingStudioFactoryComponent } from '@taskbuilder/reporting-studio';
 
 import { environment } from './../environments/environment';
 
 import { CoreGuard, LoginComponent, PageNotFoundComponent, ProxyRouteComponent } from '@taskbuilder/core';
 import { HomeComponent } from '@taskbuilder/menu';
+//import { StandaloneReportComponent } from '@taskbuilder/reporting-studio';
+
+
+
+
 
 export const routing: ModuleWithProviders = RouterModule.forRoot([
   { path: '', component: HomeComponent, canActivate: [CoreGuard] },
   { path: 'login', component: LoginComponent },
-  {
-    path: 'proxy',
-    outlet: 'dynamic',
-    component: ProxyRouteComponent,
-    children: [
-      { path: 'rs', loadChildren: '@taskbuilder/reporting-studio#ReportingStudioModule' },
-    ],
-  },
+ // { path: 'rs/:ns', component: StandaloneReportComponent },
   { path: '**', component: PageNotFoundComponent },
 ]);
 
