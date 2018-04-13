20ebe5b416ab08e36a0027edd7bb9cfc37e00917
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-04-13 17:02:29 +0200

    inizio tb-image con lightbox
    per ora commentato ma pusho il lavoro finora fatto

--- client/core/src/shared/controls/image/image.component.ts
+++ client/core/src/shared/controls/image/image.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ChangeDetectorRef } from '@angular/core';
+// import { NgxGalleryOptions, NgxGalleryImage } from 'ngx-gallery';
 
 import { TbComponentService } from './../../../core/services/tbcomponent.service';
 import { LayoutService } from './../../../core/services/layout.service';
@@ -15,11 +16,20 @@ export class ImageComponent extends ControlComponent {
 
   @Input() title: string = '';
 
+  // images: NgxGalleryImage[];
+  // options: NgxGalleryOptions[] = [{
+  //   image: false,
+  //   height: '100px'
+  // }, {
+  //   breakpoint: 500,
+  //   width: '100%'
+  // }];
+
   constructor(
     public infoService: InfoService,
     layoutService: LayoutService,
     tbComponentService: TbComponentService,
-    changeDetectorRef:ChangeDetectorRef) {
+    changeDetectorRef: ChangeDetectorRef) {
     super(layoutService, tbComponentService, changeDetectorRef);
   }
 
@@ -27,11 +37,25 @@ export class ImageComponent extends ControlComponent {
 
     let imgStyles = {};
 
-    if(this.width) imgStyles['max-width'] = this.width + 'px';
-    if(this.height) imgStyles['max-height'] = this.height + 'px';
+    if (this.width) imgStyles['max-width'] = this.width + 'px';
+    if (this.height) imgStyles['max-height'] = this.height + 'px';
+
+
+    // this.options = [{
+    //   // image: false,
+    //   // height: '100px'
+    // }, {
+    //   breakpoint: 500,
+    //   width: '100%'
+    // }];
 
     return imgStyles;
   }
+
+  // getImage(){
+  //   return ["http://localhost:5000/tbloader/api/tb/document/getImage/?src=Image.TBF.TBFModule.images.SplashConsole_90.jpg"];
+  //   // return [this.getImageUrl(this.model.value)];
+  // }
 
   getImageUrl(namespace: string) {
     return this.infoService.getDocumentBaseUrl() + 'getImage/?src=' + namespace;
