4fb4cbbb2a78d202f21de69da97e6cbce4f32fad
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2018-04-17 10:23:52 +0200

    tb-image con lightbox

--- client/core/src/shared/controls/image/image.component.ts
+++ client/core/src/shared/controls/image/image.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ChangeDetectorRef } from '@angular/core';
-// import { NgxGalleryOptions, NgxGalleryImage } from 'ngx-gallery';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryImageSize } from 'ngx-gallery';
 
 import { TbComponentService } from './../../../core/services/tbcomponent.service';
 import { LayoutService } from './../../../core/services/layout.service';
@@ -16,14 +16,18 @@ export class ImageComponent extends ControlComponent {
 
   @Input() title: string = '';
 
-  // images: NgxGalleryImage[];
-  // options: NgxGalleryOptions[] = [{
-  //   image: false,
-  //   height: '100px'
-  // }, {
-  //   breakpoint: 500,
-  //   width: '100%'
-  // }];
+  imgStyles = {};
+
+  images: NgxGalleryImage[] = [];
+  options: NgxGalleryOptions[] = [{
+    closeIcon: 'm4-icon m4-tb-closewindows-2',
+    image: false,
+    thumbnailsColumns: 1,
+    previewCloseOnEsc: true,
+    previewCloseOnClick: true,
+    thumbnailsMargin: 0,
+    thumbnailMargin: 0
+  }];
 
   constructor(
     public infoService: InfoService,
@@ -35,27 +39,24 @@ export class ImageComponent extends ControlComponent {
 
   getStyles() {
 
-    let imgStyles = {};
+    if (this.width) this.imgStyles['max-width'] = this.width + 'px';
 
-    if (this.width) imgStyles['max-width'] = this.width + 'px';
-    if (this.height) imgStyles['max-height'] = this.height + 'px';
+    if (this.height) this.imgStyles['max-height'] = this.height + 'px';
 
-
-    // this.options = [{
-    //   // image: false,
-    //   // height: '100px'
-    // }, {
-    //   breakpoint: 500,
-    //   width: '100%'
-    // }];
-
-    return imgStyles;
+    return this.imgStyles;
   }
 
-  // getImage(){
-  //   return ["http://localhost:5000/tbloader/api/tb/document/getImage/?src=Image.TBF.TBFModule.images.SplashConsole_90.jpg"];
-  //   // return [this.getImageUrl(this.model.value)];
-  // }
+  getImages(namespace: string) {
+    if (!namespace) return [];
+
+    return [
+      {
+        small: this.getImageUrl(namespace),
+        big: this.getImageUrl(namespace),
+        url: this.getImageUrl(namespace)
+      }
+    ];
+  }
 
   getImageUrl(namespace: string) {
     return this.infoService.getDocumentBaseUrl() + 'getImage/?src=' + namespace;
